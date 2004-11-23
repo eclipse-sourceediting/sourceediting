5d8acdff3fbac70c57c538a98011680d29eab45d
pavery <pavery>
2004-11-23 22:31:30 +0000

    adding dummy test suite.

---
+++ tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/application/SSETestSuite.java
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.sse.ui.tests.application;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * @author pavery
+ */
+public class SSETestSuite extends TestSuite {
+	public static Test suite() {
+		return new SSETestSuite();
+	}
+
+	public SSETestSuite() {
+		super("SSETestSuite");
+		// no tests yet
+		//addTest(new TestSuite(TestModelsFromFiles.class, "TestModelsFromFiles"));
+	}
+}
