fe4ee66d9500a776af26cd3290eb578ec3083e5b
nitind <nitind>
2004-12-21 20:59:49 +0000

    This commit was manufactured by cvs2svn to create tag 'v20041221_1609'.

--- bundles/org.eclipse.wst.sse.ui/DevTimeSupport/JSEditor/build.bat
+++
@@ -1,4 +0,0 @@
-javac -g -classpath C:\java\j2sdkee1.2.1\lib\j2ee.jar  *.java
-@if ERRORLEVEL 1 goto done
-java  -Djava.compiler= -classpath .;C:\java\j2sdkee1.2.1\lib\j2ee.jar MakeJavaReflectFCRecs
-:done
