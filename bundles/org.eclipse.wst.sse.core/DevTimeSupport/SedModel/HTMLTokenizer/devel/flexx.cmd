9dabe2550ff7de1ae2534d08c2efcb37b53beb16
nitind <nitind>
2005-01-27 21:53:41 +0000

    update for new JRE and JFlex installation

--- bundles/org.eclipse.wst.sse.core/DevTimeSupport/SedModel/HTMLTokenizer/devel/flexx.cmd
+++ bundles/org.eclipse.wst.sse.core/DevTimeSupport/SedModel/HTMLTokenizer/devel/flexx.cmd
@@ -1,3 +1,3 @@
 @echo off
-PATH=%PATH%;c:\jdk1.4.2_04\bin
-java -Xmx470000000 -cp d:\JFlex\lib\JFlex.jar;. JFlex.Main XMLTokenizer.jflex -skel skeleton.sse && rm -f XMLTokenizer.java~ XMLTokenizer~ && copy XMLTokenizer.java ..\..\..\..\..\org.eclipse.wst.sse.core.xml\src\com\ibm\sse\model\xml\internal\parser\XMLTokenizer.java
+PATH=%PATH%;c:\jdk1.4.2_05\bin
+java -Xmx470000000 -cp d:\JFlex\1.2.2\lib\JFlex.jar;. JFlex.Main XMLTokenizer.jflex -skel skeleton.sse && rm -f XMLTokenizer.java~ XMLTokenizer~ && copy XMLTokenizer.java ..\..\..\..\..\org.eclipse.wst.xml.core\src\org\eclipse\wst\xml\core\internal\parser\XMLTokenizer.java
