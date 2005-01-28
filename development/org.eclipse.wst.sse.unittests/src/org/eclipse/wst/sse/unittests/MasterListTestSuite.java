21dbc07d9ae87e548eae31745b1effcceac48c9c
david_williams <david_williams>
2005-01-28 05:00:12 +0000

    corrected and split unit test to functional set and performance set

--- development/org.eclipse.wst.sse.unittests/src/org/eclipse/wst/sse/unittests/MasterListTestSuite.java
+++ development/org.eclipse.wst.sse.unittests/src/org/eclipse/wst/sse/unittests/MasterListTestSuite.java
@@ -18,20 +18,16 @@ import junit.framework.TestSuite;
 import org.eclipse.jst.jsp.core.tests.JSPCoreTestSuite;
 import org.eclipse.jst.jsp.tests.encoding.JSPEncodingTestSuite;
 import org.eclipse.jst.jsp.ui.tests.JSPUITestSuite;
-import org.eclipse.jst.jsp.ui.tests.performance.JSPUIPerformanceTests;
 import org.eclipse.wst.css.core.tests.CSSCoreTestSuite;
 import org.eclipse.wst.css.tests.encoding.CSSEncodingTestSuite;
-import org.eclipse.wst.css.ui.tests.performance.CSSUIPerformanceTestSuite;
 import org.eclipse.wst.html.core.tests.HTMLCoreTestSuite;
 import org.eclipse.wst.html.tests.encoding.HTMLEncodingTestSuite;
 import org.eclipse.wst.html.ui.tests.HTMLUITestSuite;
-import org.eclipse.wst.html.ui.tests.performance.HTMLUIPerformanceTestSuite;
 import org.eclipse.wst.sse.core.tests.SSEModelTestSuite;
 import org.eclipse.wst.sse.ui.tests.SSEUITestSuite;
-import org.eclipse.wst.sse.ui.tests.performance.SSEUIPerformanceTestSuite;
 import org.eclipse.wst.xml.core.tests.SSEModelXMLTestSuite;
+import org.eclipse.wst.xml.tests.encoding.EncodingTestSuite;
 import org.eclipse.wst.xml.ui.tests.XMLUITestSuite;
-import org.eclipse.wst.xml.ui.tests.performance.XMLUIPerformanceTestSuite;
 
 /*****************************************************************************
  * Copyright (c) 2004 IBM Corporation and others. All rights reserved. This
@@ -47,30 +43,22 @@ public class MasterListTestSuite extends TestSuite {
 
 	public MasterListTestSuite() {
 		super("All Tests");
-		
+
 		addTest(JSPCoreTestSuite.suite());
 		addTest(JSPEncodingTestSuite.suite());
 		addTest(JSPUITestSuite.suite());
-		addTest(JSPUIPerformanceTests.suite());
 		addTest(CSSCoreTestSuite.suite());
 		addTest(CSSEncodingTestSuite.suite());
-		addTest(CSSUIPerformanceTestSuite.suite());
 		addTest(HTMLCoreTestSuite.suite());
 		addTest(HTMLEncodingTestSuite.suite());
 		addTest(HTMLUITestSuite.suite());
-		addTest(HTMLUIPerformanceTestSuite.suite());
-		
 		addTest(SSEModelTestSuite.suite());
 		addTest(SSEUITestSuite.suite());
-		addTest(SSEUIPerformanceTestSuite.suite());
 		addTest(SSEModelXMLTestSuite.suite());
-		
-		// revisit org.eclipse.wst.xml.tests.encoding
-		
+		addTest(EncodingTestSuite.suite());
 		addTest(XMLUITestSuite.suite());
-		addTest(XMLUIPerformanceTestSuite.suite());
-		
-		
+
+
 	}
 
 	public void testAll() {
