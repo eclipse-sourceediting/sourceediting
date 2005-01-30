4eff835a66588f5e403ab852d71ed6c877c020b1
david_williams <david_williams>
2005-01-30 05:54:27 +0000

    added minor test to have clear test of creating Colors and Display.

--- tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/VerifyEditorPlugin.java
+++ tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/VerifyEditorPlugin.java
@@ -1,8 +1,14 @@
 package org.eclipse.wst.sse.ui.tests;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.TestCase;
 
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.wst.sse.ui.EditorPlugin;
 
 /*****************************************************************************
@@ -17,6 +23,8 @@ import org.eclipse.wst.sse.ui.EditorPlugin;
 
 public class VerifyEditorPlugin extends TestCase {
 
+		List colorList = new ArrayList(); 
+	
 	public void testPluginExists() {
 		Plugin plugin = null;
 		try {
@@ -27,5 +35,33 @@ public class VerifyEditorPlugin extends TestCase {
 		}
 		assertNotNull("sse editor plugin could not be instantiated", plugin);
 
+	}
+	/**
+	 * The purpose of this test is just to test if we can directly create
+	 * lots of RBG colors, even if display set to "256" (on Linux). 
+	 * And, BTW, it does not fail even with 256 colors set, at least 
+	 * on Linux, in dev. env. Varying all three RGB numbers, though, 
+	 * it is easy to run out of memory! 
+	 *
+	 */
+	public void testColorHandles() {
+		Display display = Display.getCurrent();
+		if (display == null) display = Display.getDefault();
+		assertNotNull("display could not be instantiated", display);
+		for (int i = 0; i < 256; i++) {
+			for (int j = 0; j < 256; j++) {
+//				for (int k = 0; k < 256; k++) {
+					colorList.add(
+								new Color(display, 
+								new RGB(i,j,50)
+								)
+								);
+//				}
+				
+			}
+			
+		}
+		// if we get this far without error, all is ok.
+		assertTrue(true);
 	}
 }
