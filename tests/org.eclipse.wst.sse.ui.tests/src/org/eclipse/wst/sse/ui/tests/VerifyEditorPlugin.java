dd6b4046d9971e1187ac0c30608155111000e6b8
david_williams <david_williams>
2005-03-29 21:08:39 +0000

    removed old deprecated editor plugin

--- tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/VerifyEditorPlugin.java
+++ tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/VerifyEditorPlugin.java
@@ -9,7 +9,7 @@ import org.eclipse.core.runtime.Plugin;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.wst.sse.ui.EditorPlugin;
+import org.eclipse.wst.sse.ui.internal.SSEUIPlugin;
 
 /*****************************************************************************
  * Copyright (c) 2004 IBM Corporation and others. All rights reserved. This
@@ -23,44 +23,42 @@ import org.eclipse.wst.sse.ui.EditorPlugin;
 
 public class VerifyEditorPlugin extends TestCase {
 
-		List colorList = new ArrayList(); 
-	
+	List colorList = new ArrayList();
+
 	public void testPluginExists() {
 		Plugin plugin = null;
 		try {
-			plugin = EditorPlugin.getDefault();
+			plugin = SSEUIPlugin.getDefault();
 
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			plugin = null;
 		}
 		assertNotNull("sse editor plugin could not be instantiated", plugin);
 
 	}
+
 	/**
-	 * The purpose of this test is just to test if we can directly create
-	 * lots of RBG colors, even if display set to "256" (on Linux). 
-	 * And, BTW, it does not fail even with 256 colors set, at least 
-	 * on Linux, in dev. env. Varying all three RGB numbers, though, 
-	 * it is easy to run out of memory! 
-	 *
+	 * The purpose of this test is just to test if we can directly create lots
+	 * of RBG colors, even if display set to "256" (on Linux). And, BTW, it
+	 * does not fail even with 256 colors set, at least on Linux, in dev. env.
+	 * Varying all three RGB numbers, though, it is easy to run out of memory!
+	 * 
 	 */
 	public void testColorHandles() {
 		Display display = Display.getCurrent();
-		if (display == null) display = Display.getDefault();
+		if (display == null)
+			display = Display.getDefault();
 		assertNotNull("display could not be instantiated", display);
 		for (int i = 0; i < 256; i++) {
 			for (int j = 0; j < 256; j++) {
-// 256 cubed runs out of memory				
-//				for (int k = 0; k < 256; k++) {
-					colorList.add(
-								new Color(display, 
-								new RGB(i,j,50)
-								)
-								);
-//				}
-				
+				// 256 cubed runs out of memory
+				// for (int k = 0; k < 256; k++) {
+				colorList.add(new Color(display, new RGB(i, j, 50)));
+				// }
+
 			}
-			
+
 		}
 		// if we get this far without error, all is ok.
 		assertTrue(true);
