0add2f717c748b2d310303337321a012103ccdb9
pavery <pavery>
2005-04-11 19:15:49 +0000

    added some more tests

--- tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/viewer/TestViewerConfiguration.java
+++ tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/viewer/TestViewerConfiguration.java
@@ -4,29 +4,32 @@ import junit.framework.TestCase;
 
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.IAutoEditStrategy;
+import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.ITextDoubleClickStrategy;
+import org.eclipse.jface.text.IUndoManager;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.formatter.IContentFormatter;
 import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
+import org.eclipse.jface.text.hyperlink.IHyperlinkPresenter;
 import org.eclipse.jface.text.information.IInformationPresenter;
+import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.reconciler.IReconciler;
 import org.eclipse.jface.text.source.IAnnotationHover;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
 import org.eclipse.wst.sse.core.text.IStructuredPartitionTypes;
+import org.eclipse.wst.sse.core.text.IStructuredPartitioning;
 import org.eclipse.wst.sse.ui.StructuredTextViewerConfiguration;
 import org.eclipse.wst.sse.ui.internal.SSEUIPlugin;
 import org.eclipse.wst.sse.ui.internal.StructuredTextViewer;
 import org.eclipse.wst.sse.ui.style.IHighlighter;
 import org.eclipse.wst.sse.ui.tests.Logger;
 
-/**
- * @author pavery
- */
 public class TestViewerConfiguration extends TestCase {
     
 	private StructuredTextViewer fViewer = null;
@@ -98,7 +101,7 @@ public class TestViewerConfiguration extends TestCase {
 		assertTrue(configuredContentTypes.length == 1);
 	}
 	
-	public void testGetContenistant() {
+	public void testGetContentAssistant() {
 		
 		// probably no display
 		if(!fDisplayExists)
@@ -209,5 +212,120 @@ public class TestViewerConfiguration extends TestCase {
 		IHyperlinkDetector[] detectors = fConfig.getHyperlinkDetectors(fViewer);
 		assertNotNull(detectors);
 		assertTrue(detectors.length == 1);
+	}
+	
+	
+	public void testConfigureOn() {
+		// TODO: get a resource
+	}
+	
+	public void testGetConfiguredDocumentPartitioning() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		String partitioning = fConfig.getConfiguredDocumentPartitioning(fViewer);
+		assertEquals(IStructuredPartitioning.DEFAULT_STRUCTURED_PARTITIONING, partitioning);
+	}
+	
+	public void testGetConfiguredTextHoverStateMasks() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		int[] masks = fConfig.getConfiguredTextHoverStateMasks(fViewer, IStructuredPartitionTypes.DEFAULT_PARTITION);
+		assertEquals(2, masks.length);
+	}
+	
+	public void testGetSetDeclaringID() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		String origID = fConfig.getDeclaringID();
+		
+		fConfig.setDeclaringID("testID");
+		String gottenID = fConfig.getDeclaringID();
+		assertEquals("testID", gottenID);
+		
+		// to not mess up other tests
+		fConfig.setDeclaringID(origID);
+	}
+	public void testGetSetEditorPart() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		IEditorPart origPart = fConfig.getEditorPart();
+		
+		fConfig.setEditorPart(null);
+		IEditorPart gottenPart = fConfig.getEditorPart();
+		assertNull(gottenPart);
+		
+		// don't mess up other tests
+		fConfig.setEditorPart(origPart);
+	}
+
+	public void testGetHyperlinkPresenter() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		IHyperlinkPresenter presenter = fConfig.getHyperlinkPresenter(fViewer);
+		assertNotNull("hyperlink presenter shouldn't be null", presenter);
+	}
+	
+	public void testGetInformationControlCreator() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		IInformationControlCreator infoControlCreator = fConfig.getInformationControlCreator(fViewer);
+		assertNotNull("info control creator was null", infoControlCreator);
+	}
+	
+	public void testGetOverviewRulerAnnotationHover() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		IAnnotationHover annotationHover = fConfig.getOverviewRulerAnnotationHover(fViewer);
+		assertNotNull("annotation hover was null", annotationHover);
+	}
+	public void testGetPresentationReconciler() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		IPresentationReconciler presentationReconciler = fConfig.getPresentationReconciler(fViewer);
+		// our default presentation reconciler is null
+		assertNull("presentation reconciler was not null", presentationReconciler);
+	}
+	public void testGetUndoManager() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		IUndoManager undoManager = fConfig.getUndoManager(fViewer);
+		assertNotNull("undo manager was null", undoManager);
+	}
+	public void testSetPreferenceStore() {
+		
+		// probably no display
+		if(!fDisplayExists)
+			return;
+		
+		// not much to tests since there's no getter
+		IPreferenceStore sseStore = SSEUIPlugin.getDefault().getPreferenceStore();
+		fConfig.setPreferenceStore(sseStore);
 	}
 }
