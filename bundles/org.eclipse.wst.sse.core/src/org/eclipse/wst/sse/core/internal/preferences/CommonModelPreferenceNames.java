9dababbec308218b58029e3549e4f6b132de8bbd
nitind <nitind>
2005-05-04 15:35:11 +0000

    clarify preference name

--- bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/preferences/CommonModelPreferenceNames.java
+++ bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/preferences/CommonModelPreferenceNames.java
@@ -73,7 +73,7 @@ public interface CommonModelPreferenceNames {
 	String TASK_TAG_TAGS = "task-tag-tags"; //$NON-NLS-1$
 	String TASK_TAG_PRIORITIES = "task-tag-priorities"; //$NON-NLS-1$
 	String TASK_TAG_ENABLE = "task-tags"; //$NON-NLS-1$
-	String TASK_TAG_PROJECTS_SKIPPED = "task-tag-projects-toBeSkipped"; //$NON-NLS-1$
+	String TASK_TAG_PROJECTS_IGNORED = "task-tag-projects-toIgnore"; //$NON-NLS-1$
 
 
 	/**
