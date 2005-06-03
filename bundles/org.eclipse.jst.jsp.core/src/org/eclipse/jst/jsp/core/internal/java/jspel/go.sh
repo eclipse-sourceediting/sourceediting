b3d794f0cdc8a09c4a5dc765c67dc1e32b229f13
nitind <nitind>
2005-06-03 20:04:59 +0000

    fix spelling

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/java/jspel/go.sh
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/java/jspel/go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-# Set JAVACC_HOME to the insallation directory of javacc 3.2
+# Set JAVACC_HOME to the installation directory of javacc 3.2
 $JAVACC_HOME/bin/jjtree.bat JSPEL.jjt
 $JAVACC_HOME/bin/javacc.bat JSPEL.jj
 sed -f fixtm.sed -i.bak JSPELParserTokenManager.java
