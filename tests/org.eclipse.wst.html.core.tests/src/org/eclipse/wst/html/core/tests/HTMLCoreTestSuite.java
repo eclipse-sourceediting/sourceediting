aba7a6a9a2bd943d984c9c56e662ef88cff39c8b
david_williams <david_williams>
2005-07-08 20:20:07 +0000

    [102068] taginfo: ResourceBundleHelper cant pick up translated files in nl fragments

--- tests/org.eclipse.wst.html.core.tests/src/org/eclipse/wst/html/core/tests/HTMLCoreTestSuite.java
+++ tests/org.eclipse.wst.html.core.tests/src/org/eclipse/wst/html/core/tests/HTMLCoreTestSuite.java
@@ -14,6 +14,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.eclipse.wst.html.core.tests.misc.HTMLCorePreferencesTest;
+import org.eclipse.wst.html.core.tests.misc.HTMLTagInfoTest;
 
 
 
@@ -32,5 +33,6 @@ public class HTMLCoreTestSuite extends TestSuite {
 
 		addTest(ModelParserTests.suite());
 		addTest(new TestSuite(HTMLCorePreferencesTest.class));
+		addTest(new TestSuite(HTMLTagInfoTest.class));
 	}
 }
