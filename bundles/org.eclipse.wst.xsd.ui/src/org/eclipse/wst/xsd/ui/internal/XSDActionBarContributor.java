482e0ce781fb33800eaf0e67576476f619311026
kchong <kchong>
2005-07-12 19:38:31 +0000

    [101529]  Editor Source menu broken - There is no Source menu for the XSD Editor.  This will also fix the Undo/Redo actions.

--- bundles/org.eclipse.wst.xsd.ui/src/org/eclipse/wst/xsd/ui/internal/XSDActionBarContributor.java
+++ bundles/org.eclipse.wst.xsd.ui/src/org/eclipse/wst/xsd/ui/internal/XSDActionBarContributor.java
@@ -23,6 +23,7 @@ import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IEditorActionBarContributor;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchPage;
@@ -32,16 +33,20 @@ import org.eclipse.ui.part.MultiPageEditorActionBarContributor;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.texteditor.ITextEditorActionConstants;
 import org.eclipse.ui.texteditor.RetargetTextEditorAction;
+import org.eclipse.wst.sse.ui.internal.ISourceViewerActionBarContributor;
+import org.eclipse.wst.sse.ui.internal.StructuredTextEditor;
 import org.eclipse.wst.sse.ui.internal.actions.StructuredTextEditorActionConstants;
 import org.eclipse.wst.xsd.ui.internal.actions.ISchemaEditorActionConstants;
 import org.eclipse.wst.xsd.ui.internal.actions.ReloadDependenciesAction;
+import org.eclipse.wst.xsd.ui.internal.actions.SourcePageActionContributor;
 import org.eclipse.wst.xsd.ui.internal.refactor.actions.RefactorActionGroup;
 
 public class XSDActionBarContributor extends MultiPageEditorActionBarContributor
 {
   protected XSDEditor xsdEditor;
   protected XSDTextEditor textEditor;
-
+  protected IEditorActionBarContributor sourceViewerActionContributor = null;
+  
   protected ReloadDependenciesAction reloadDependenciesAction;
   
   protected List fPartListeners= new ArrayList();
@@ -56,6 +61,8 @@ public class XSDActionBarContributor extends MultiPageEditorActionBarContributor
   public XSDActionBarContributor()
   {
     super();
+    
+    sourceViewerActionContributor = new SourcePageActionContributor();
 
     // Reload Dependencies
     reloadDependenciesAction = new ReloadDependenciesAction(XSDEditorPlugin.getXSDString("_UI_MENU_RELOAD_DEPENDENCIES"));
@@ -95,7 +102,35 @@ public class XSDActionBarContributor extends MultiPageEditorActionBarContributor
   public void setActivePage(IEditorPart activeEditor)
   {
     updateActions();
+    
+    if (activeEditor != null && activeEditor instanceof StructuredTextEditor)
+    {
+      activateSourcePage(activeEditor, true);
+    }
+    else
+    {
+      activateSourcePage(xsdEditor, false);
+    }
+
+    IActionBars actionBars = getActionBars();
+    if (actionBars != null) {
+      // update menu bar and tool bar
+      actionBars.updateActionBars();
+    }
+    
+    updateAction(IWorkbenchActionConstants.UNDO, ITextEditorActionConstants.UNDO, true);
+    updateAction(IWorkbenchActionConstants.REDO, ITextEditorActionConstants.REDO, true);
+
     getActionBars().updateActionBars();
+  }
+  
+  protected void activateSourcePage(IEditorPart activeEditor, boolean state)
+  {
+    if (sourceViewerActionContributor != null && sourceViewerActionContributor instanceof ISourceViewerActionBarContributor)
+    {
+      sourceViewerActionContributor.setActiveEditor(activeEditor);
+      ((ISourceViewerActionBarContributor) sourceViewerActionContributor).setViewerSpecificContributionsEnabled(state);
+    }
   }
 
   protected void updateAction(String globalActionId, String textEditorActionId, boolean enable)
@@ -166,8 +201,6 @@ public void addToToolBar(IToolBarManager toolBarManager)
    */
   public void setActiveEditor(IEditorPart targetEditor)
   {
-    super.setActiveEditor(targetEditor);
-
     if (targetEditor instanceof XSDEditor)
     {
       xsdEditor = (XSDEditor) targetEditor;
@@ -176,12 +209,16 @@ public void addToToolBar(IToolBarManager toolBarManager)
       textEditor = ((XSDEditor)targetEditor).getXSDTextEditor();
       if (textEditor != null)
       {      
-      	 
-      	renameElementAction.setAction(getAction(textEditor, ISchemaEditorActionConstants.RETARGET_RENAME_ELEMENT_ACTION_ID));
+         
+        renameElementAction.setAction(getAction(textEditor, ISchemaEditorActionConstants.RETARGET_RENAME_ELEMENT_ACTION_ID));
         updateActions();  
         getActionBars().updateActionBars();
       }
     }
+    super.setActiveEditor(targetEditor);
+    
+    updateAction(IWorkbenchActionConstants.UNDO, ITextEditorActionConstants.UNDO, true);
+    updateAction(IWorkbenchActionConstants.REDO, ITextEditorActionConstants.REDO, true);
   }
 
   public void init(IActionBars bars, IWorkbenchPage page)
@@ -195,7 +232,24 @@ public void addToToolBar(IToolBarManager toolBarManager)
     // register actions that have a dynamic editor. 
 
     bars.setGlobalActionHandler(ISchemaEditorActionConstants.RETARGET_RELOAD_DEPENDENCIES_ACTION_ID, reloadDependenciesAction);
-
+    
+    initSourceViewerActionContributor(bars);
+    
     super.init(bars, page);
   }
+  
+  protected void initSourceViewerActionContributor(IActionBars actionBars) {
+    if (sourceViewerActionContributor != null)
+      sourceViewerActionContributor.init(actionBars, getPage());
+  }
+
+  
+  public void dispose()
+  {
+    super.dispose();
+    
+    if (sourceViewerActionContributor != null)
+      sourceViewerActionContributor.dispose();
+  }
+
 }
