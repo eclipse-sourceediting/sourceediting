9552616664013540f1b3cec8e09e37bf60189311
david_williams <david_williams>
2005-08-12 19:42:03 +0000

    removed accidental inappropriate copyright statements

--- bundles/org.eclipse.wst.xml.core/src/org/w3c/dom/DOMConfiguration.java
+++ bundles/org.eclipse.wst.xml.core/src/org/w3c/dom/DOMConfiguration.java
@@ -1,16 +1,12 @@
 package org.w3c.dom;
 
 public interface DOMConfiguration {
-    public void setParameter(String name, 
-                             Object value)
-                             throws DOMException;
+	public void setParameter(String name, Object value) throws DOMException;
 
-    public Object getParameter(String name)
-                               throws DOMException;
+	public Object getParameter(String name) throws DOMException;
 
-    public boolean canSetParameter(String name, 
-                                   Object value);
+	public boolean canSetParameter(String name, Object value);
 
-    public DOMStringList getParameterNames();
+	public DOMStringList getParameterNames();
 
 }
