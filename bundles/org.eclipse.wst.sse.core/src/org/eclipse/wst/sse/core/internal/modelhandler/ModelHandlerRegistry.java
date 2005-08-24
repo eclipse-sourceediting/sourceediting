bf8c0dd5f2171db4ab4582ad86c6e2aef9028895
nitind <nitind>
2005-08-24 22:24:31 +0000

    cleanup and improve content type arbitration

--- bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/modelhandler/ModelHandlerRegistry.java
+++ bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/modelhandler/ModelHandlerRegistry.java
@@ -12,7 +12,6 @@
  *******************************************************************************/
 package org.eclipse.wst.sse.core.internal.modelhandler;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -22,7 +21,6 @@ import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.content.IContentDescription;
 import org.eclipse.core.runtime.content.IContentType;
-import org.eclipse.core.runtime.content.IContentTypeManager;
 import org.eclipse.wst.sse.core.internal.Logger;
 import org.eclipse.wst.sse.core.internal.encoding.CodedIO;
 import org.eclipse.wst.sse.core.internal.ltk.modelhandler.IModelHandler;
@@ -31,14 +29,8 @@ import org.eclipse.wst.sse.core.internal.util.Utilities;
 
 public class ModelHandlerRegistry {
 	private static ModelHandlerRegistry instance = null;
+	static final String INTERNAL_DEFAULT_EXTENSION = "org.eclipse.wst.xml.core.internal.modelhandler";
 
-	private static IContentTypeManager getContentTypeRegistry() {
-		IContentTypeManager registry = Platform.getContentTypeManager();
-		return registry;
-	}
-
-	/**
-	 */
 	public synchronized static ModelHandlerRegistry getInstance() {
 		if (instance == null) {
 			instance = new ModelHandlerRegistry();
@@ -49,13 +41,7 @@ public class ModelHandlerRegistry {
 	private IModelHandler defaultHandler = null;
 	private ModelHandlerRegistryReader reader = new ModelHandlerRegistryReader();
 
-	/*
-	 * @see ContentTypeRegistry#getTypeFor(String)
-	 */
-	/**
-	 * Constructor for ContentTypeRegistryImpl.
-	 */
-	protected ModelHandlerRegistry() {
+	private ModelHandlerRegistry() {
 		super();
 		reader = new ModelHandlerRegistryReader().readRegistry();
 	}
@@ -65,7 +51,7 @@ public class ModelHandlerRegistry {
 	 * to be sure to get the existing instance, but then we do remember it, so
 	 * subsequent requests will be faster. The first time through, we do check
 	 * the whole list, to be sure there is only one.
-	 *  
+	 * 
 	 */
 	final public IModelHandler getDefault() {
 		if (defaultHandler == null) {
@@ -73,11 +59,14 @@ public class ModelHandlerRegistry {
 			for (int i = 0; i < elements.length; i++) {
 				boolean ofInterest = reader.isElementDefault(elements[i]);
 				if (ofInterest) {
-					// if, here within the search loop we've already found
-					// one defaultHandler, then something is wrong!
+					/*
+					 * If, here within the search loop we've already found one
+					 * defaultHandler, then something is wrong!
+					 */
 					if (defaultHandler == null) {
 						defaultHandler = reader.getInstance(elements[i]);
-					} else {
+					}
+					else {
 						String errorString = "Program or configuration error. More than one default content handler found"; //$NON-NLS-1$
 						Logger.log(Logger.ERROR, errorString);
 						throw new IllegalStateException(errorString);
@@ -94,39 +83,74 @@ public class ModelHandlerRegistry {
 	}
 
 	/**
-	 * Finds the contentTypeDescription based on outcome of the
-	 * ContentTypeDescription's canHandle(IResource) method.
+	 * Finds a ModelHandler based on literal extension id. It's basically a
+	 * "first found first returned". No specific order is guaranteed and the
+	 * uniqueness of IDs is not considered.
 	 * 
+	 * @param extensionId
+	 * @return the given extension, or null
+	 */
+	private IModelHandler getHandlerExtension(String extensionId) {
+		IModelHandler found = null;
+		IConfigurationElement[] elements = reader.elements;
+		if (elements != null) {
+			for (int i = 0; i < elements.length; i++) {
+				String currentId = reader.getId(elements[i]);
+				if (extensionId.equals(currentId)) {
+					IModelHandler item = reader.getInstance(elements[i]);
+					found = item;
+				}
+			}
+		}
+		else {
+			Logger.log(Logger.WARNING_DEBUG, "There were no Model Handler found in registry"); //$NON-NLS-1$
+		}
+		return found;
+	}
+
+	/**
+	 * Finds the registered IModelHandler for a given named file's content
+	 * type.
+	 * 
+	 * @param file
+	 * @return The IModelHandler registered for the content type of the given
+	 *         file. If an exact match is not found, the most-specific match
+	 *         according to IContentType.isKindOf() will be returned. If none
+	 *         are found, either a default or null will be returned.
 	 * @throws CoreException
 	 */
-	public IModelHandler getHandlerFor(IFile iFile) throws CoreException {
+	public IModelHandler getHandlerFor(IFile file) throws CoreException {
 		IModelHandler modelHandler = null;
 		IContentDescription contentDescription = null;
 		IContentType contentType = null;
-		boolean exists = iFile.exists();
-		if (exists) {
-			// try the optimized method first as the description may be cached
-			contentDescription = iFile.getContentDescription();
+		boolean accessible = file.isAccessible();
+		if (accessible) {
+			/* Try the optimized method first as the description may be cached */
+			contentDescription = file.getContentDescription();
 			if (contentDescription != null) {
 				// use the provided description
 				contentType = contentDescription.getContentType();
-			} else {
-				// use the more thorough discovery method to get a description
+			}
+			else {
+				/* use the more thorough discovery method to get a description */
 				InputStream contents = null;
 				try {
-					contents = iFile.getContents(true);
-					contentDescription = Platform.getContentTypeManager().getDescriptionFor(contents, iFile.getName(), IContentDescription.ALL);
+					contents = file.getContents(true);
+					contentDescription = Platform.getContentTypeManager().getDescriptionFor(contents, file.getName(), IContentDescription.ALL);
 					if (contentDescription != null) {
 						contentType = contentDescription.getContentType();
 					}
-				} catch (IOException e) {
+				}
+				catch (IOException e) {
 					// nothing further can be done, but will log for debugging
 					Logger.logException(e);
-				} finally {
+				}
+				finally {
 					if (contents != null) {
 						try {
 							contents.close();
-						} catch (IOException e1) {
+						}
+						catch (IOException e1) {
 							// nothing can be done
 						}
 					}
@@ -134,48 +158,47 @@ public class ModelHandlerRegistry {
 			}
 		}
 
-		// if we couldn't get the content type from a description, try basing
-		// it on the filename
+		/*
+		 * If we couldn't get the content type from a description, try basing
+		 * it on just the filename
+		 */
 		if (contentType == null) {
-			contentType = Platform.getContentTypeManager().findContentTypeFor(iFile.getName());
+			contentType = Platform.getContentTypeManager().findContentTypeFor(file.getName());
 		}
 
 		if (contentType != null) {
 			modelHandler = getHandlerForContentType(contentType);
-		} else {
-			// temp hard coding for null content type
-			modelHandler = getHandlerForID("org.eclipse.wst.sse.core.handler.xml"); //$NON-NLS-1$
+		}
+		else {
+			// hard coding for null content type
+			modelHandler = getHandlerExtension(INTERNAL_DEFAULT_EXTENSION); //$NON-NLS-1$
 		}
 
 		return modelHandler;
 	}
 
+
 	/**
-	 * @see ContentTypeRegistry#add(ContentTypeDescription)
-	 */
-	//	void add(IModelHandler contentTypeDescription) {
-	//		arrayList.add(contentTypeDescription);
-	//	}
-	/*
-	 * @see ContentTypeRegistry#getModelFor(String)
-	 */
-	/**
+	 * Finds the registered IModelHandler for a given named InputStream.
+	 * 
+	 * @param inputName
+	 * @param inputStream
+	 * @return The IModelHandler registered for the content type of the given
+	 *         input. If an exact match is not found, the most-specific match
+	 *         according to IContentType.isKindOf() will be returned. If none
+	 *         are found, either a default or null will be returned.
 	 * @throws IOException
-	 * @see ContentTypeRegistry#getTypeFor(String, InputStream)
 	 */
-	public IModelHandler getHandlerFor(String filename, InputStream inputStream) throws IOException {
+	public IModelHandler getHandlerFor(String inputName, InputStream inputStream) throws IOException {
 		InputStream iStream = Utilities.getMarkSupportedStream(inputStream);
 		IModelHandler modelHandler = null;
 		IContentType contentType = null;
 		if (inputStream != null) {
 			try {
 				iStream.mark(CodedIO.MAX_MARK_SIZE);
-				contentType = getContentTypeRegistry().findContentTypeFor(Utilities.getLimitedStream(iStream), filename);
+				contentType = Platform.getContentTypeManager().findContentTypeFor(Utilities.getLimitedStream(iStream), inputName);
 			}
-			// XXX: Remove when we build with the fix for Eclipse bug #63625
-			catch (FileNotFoundException fnfe) {
-				Logger.logException(fnfe);
-			} finally {
+			finally {
 				if (iStream != null && iStream.markSupported()) {
 					iStream.reset();
 				}
@@ -183,97 +206,90 @@ public class ModelHandlerRegistry {
 
 		}
 		if (contentType == null) {
-			contentType = getContentTypeRegistry().findContentTypeFor(filename);
+			contentType = Platform.getContentTypeManager().findContentTypeFor(inputName);
 		}
-		// if all else failed, try to detect solely on contents; done last for performance reasons
+		// if all else failed, try to detect solely on contents; done last for
+		// performance reasons
 		if (contentType == null) {
-			contentType = getContentTypeRegistry().findContentTypeFor(Utilities.getLimitedStream(iStream), null);
+			contentType = Platform.getContentTypeManager().findContentTypeFor(Utilities.getLimitedStream(iStream), null);
 		}
 		modelHandler = getHandlerForContentType(contentType);
 		return modelHandler;
 	}
 
-
 	/**
-	 * Gets registered modelHandlers for given content type. TODO: eventually
-	 * need to look at contentType's parent types to see if more general type
-	 * can handle (e.g. if we got xsl which was a subtype of xml).
+	 * Finds the registered IModelHandler for a given IContentType.
 	 * 
 	 * @param contentType
-	 * @return
+	 * @return The IModelHandler registered for the given content type. If an
+	 *         exact match is not found, the most-specific match according to
+	 *         IContentType.isKindOf() will be returned. If none are found,
+	 *         either a default or null will be returned.
 	 */
 	private IModelHandler getHandlerForContentType(IContentType contentType) {
-
-		IModelHandler found = null;
-		// temp hard coding for null content type
-		if (contentType == null) {
-			found = getHandlerForID("org.eclipse.wst.sse.core.handler.xml"); //$NON-NLS-1$
-		} else {
-			//String associatedContentTypeId = contentType.getId();
+		IModelHandler handler = null;
+		if (contentType != null) {
+			IConfigurationElement exactContentTypeElement = null;
+			IConfigurationElement kindOfContentTypeElement = null;
+			int kindOfContentTypeDepth = 0;
 			IConfigurationElement[] elements = reader.elements;
 			if (elements != null) {
-				for (int i = 0; i < elements.length; i++) {
+				for (int i = 0; i < elements.length && exactContentTypeElement == null; i++) {
 					String currentId = reader.getAssociatedContentTypeId(elements[i]);
-					IContentType modelContentType = Platform.getContentTypeManager().getContentType(currentId);
-					if (contentType.isKindOf(modelContentType)) {
-						IModelHandler item = reader.getInstance(elements[i]);
-						found = item;
+					IContentType associatedContentType = Platform.getContentTypeManager().getContentType(currentId);
+					if (contentType.equals(associatedContentType)) {
+						exactContentTypeElement = elements[i];
+					}
+					else if (contentType.isKindOf(associatedContentType)) {
+						/*
+						 * Update the kindOfElement variable only if this
+						 * element's content type is "deeper" (depth test
+						 * ensures the first content type is remembered)
+						 */
+						IContentType testContentType = associatedContentType;
+						int testDepth = 0;
+						while (testContentType != null) {
+							testDepth++;
+							testContentType = testContentType.getBaseType();
+						}
+						if (testDepth > kindOfContentTypeDepth) {
+							kindOfContentTypeElement = elements[i];
+							kindOfContentTypeDepth = testDepth;
+						}
 					}
 				}
-			} else {
+			}
+			else {
 				Logger.log(Logger.WARNING_DEBUG, "There were no Model Handler found in registry"); //$NON-NLS-1$
 			}
+			if (exactContentTypeElement != null) {
+				handler = reader.getInstance(exactContentTypeElement);
+			}
+			else if (kindOfContentTypeElement != null) {
+				handler = reader.getInstance(kindOfContentTypeElement);
+			}
 		}
-		return found;
+
+		if (handler == null) {
+			// temp hard coding for null content type arguments
+			handler = getHandlerExtension(INTERNAL_DEFAULT_EXTENSION); //$NON-NLS-1$
+		}
+		return handler;
 	}
 
 	/**
-	 * Finds the ModelHandler based on literal content type id. This should
-	 * not normally be needed, in is in cases when a model needs to be
-	 * created, and there is no resource. Its basically a "first found first
-	 * returned". Note the order is fairly unpredictable, so non-unique ids
-	 * would cause problems, and are not checked.
+	 * Finds the registered IModelHandler for a given content type ID. No
+	 * specific order is guaranteed and the uniqueness of IDs is not
+	 * considered.
+	 * 
+	 * @param contentType
+	 * @return The IModelHandler registered for the given content type ID. If
+	 *         an exact match is not found, the most-specific match according
+	 *         to IContentType.isKindOf() will be returned. If none are found,
+	 *         either a default or null will be returned.
 	 */
 	public IModelHandler getHandlerForContentTypeId(String contentTypeId) {
 		IContentType contentType = Platform.getContentTypeManager().getContentType(contentTypeId);
 		return getHandlerForContentType(contentType);
-		//		IModelHandler found = null;
-		//		IConfigurationElement[] elements = reader.elements;
-		//		if (elements != null) {
-		//			for (int i = 0; i < elements.length; i++) {
-		//				String currentId = reader.getAssociatedContentTypeId(elements[i]);
-		//				if (contentTypeId.equals(currentId)) {
-		//					IModelHandler item = reader.getInstance(elements[i]);
-		//					found = item;
-		//				}
-		//			}
-		//		}
-		//		else {
-		//			Logger.log(Logger.WARNING_DEBUG, "There were no Model Handler found
-		// in registry");
-		//		}
-		//		return found;
-	}
-
-	/**
-	 * Finds the ModelHandler based on literal id. Its basically a "first
-	 * found first returned". Note the order is fairly unpredictable, so
-	 * non-unique ids would cause problems, and are not checked.
-	 */
-	private IModelHandler getHandlerForID(String modelId) {
-		IModelHandler found = null;
-		IConfigurationElement[] elements = reader.elements;
-		if (elements != null) {
-			for (int i = 0; i < elements.length; i++) {
-				String currentId = reader.getId(elements[i]);
-				if (modelId.equals(currentId)) {
-					IModelHandler item = reader.getInstance(elements[i]);
-					found = item;
-				}
-			}
-		} else {
-			Logger.log(Logger.WARNING_DEBUG, "There were no Model Handler found in registry"); //$NON-NLS-1$
-		}
-		return found;
 	}
 }
