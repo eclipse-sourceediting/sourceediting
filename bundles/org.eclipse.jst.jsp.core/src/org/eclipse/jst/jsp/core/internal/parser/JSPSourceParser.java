30582d759a675f276dc22dcedcf8384a7bef0eb6
david_williams <david_williams>
2005-08-30 04:41:06 +0000

    [108325] IllegalArgumentException when using webapp relative taglib locations

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/parser/JSPSourceParser.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/parser/JSPSourceParser.java
@@ -13,6 +13,7 @@ package org.eclipse.jst.jsp.core.internal.parser;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.jst.jsp.core.internal.Logger;
 import org.eclipse.jst.jsp.core.internal.contentmodel.tld.provisional.JSP12TLDNames;
 import org.eclipse.jst.jsp.core.internal.parser.internal.JSPTokenizer;
 import org.eclipse.jst.jsp.core.internal.provisional.JSP12Namespace;
@@ -91,7 +92,7 @@ public class JSPSourceParser extends XMLSourceParser implements JSPCapableParser
 					}
 				}
 				catch (StringIndexOutOfBoundsException sioobExc) {
-					//do nothing
+					// do nothing
 				}
 			}
 			// could test > 1, but since we only care if there are 5 (<,
@@ -227,32 +228,67 @@ public class JSPSourceParser extends XMLSourceParser implements JSPCapableParser
 		// they are adjusted here to be indexes from the currentNode's start
 		// offset
 		IStructuredDocumentRegion headNode = null;
-		IStructuredDocumentRegion lastNode = null;
-		ITextRegion region = null;
-		// DMW: 2/12/03. Made current node local variable, since
-		// we changed class to not require state
-		IStructuredDocumentRegion currentNode = null;
-		String type = null;
+		try {
+			IStructuredDocumentRegion lastNode = null;
+			ITextRegion region = null;
+			// DMW: 2/12/03. Made current node local variable, since
+			// we changed class to not require state
+			IStructuredDocumentRegion currentNode = null;
+			String type = null;
 
-		while ((region = getNextRegion()) != null) {
-			type = region.getType();
-			// these types (might) demand a IStructuredDocumentRegion for each
-			// of them
-			if (type == DOMRegionContext.BLOCK_TEXT) {
-				if (currentNode != null && currentNode.getLastRegion().getType() == DOMRegionContext.BLOCK_TEXT) {
-					// multiple block texts indicated embedded containers; no
-					// new IStructuredDocumentRegion
-					currentNode.addRegion(region);
-					currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
-					region.adjustStart(-currentNode.getStart());
-					// DW, 4/16/2003 token regions no longer have parents
-					//region.setParent(currentNode);
-					if (region instanceof ITextRegionContainer) {
-						((ITextRegionContainer) region).setParent(currentNode);
+			while ((region = getNextRegion()) != null) {
+				type = region.getType();
+				// these types (might) demand a IStructuredDocumentRegion for
+				// each
+				// of them
+				if (type == DOMRegionContext.BLOCK_TEXT) {
+					if (currentNode != null && currentNode.getLastRegion().getType() == DOMRegionContext.BLOCK_TEXT) {
+						// multiple block texts indicated embedded containers;
+						// no
+						// new IStructuredDocumentRegion
+						currentNode.addRegion(region);
+						currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
+						region.adjustStart(-currentNode.getStart());
+						// DW, 4/16/2003 token regions no longer have parents
+						// region.setParent(currentNode);
+						if (region instanceof ITextRegionContainer) {
+							((ITextRegionContainer) region).setParent(currentNode);
+						}
+					}
+					else {
+						// not continuing a IStructuredDocumentRegion
+						if (currentNode != null) {
+							// ensure that any existing node is at least
+							// terminated
+							if (!currentNode.isEnded()) {
+								currentNode.setLength(region.getStart() - currentNode.getStart());
+								// fCurrentNode.setTextLength(region.getStart()
+								// -
+								// fCurrentNode.getStart());
+							}
+							lastNode = currentNode;
+						}
+						fireNodeParsed(currentNode);
+						currentNode = createStructuredDocumentRegion(type);
+						if (lastNode != null) {
+							lastNode.setNext(currentNode);
+						}
+						currentNode.setPrevious(lastNode);
+						currentNode.setStart(region.getStart());
+						currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
+						currentNode.setEnded(true);
+						region.adjustStart(-currentNode.getStart());
+						currentNode.addRegion(region);
+						// DW, 4/16/2003 token regions no longer have parents
+						// region.setParent(currentNode);
+						if (region instanceof ITextRegionContainer) {
+							((ITextRegionContainer) region).setParent(currentNode);
+						}
+
 					}
 				}
-				else {
-					// not continuing a IStructuredDocumentRegion
+				// the following contexts OPEN new StructuredDocumentRegions
+				else if ((currentNode != null && currentNode.isEnded()) || (type == DOMRegionContext.XML_CONTENT) || (type == DOMRegionContext.XML_CHAR_REFERENCE) || (type == DOMRegionContext.XML_ENTITY_REFERENCE) || (type == DOMRegionContext.XML_PI_OPEN) || (type == DOMRegionContext.XML_TAG_OPEN) || (type == DOMRegionContext.XML_END_TAG_OPEN) || (type == DOMRegionContext.XML_COMMENT_OPEN) || (type == DOMRegionContext.XML_CDATA_OPEN) || (type == DOMRegionContext.XML_DECLARATION_OPEN) || (type == DOMJSPRegionContexts.JSP_COMMENT_OPEN) || (type == DOMJSPRegionContexts.JSP_DECLARATION_OPEN) || (type == DOMJSPRegionContexts.JSP_DIRECTIVE_OPEN) || (type == DOMJSPRegionContexts.JSP_EXPRESSION_OPEN) || (type == DOMJSPRegionContexts.JSP_SCRIPTLET_OPEN) || (type == DOMJSPRegionContexts.JSP_CLOSE) || type == DOMJSPRegionContexts.JSP_EL_OPEN) {
 					if (currentNode != null) {
 						// ensure that any existing node is at least
 						// terminated
@@ -270,154 +306,124 @@ public class JSPSourceParser extends XMLSourceParser implements JSPCapableParser
 					}
 					currentNode.setPrevious(lastNode);
 					currentNode.setStart(region.getStart());
-					currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
-					currentNode.setEnded(true);
-					region.adjustStart(-currentNode.getStart());
 					currentNode.addRegion(region);
+					currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
+					region.adjustStart(-currentNode.getStart());
 					// DW, 4/16/2003 token regions no longer have parents
-					//region.setParent(currentNode);
+					// region.setParent(currentNode);
 					if (region instanceof ITextRegionContainer) {
 						((ITextRegionContainer) region).setParent(currentNode);
 					}
 
 				}
-			}
-			// the following contexts OPEN new StructuredDocumentRegions
-			else if ((currentNode != null && currentNode.isEnded()) || (type == DOMRegionContext.XML_CONTENT) || (type == DOMRegionContext.XML_CHAR_REFERENCE) || (type == DOMRegionContext.XML_ENTITY_REFERENCE)
-						|| (type == DOMRegionContext.XML_PI_OPEN) || (type == DOMRegionContext.XML_TAG_OPEN) || (type == DOMRegionContext.XML_END_TAG_OPEN) || (type == DOMRegionContext.XML_COMMENT_OPEN) || (type == DOMRegionContext.XML_CDATA_OPEN)
-						|| (type == DOMRegionContext.XML_DECLARATION_OPEN) || (type == DOMJSPRegionContexts.JSP_COMMENT_OPEN) || (type == DOMJSPRegionContexts.JSP_DECLARATION_OPEN) || (type == DOMJSPRegionContexts.JSP_DIRECTIVE_OPEN)
-						|| (type == DOMJSPRegionContexts.JSP_EXPRESSION_OPEN) || (type == DOMJSPRegionContexts.JSP_SCRIPTLET_OPEN) || (type == DOMJSPRegionContexts.JSP_CLOSE) || type == DOMJSPRegionContexts.JSP_EL_OPEN) {
-				if (currentNode != null) {
-					// ensure that any existing node is at least terminated
-					if (!currentNode.isEnded()) {
-						currentNode.setLength(region.getStart() - currentNode.getStart());
-						// fCurrentNode.setTextLength(region.getStart() -
-						// fCurrentNode.getStart());
-					}
-					lastNode = currentNode;
-				}
-				fireNodeParsed(currentNode);
-				currentNode = createStructuredDocumentRegion(type);
-				if (lastNode != null) {
-					lastNode.setNext(currentNode);
-				}
-				currentNode.setPrevious(lastNode);
-				currentNode.setStart(region.getStart());
-				currentNode.addRegion(region);
-				currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
-				region.adjustStart(-currentNode.getStart());
-				// DW, 4/16/2003 token regions no longer have parents
-				//region.setParent(currentNode);
-				if (region instanceof ITextRegionContainer) {
-					((ITextRegionContainer) region).setParent(currentNode);
-				}
-
-			}
-			// the following contexts NEITHER open nor close
-			// StructuredDocumentRegions; just add to them
-			else if ((type == DOMRegionContext.XML_TAG_NAME) || (type == DOMRegionContext.XML_TAG_ATTRIBUTE_NAME) || (type == DOMRegionContext.XML_TAG_ATTRIBUTE_EQUALS) || (type == DOMRegionContext.XML_TAG_ATTRIBUTE_VALUE)
-						|| (type == DOMRegionContext.XML_COMMENT_TEXT) || (type == DOMRegionContext.XML_PI_CONTENT) || (type == DOMRegionContext.XML_DOCTYPE_INTERNAL_SUBSET) || (type == DOMJSPRegionContexts.JSP_COMMENT_TEXT)
-						|| (type == DOMJSPRegionContexts.JSP_ROOT_TAG_NAME) || (type == DOMJSPRegionContexts.JSP_DIRECTIVE_NAME) || type == DOMJSPRegionContexts.JSP_EL_CONTENT) {
-				currentNode.addRegion(region);
-				currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
-				region.adjustStart(-currentNode.getStart());
-				// DW, 4/16/2003 token regions no longer have parents
-				//region.setParent(currentNode);
-				if (region instanceof ITextRegionContainer) {
-					((ITextRegionContainer) region).setParent(currentNode);
-				}
-			}
-			// the following contexts close off StructuredDocumentRegions
-			// cleanly
-			else if ((type == DOMRegionContext.XML_PI_CLOSE) || (type == DOMRegionContext.XML_TAG_CLOSE) || (type == DOMRegionContext.XML_EMPTY_TAG_CLOSE) || (type == DOMRegionContext.XML_COMMENT_CLOSE) || (type == DOMRegionContext.XML_CDATA_CLOSE)
-						|| (type == DOMJSPRegionContexts.JSP_CLOSE) || (type == DOMJSPRegionContexts.JSP_COMMENT_CLOSE) || (type == DOMJSPRegionContexts.JSP_DIRECTIVE_CLOSE) || (type == DOMRegionContext.XML_DECLARATION_CLOSE)
-						|| type == DOMJSPRegionContexts.JSP_EL_CLOSE) {
-				currentNode.setEnded(true);
-				currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
-				currentNode.addRegion(region);
-				region.adjustStart(-currentNode.getStart());
-				// DW, 4/16/2003 token regions no longer have parents
-				//region.setParent(currentNode);
-				if (region instanceof ITextRegionContainer) {
-					((ITextRegionContainer) region).setParent(currentNode);
-				}
-			}
-			// this is extremely rare, but valid
-			else if (type == DOMRegionContext.WHITE_SPACE) {
-				ITextRegion lastRegion = currentNode.getLastRegion();
-				// pack the embedded container with this region
-				if (lastRegion instanceof ITextRegionContainer) {
-					ITextRegionContainer container = (ITextRegionContainer) lastRegion;
-					container.getRegions().add(region);
-					// DW, 4/16/2003 container regions have parent. Probably a
-					// better place to set,
-					// but for now, will (re)set each time through
-					container.setParent(currentNode);
-					// DW, 4/16/2003 token regions no longer have parents
-					//region.setParent(container);
-					region.adjustStart(container.getLength() - region.getStart());
-				}
-				currentNode.getLastRegion().adjustLength(region.getLength());
-				currentNode.adjustLength(region.getLength());
-			}
-			else if (type == DOMRegionContext.UNDEFINED && currentNode != null) {
-				// skip on a very-first region situation as the default
-				// behavior is good enough
-				// combine with previous if also undefined
-				if (currentNode.getLastRegion() != null && currentNode.getLastRegion().getType() == DOMRegionContext.UNDEFINED) {
-					currentNode.getLastRegion().adjustLength(region.getLength());
-					currentNode.adjustLength(region.getLength());
-				}
-				// previous wasn't undefined
-				else {
+				// the following contexts NEITHER open nor close
+				// StructuredDocumentRegions; just add to them
+				else if ((type == DOMRegionContext.XML_TAG_NAME) || (type == DOMRegionContext.XML_TAG_ATTRIBUTE_NAME) || (type == DOMRegionContext.XML_TAG_ATTRIBUTE_EQUALS) || (type == DOMRegionContext.XML_TAG_ATTRIBUTE_VALUE) || (type == DOMRegionContext.XML_COMMENT_TEXT) || (type == DOMRegionContext.XML_PI_CONTENT) || (type == DOMRegionContext.XML_DOCTYPE_INTERNAL_SUBSET) || (type == DOMJSPRegionContexts.JSP_COMMENT_TEXT) || (type == DOMJSPRegionContexts.JSP_ROOT_TAG_NAME) || (type == DOMJSPRegionContexts.JSP_DIRECTIVE_NAME) || type == DOMJSPRegionContexts.JSP_EL_CONTENT) {
 					currentNode.addRegion(region);
 					currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
 					region.adjustStart(-currentNode.getStart());
+					// DW, 4/16/2003 token regions no longer have parents
+					// region.setParent(currentNode);
+					if (region instanceof ITextRegionContainer) {
+						((ITextRegionContainer) region).setParent(currentNode);
+					}
 				}
-				if (region instanceof ITextRegionContainer) {
-					((ITextRegionContainer) region).setParent(currentNode);
+				// the following contexts close off StructuredDocumentRegions
+				// cleanly
+				else if ((type == DOMRegionContext.XML_PI_CLOSE) || (type == DOMRegionContext.XML_TAG_CLOSE) || (type == DOMRegionContext.XML_EMPTY_TAG_CLOSE) || (type == DOMRegionContext.XML_COMMENT_CLOSE) || (type == DOMRegionContext.XML_CDATA_CLOSE) || (type == DOMJSPRegionContexts.JSP_CLOSE) || (type == DOMJSPRegionContexts.JSP_COMMENT_CLOSE) || (type == DOMJSPRegionContexts.JSP_DIRECTIVE_CLOSE) || (type == DOMRegionContext.XML_DECLARATION_CLOSE) || type == DOMJSPRegionContexts.JSP_EL_CLOSE) {
+					currentNode.setEnded(true);
+					currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
+					currentNode.addRegion(region);
+					region.adjustStart(-currentNode.getStart());
+					// DW, 4/16/2003 token regions no longer have parents
+					// region.setParent(currentNode);
+					if (region instanceof ITextRegionContainer) {
+						((ITextRegionContainer) region).setParent(currentNode);
+					}
 				}
-			}
-			else {
-				// if an unknown type is the first region in the document,
-				// ensure that a node exists
-				if (currentNode == null) {
-					currentNode = createStructuredDocumentRegion(type);
-					currentNode.setStart(region.getStart());
+				// this is extremely rare, but valid
+				else if (type == DOMRegionContext.WHITE_SPACE) {
+					ITextRegion lastRegion = currentNode.getLastRegion();
+					// pack the embedded container with this region
+					if (lastRegion instanceof ITextRegionContainer) {
+						ITextRegionContainer container = (ITextRegionContainer) lastRegion;
+						container.getRegions().add(region);
+						// DW, 4/16/2003 container regions have parent.
+						// Probably a
+						// better place to set,
+						// but for now, will (re)set each time through
+						container.setParent(currentNode);
+						// DW, 4/16/2003 token regions no longer have parents
+						// region.setParent(container);
+						region.adjustStart(container.getLength() - region.getStart());
+					}
+					currentNode.getLastRegion().adjustLength(region.getLength());
+					currentNode.adjustLength(region.getLength());
 				}
-				currentNode.addRegion(region);
-				currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
-				region.adjustStart(-currentNode.getStart());
-				// DW, 4/16/2003 token regions no longer have parents
-				//region.setParent(currentNode);
-				if (region instanceof ITextRegionContainer) {
-					((ITextRegionContainer) region).setParent(currentNode);
+				else if (type == DOMRegionContext.UNDEFINED && currentNode != null) {
+					// skip on a very-first region situation as the default
+					// behavior is good enough
+					// combine with previous if also undefined
+					if (currentNode.getLastRegion() != null && currentNode.getLastRegion().getType() == DOMRegionContext.UNDEFINED) {
+						currentNode.getLastRegion().adjustLength(region.getLength());
+						currentNode.adjustLength(region.getLength());
+					}
+					// previous wasn't undefined
+					else {
+						currentNode.addRegion(region);
+						currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
+						region.adjustStart(-currentNode.getStart());
+					}
+					if (region instanceof ITextRegionContainer) {
+						((ITextRegionContainer) region).setParent(currentNode);
+					}
+				}
+				else {
+					// if an unknown type is the first region in the document,
+					// ensure that a node exists
+					if (currentNode == null) {
+						currentNode = createStructuredDocumentRegion(type);
+						currentNode.setStart(region.getStart());
+					}
+					currentNode.addRegion(region);
+					currentNode.setLength(region.getStart() + region.getLength() - currentNode.getStart());
+					region.adjustStart(-currentNode.getStart());
+					// DW, 4/16/2003 token regions no longer have parents
+					// region.setParent(currentNode);
+					if (region instanceof ITextRegionContainer) {
+						((ITextRegionContainer) region).setParent(currentNode);
+					}
+
+					if (Debug.debugTokenizer)
+						System.out.println(getClass().getName() + " found region of not specifically handled type " + region.getType() + " @ " + region.getStart() + "[" + region.getLength() + "]");//$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
+
 				}
 
-				if (Debug.debugTokenizer)
-					System.out.println(getClass().getName() + " found region of not specifically handled type " + region.getType() + " @ " + region.getStart() + "[" + region.getLength() + "]");//$NON-NLS-4$//$NON-NLS-3$//$NON-NLS-2$//$NON-NLS-1$
-
+				// these regions also get their own node, so close them
+				// cleanly
+				// NOTE: these regions have new StructuredDocumentRegions
+				// created
+				// for them above; it may
+				// be more readable if that is handled here as well, but the
+				// current layout
+				// ensures that they open StructuredDocumentRegions the same
+				// way
+				if ((type == DOMRegionContext.XML_CONTENT) || (type == DOMRegionContext.XML_CHAR_REFERENCE) || (type == DOMRegionContext.XML_ENTITY_REFERENCE) || (type == DOMJSPRegionContexts.JSP_DECLARATION_OPEN) || (type == DOMJSPRegionContexts.JSP_EXPRESSION_OPEN) || (type == DOMJSPRegionContexts.JSP_SCRIPTLET_OPEN) || (type == DOMJSPRegionContexts.JSP_CONTENT) || (type == DOMJSPRegionContexts.JSP_CLOSE)) {
+					currentNode.setEnded(true);
+				}
+				if (headNode == null && currentNode != null) {
+					headNode = currentNode;
+				}
 			}
-
-			// these regions also get their own node, so close them cleanly
-			// NOTE: these regions have new StructuredDocumentRegions created
-			// for them above; it may
-			// be more readable if that is handled here as well, but the
-			// current layout
-			// ensures that they open StructuredDocumentRegions the same way
-			if ((type == DOMRegionContext.XML_CONTENT) || (type == DOMRegionContext.XML_CHAR_REFERENCE) || (type == DOMRegionContext.XML_ENTITY_REFERENCE) || (type == DOMJSPRegionContexts.JSP_DECLARATION_OPEN)
-						|| (type == DOMJSPRegionContexts.JSP_EXPRESSION_OPEN) || (type == DOMJSPRegionContexts.JSP_SCRIPTLET_OPEN) || (type == DOMJSPRegionContexts.JSP_CONTENT) || (type == DOMJSPRegionContexts.JSP_CLOSE)) {
-				currentNode.setEnded(true);
+			if (currentNode != null) {
+				fireNodeParsed(currentNode);
+				currentNode.setPrevious(lastNode);
 			}
-			if (headNode == null && currentNode != null) {
-				headNode = currentNode;
-			}
+			primReset();
 		}
-		if (currentNode != null) {
-			fireNodeParsed(currentNode);
-			currentNode.setPrevious(lastNode);
+		catch (Exception e) {
+			Logger.log(Logger.ERROR, "Parser error: " + e.getMessage());
 		}
-		primReset();
 		return headNode;
 	}
 
