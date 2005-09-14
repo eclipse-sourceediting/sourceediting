7761e4f02de9736b71032d6e1db7510d3a0e2086
pavery <pavery>
2005-09-14 18:41:28 +0000

    *** empty log message ***

--- tests/org.eclipse.wst.css.ui.tests/src/org/eclipse/wst/css/ui/tests/ExistenceTest.java
+++ tests/org.eclipse.wst.css.ui.tests/src/org/eclipse/wst/css/ui/tests/ExistenceTest.java
@@ -5,7 +5,14 @@ import junit.framework.TestCase;
 import org.eclipse.core.runtime.Plugin;
 import org.eclipse.wst.css.ui.internal.CSSUIPlugin;
 
+/**
+ * 
+ * @since 1.0
+ */
 public class ExistenceTest extends TestCase {
+	/**
+	 * tests if CSSUIPlugin can be loaded
+	 */
 	public void testExists() {
 		Plugin p = CSSUIPlugin.getDefault();
 		assertNotNull("couldn't load CSS UI plugin", p);
