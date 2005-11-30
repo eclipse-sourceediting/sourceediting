7416cdb7c7c6f71d2be8ea01b5f648ad905dfb6d
kchong <kchong>
2005-11-30 22:22:22 +0000

    [113762] Incorrect copyright statements in o.e.wst.xsd.*

--- bundles/org.eclipse.wst.xsd.ui/src/org/eclipse/wst/xsd/ui/internal/text/XSDModelQueryExtension.java
+++ bundles/org.eclipse.wst.xsd.ui/src/org/eclipse/wst/xsd/ui/internal/text/XSDModelQueryExtension.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - Initial API and implementation
+ *******************************************************************************/
 package org.eclipse.wst.xsd.ui.internal.text;
 
 import java.util.ArrayList;
@@ -17,15 +27,6 @@ public class XSDModelQueryExtension extends ModelQueryExtension
   public XSDModelQueryExtension()
   {
   }
-  
-  //public boolean isApplicableChildElement(Node parentNode, String namespace, String name)
-  //{
-  //  if ("any".equals(name) || "choice".equals(name) || "group".equals(name))
-  //  {
-  //    return false;
-  //  }
-  //  return true;
-  //}
   
   public String[] getAttributeValues(Element e, String namespace, String name)
   {
