77e8174409885a8210f8e5ee09447291da3e209b
pavery <pavery>
2005-12-01 21:55:46 +0000

    [116523] modified test to recreate the NPE

--- tests/org.eclipse.jst.jsp.ui.tests/src/org/eclipse/jst/jsp/ui/tests/threaded/ThreadedModelReadEditTest.java
+++ tests/org.eclipse.jst.jsp.ui.tests/src/org/eclipse/jst/jsp/ui/tests/threaded/ThreadedModelReadEditTest.java
@@ -1,4 +1,4 @@
-package org.eclipse.jst.jsp.ui.tests.indexing;
+package org.eclipse.jst.jsp.ui.tests.threaded;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -15,52 +15,135 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jst.jsp.core.internal.java.JSPTranslator;
 import org.eclipse.jst.jsp.ui.tests.util.ProjectUtil;
 import org.eclipse.text.edits.InsertEdit;
 import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
 import org.eclipse.wst.sse.core.internal.provisional.IndexedRegion;
 import org.eclipse.wst.sse.core.internal.provisional.StructuredModelManager;
+import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 /**
- * Tests thread safety of JSP indexing.
  * Created for: https://bugs.eclipse.org/bugs/show_bug.cgi?id=116523
- * 
  */
-public class JspIndexingTest extends TestCase {
+public class ThreadedModelReadEditTest extends TestCase {
 
 	private final String PROJECT_NAME = "PROJECT_116523";
-
-	public void test_116523() {
-
-		createProject();
-		
-		// try and do some concurrent editing/indexing
-		for(int j=0; j<5; j++) {
-			for(int i=0; i<100; i++) 
-				doTest("test_" + i + ".jsp");
+	private static boolean isSetup = false;
+	
+	private class TranslateJob extends Job {
+		String fFilePath = null;
+		public TranslateJob(String filename, String jobname) {
+			super(jobname);
+			fFilePath = filename;
+		}
+		protected IStatus run(IProgressMonitor monitor) {
+			IFile f = getOrCreateFile(fFilePath);
+			JSPTranslator trans = new JSPTranslator();
+			IStructuredModel sModel = null;
+			try {
+				sModel = StructuredModelManager.getModelManager().getModelForRead(f);
+				if(sModel != null) {
+					trans.reset((IDOMNode)sModel.getIndexedRegion(0), new NullProgressMonitor());
+					trans.translate();
+				}
+				
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (CoreException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally {
+				if(sModel != null)
+					sModel.releaseFromRead();
+			}
+			
+			return Status.OK_STATUS;
 		}
 	}
 	
-	private void doTest(String filePath) {
+	protected void setUp() throws Exception {
+		super.setUp();
+		if(!isSetup) {
+			doSetup();
+			isSetup = true;
+		}
+	}
+	
+	private void doSetup() {
+		createProject();
+	}
+	/*
+	 * create 100 test files
+	 * repeat doTest() on those 100 files 5x
+	 */
+//	public void test_116523() {
+//
+//		// try and do some concurrent editing/indexing
+//		for(int j=0; j<5; j++) {
+//			for(int i=0; i<100; i++) 
+//				doTest("test_" + i + ".jsp");
+//		}
+//	}
+//	/*
+//	 * doTest() on same file 100x
+//	 */
+//	public void test_116523_2() {
+//
+//		// try and do some concurrent editing/indexing
+//		for(int i=0; i<100; i++) 
+//			doTest("test_1.jsp");
+//	}
+	
+	/*
+	 * doTest() (insert text & edit dom)
+	 * and force JSPTranslation on another thread
+	 * 
+	 * repeat lots of times
+	 */
+	public void test_116523_3() {
+		String filename = "t_test_1.jsp";
+		
+		getOrCreateFile(PROJECT_NAME + "/" + filename);
+		
+		for(int j=0; j<100; j++) {
+			TranslateJob translateJob = new TranslateJob(PROJECT_NAME + "/" + filename, "JSP Translate Job");
+			translateJob.schedule();
+			performEdits(filename);
+		}
+	}
+	
+	private void performEdits(String filePath) {
 		
 		IFile blankJspFile = getOrCreateFile(PROJECT_NAME + "/" + filePath);
 		
-		fileBufferInsert(blankJspFile,  "<logic:iterate id=\"iterateID\" type=\"java.util.List\" indexId=\"indexID\"></logic:iterate>\n",0);
+		// document edit
+		iDocumentInsert(blankJspFile,  "<logic:iterate id=\"iterateID\" type=\"java.util.List\" indexId=\"indexID\"></logic:iterate>\n",0);
+		
+		// dom edit
 		HashMap attrs = new HashMap();
 		attrs.put("uri", "struts-logic.tld");
 		attrs.put("prefix", "logic");
 		domInsertElement(blankJspFile, "jsp:directive.taglib", attrs, 0);
 	}
 	
-	private IFile getOrCreateFile(String filePath) {
+	protected IFile getOrCreateFile(String filePath) {
 		IFile blankJspFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(filePath));
 		if (blankJspFile != null && !blankJspFile.exists()) {
 			try {
@@ -111,7 +194,7 @@ public class JspIndexingTest extends TestCase {
 		// System.out.println("inserted element, dom style");
 	}
 	
-	public void fileBufferInsert(IFile file, String text, int insertionPoint) {
+	public void iDocumentInsert(IFile file, String text, int insertionPoint) {
 		
 		// get the file's text buffer
 		ITextFileBufferManager bufferManager = FileBuffers.getTextFileBufferManager();
