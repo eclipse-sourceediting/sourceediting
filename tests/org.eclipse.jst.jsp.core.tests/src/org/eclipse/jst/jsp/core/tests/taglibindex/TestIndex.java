8ce06e111b6eb577207adf3f8049c0daa2e07223
nitind <nitind>
2005-12-09 16:18:38 +0000

    add testcase with whitespace in folder name for bug 118251

--- tests/org.eclipse.jst.jsp.core.tests/src/org/eclipse/jst/jsp/core/tests/taglibindex/TestIndex.java
+++ tests/org.eclipse.jst.jsp.core.tests/src/org/eclipse/jst/jsp/core/tests/taglibindex/TestIndex.java
@@ -123,5 +123,62 @@ public class TestIndex extends TestCase {
 
 		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-f/WebContent/"));
 		assertEquals("wrong number of taglib records found after copying", 4, records.length);
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-f/WebContent"));
+		assertEquals("wrong number of taglib records found after copying", 4, records.length);
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-f/WebContent/WEB-INF"));
+		assertEquals("wrong number of taglib records found after copying", 4, records.length);
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-f/WebContent/WEB-INF/web.xml"));
+		assertEquals("wrong number of taglib records found after copying", 4, records.length);
+	}
+
+	public void testBug118251_g() throws Exception {
+		// Create new project
+		IProject project = BundleResourceUtil.createSimpleProject("bug_118251-g", null, null);
+		assertTrue(project.exists());
+		ITaglibRecord[] records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-g"));
+		assertEquals("wrong number of taglib records found before unpacking", 0, records.length);
+
+		BundleResourceUtil.copyBundleEntriesIntoWorkspace("/testfiles/bug_118251-g", "/bug_118251-g");
+		// bug_118251-g/Web Content/WEB-INF/web.xml
+		// bug_118251-g/Web Content/WEB-INF/tld/sample2_for_118251-e.tld
+		// bug_118251-g/Web Content/META-INF/MANIFEST.MF
+		// bug_118251-g/Web Content/test1.jsp
+		// bug_118251-g/.classpath
+		// bug_118251-g/.project
+		String url = "http://example.com/sample-taglib";
+		ITaglibRecord taglibRecord = TaglibIndex.resolve("/bug_118251-g/Web Content/test1.jsp", url, false);
+		assertNull("record found for " + url, taglibRecord);
+
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-g/Web Content/"));
+		assertEquals("wrong number of taglib records found after unpacking but before copying", 3, records.length);
+
+		/*
+		 * increase by <b>one</b> for the URL to the TLD in the jar (one
+		 * implicit for the TLD in the jar as a resource and another implicit
+		 * overwriting it with the same URL to the TLD in the jar on the
+		 * classpath)
+		 */
+		BundleResourceUtil.copyBundleEntryIntoWorkspace("/testfiles/bug_118251-sample/sample_tld.jar", "/bug_118251-g/Web Content/WEB-INF/lib/sample_tld.jar");
+
+		taglibRecord = TaglibIndex.resolve("/bug_118251-g/Web Content/test1.jsp", url, false);
+		assertNotNull("no record found for " + url, taglibRecord);
+		assertTrue("record found was wrong type", taglibRecord instanceof IURLRecord);
+		assertNotNull("record has no base location", ((IURLRecord) taglibRecord).getBaseLocation());
+		assertEquals("record has wrong short name", "sample", ((IURLRecord) taglibRecord).getShortName());
+		assertEquals("record has wrong URI", url, ((IURLRecord) taglibRecord).getURI());
+		URL recordURL = ((IURLRecord) taglibRecord).getURL();
+		assertNotNull("record has no URL", recordURL);
+		assertTrue("record has wrong URL", recordURL.toString().length() > 4);
+		assertEquals("record has wrong URL protocol", "jar:", recordURL.toString().substring(0, 4));
+		assertEquals("record has wrong URL", "/bug_118251-g/Web Content/WEB-INF/lib/sample_tld.jar!/folder/sample_for_118251.tld", recordURL.toString().substring(recordURL.toString().length() - 82));
+
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-g/Web Content/"));
+		assertEquals("wrong number of taglib records found after copying", 4, records.length);
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-g/Web Content"));
+		assertEquals("wrong number of taglib records found after copying", 4, records.length);
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-g/Web Content/WEB-INF"));
+		assertEquals("wrong number of taglib records found after copying", 4, records.length);
+		records = TaglibIndex.getAvailableTaglibRecords(new Path("/bug_118251-g/Web Content/WEB-INF/web.xml"));
+		assertEquals("wrong number of taglib records found after copying", 4, records.length);
 	}
 }
