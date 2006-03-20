c08a9b1f8cca7fe24522911de9c27cab17a56e0a
kchong <kchong>
2006-03-20 15:06:55 +0000

    Update Commands

---
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/common/commands/RemoveAppInfoCommand.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.xsd.ui.common.commands;
+
+import org.eclipse.gef.commands.Command;
+import org.eclipse.xsd.XSDAnnotation;
+import org.w3c.dom.Node;
+
+public class RemoveAppInfoCommand extends Command
+{
+  XSDAnnotation xsdAnnotation;
+  Node appInfo;
+
+  public RemoveAppInfoCommand(String label, XSDAnnotation xsdAnnotation, Node appInfo)
+  {
+    super(label);
+    this.xsdAnnotation = xsdAnnotation;
+    this.appInfo = appInfo;
+  }
+
+  public void execute()
+  {
+    super.execute();
+    xsdAnnotation.getApplicationInformation().remove(appInfo);
+    xsdAnnotation.getElement().removeChild(appInfo);
+    xsdAnnotation.updateElement();
+  }
+
+  public void undo()
+  {
+    super.undo();
+    xsdAnnotation.getApplicationInformation().add(appInfo);
+    xsdAnnotation.getElement().appendChild(appInfo);
+  }
+}
