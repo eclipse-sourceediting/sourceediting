5e2cec9786857667b21cf1f45770eb1e6e80ac98
nitind <nitind>
2006-04-18 18:40:47 +0000

    This commit was manufactured by cvs2svn to create tag 'v200604181942'.

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/IDocumentChangedNotifier.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.wst.xsd.ui.internal.common.properties.sections;
-
-import org.eclipse.wst.sse.core.internal.provisional.INodeAdapter;
-
-public interface IDocumentChangedNotifier
-{
-  public void addListener(INodeAdapter adapter);
-  public void removeListener(INodeAdapter adapter);  
-}
