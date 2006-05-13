2d15609908d1b1efd39ae61b66770cc3b3098a3e
kchong <kchong>
2006-05-13 18:30:54 +0000

    [141673] Adding new components should put it in direct edit mode, and then reselected in top level view

--- bundles/org.eclipse.wst.xsd.ui/src-adt/org/eclipse/wst/xsd/ui/internal/adt/design/directedit/LabelCellEditorLocator.java
+++ bundles/org.eclipse.wst.xsd.ui/src-adt/org/eclipse/wst/xsd/ui/internal/adt/design/directedit/LabelCellEditorLocator.java
@@ -34,11 +34,10 @@ public class LabelCellEditorLocator implements CellEditorLocator
   public void relocate(CellEditor celleditor)
   {
     Text text = (Text) celleditor.getControl();
-    
+
+    Label label = namedEditPart.getNameLabelFigure();
     if (text.getBounds().x <= 0)
     {
-      Label label = namedEditPart.getNameLabelFigure();
-      
       int widthToRemove = 0;
       // HACK 
       if (namedEditPart instanceof BaseFieldEditPart)
@@ -84,6 +83,16 @@ public class LabelCellEditorLocator implements CellEditorLocator
         offset++;
       }
       text.setSelection(offset, offset);
+    }
+    else
+    {
+      org.eclipse.swt.graphics.Point sel = text.getSelection();
+      org.eclipse.swt.graphics.Point pref = text.computeSize(-1, -1);
+      Rectangle rect = label.getTextBounds().getCopy();
+      label.translateToAbsolute(rect);
+      text.setBounds(rect.x-4, rect.y-1, pref.x+1, pref.y+1); 
+      text.setSelection(0);
+      text.setSelection(sel); 
     }
   }
 }
