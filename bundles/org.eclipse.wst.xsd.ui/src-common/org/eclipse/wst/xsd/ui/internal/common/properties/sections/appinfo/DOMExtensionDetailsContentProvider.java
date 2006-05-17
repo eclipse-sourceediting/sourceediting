bb1e4ef247a3a2007f73735900a8ebee67d53943
csalter <csalter>
2006-05-17 03:39:24 +0000

    [141066]  make extensions property sheet work properly

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/appinfo/DOMExtensionDetailsContentProvider.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/appinfo/DOMExtensionDetailsContentProvider.java
@@ -3,9 +3,9 @@ package org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import org.eclipse.wst.xml.core.internal.contentmodel.CMAttributeDeclaration;
 import org.eclipse.wst.xml.core.internal.contentmodel.CMElementDeclaration;
-import org.eclipse.wst.xml.core.internal.contentmodel.CMNamedNodeMap;
 import org.eclipse.wst.xml.core.internal.contentmodel.modelquery.ModelQuery;
 import org.eclipse.wst.xml.core.internal.modelquery.ModelQueryUtil;
 import org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo.custom.DefaultListNodeEditorConfiguration;
@@ -27,7 +27,7 @@ public class DOMExtensionDetailsContentProvider implements ExtensionDetailsConte
     HashMap resultMap = new HashMap();
     if (input instanceof Element)
     {
-      Element element = (Element) input;
+      Element element = (Element) input;     
       NamedNodeMap attributes = element.getAttributes();
       for (int i = 0; i < attributes.getLength(); i++)
       {
@@ -43,18 +43,14 @@ public class DOMExtensionDetailsContentProvider implements ExtensionDetailsConte
         CMElementDeclaration ed = modelQuery.getCMElementDeclaration(element);
         if (ed != null)
         {
-          CMNamedNodeMap attrMap = ed.getAttributes();
-          if (attrMap != null)
-          {
-            int attrMapLength = attrMap.getLength();
-            for (int i = 0; i < attrMapLength; i++)
-            {
-              CMAttributeDeclaration ad = (CMAttributeDeclaration) attrMap.item(i);
+          List list = modelQuery.getAvailableContent(element, ed, ModelQuery.INCLUDE_ATTRIBUTES);
+          for (Iterator i = list.iterator(); i.hasNext(); )
+          {  
+              CMAttributeDeclaration ad = (CMAttributeDeclaration)i.next();
               if (resultMap.get(ad.getNodeName()) == null)
               {
                 resultMap.put(ad.getNodeName(), new DOMExtensionItem(element, ad));
-              }
-            }
+              }            
           }
           //
           int contentType = ed.getContentType();
