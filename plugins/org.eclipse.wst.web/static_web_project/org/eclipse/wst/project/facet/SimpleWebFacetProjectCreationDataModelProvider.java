5f0520a348cc45beabe159639574733e45d0fb1f
cbridgha <cbridgha>
2006-08-11 19:09:12 +0000

    [152011] JS keep runtimes unfiltered

--- plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/SimpleWebFacetProjectCreationDataModelProvider.java
+++ plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/SimpleWebFacetProjectCreationDataModelProvider.java
@@ -1,8 +1,13 @@
 package org.eclipse.wst.project.facet;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.eclipse.wst.common.componentcore.datamodel.FacetProjectCreationDataModelProvider;
+import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetDataModelProperties;
 import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
 import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 
 public class SimpleWebFacetProjectCreationDataModelProvider extends FacetProjectCreationDataModelProvider {
 
@@ -15,6 +20,10 @@ public class SimpleWebFacetProjectCreationDataModelProvider extends FacetProject
 		FacetDataModelMap map = (FacetDataModelMap) getProperty(FACET_DM_MAP);
 		IDataModel simpleWebFacet = DataModelFactory.createDataModel(new SimpleWebFacetInstallDataModelProvider());
 		map.add(simpleWebFacet);
+		
+		Collection requiredFacets = new ArrayList();
+		requiredFacets.add(ProjectFacetsManager.getProjectFacet(simpleWebFacet.getStringProperty(IFacetDataModelProperties.FACET_ID)));
+		setProperty(REQUIRED_FACETS_COLLECTION, requiredFacets);
 	}
 
 }
