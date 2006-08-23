ea1a77bfb7a992126dfe21175146784d7c86fae4
amywu <amywu>
2006-08-23 23:28:12 +0000

    [154899] DirtyRegionProcessor#contains is called a lot

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/reconcile/StructuredRegionProcessor.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/reconcile/StructuredRegionProcessor.java
@@ -87,41 +87,147 @@ public class StructuredRegionProcessor extends DocumentRegionProcessor {
 	private IModelLifecycleListener fLifeCycleListener = new ModelLifecycleListener();
 
 	/**
-	 * @see org.eclipse.wst.sse.ui.internal.reconcile.DirtyRegionProcessor#contains(org.eclipse.jface.text.reconciler.DirtyRegion,
-	 *      org.eclipse.jface.text.reconciler.DirtyRegion)
+	 * @deprecated use getOuterRegion() instead
 	 */
 	protected boolean contains(DirtyRegion root, DirtyRegion possible) {
+		// remove method post wtp 1.5.1
 		// this method is a performance hit
 		// look for alternatives
 
-		boolean contains = false;
-		IStructuredModel sModel = getStructuredModelForRead(getDocument());
-		try {
-			if (sModel != null) {
-				IndexedRegion rootRegion = sModel.getIndexedRegion(root.getOffset());
-				IndexedRegion rootRegion2 = sModel.getIndexedRegion(root.getOffset() + root.getLength());
-				IndexedRegion possRegion = sModel.getIndexedRegion(possible.getOffset());
-				IndexedRegion possRegion2 = sModel.getIndexedRegion(possible.getOffset() + possible.getLength());
-				if (rootRegion != null && possRegion != null) {
-					int rootStart = rootRegion.getStartOffset();
-					int rootEnd = rootRegion2 != null ? rootRegion2.getEndOffset() : getDocument().getLength();
-					int possStart = possRegion.getStartOffset();
-					int possEnd = possRegion2 != null ? possRegion2.getEndOffset() : getDocument().getLength();
+		// call super to just check if possible is already contained within
+		// root based on offsets
+		boolean contains = super.contains(root, possible);
+		if (!contains) {
+			IStructuredModel sModel = getStructuredModelForRead(getDocument());
+			try {
+				if (sModel != null) {
+					// first just check if possregion appears after rootregion
+					IndexedRegion rootRegion = sModel.getIndexedRegion(root.getOffset());
+					IndexedRegion possRegion = sModel.getIndexedRegion(possible.getOffset());
+					if (rootRegion != null && possRegion != null) {
+						int rootStart = rootRegion.getStartOffset();
+						int possStart = possRegion.getStartOffset();
+						if (rootStart <= possStart) {
+							// then check if rootregion's end appears after
+							// possregion's end
+							IndexedRegion rootRegion2 = sModel.getIndexedRegion(root.getOffset() + root.getLength());
+							int rootEnd = rootRegion2 != null ? rootRegion2.getEndOffset() : getDocument().getLength();
+							IndexedRegion possRegion2 = sModel.getIndexedRegion(possible.getOffset() + possible.getLength());
+							int possEnd = possRegion2 != null ? possRegion2.getEndOffset() : getDocument().getLength();
 
-					if (rootStart <= possStart && rootEnd >= possEnd) {
-						contains = true;
+							if (rootEnd >= possEnd) {
+								contains = true;
+							}
+							if (DEBUG)
+								System.out.println("checking if [" + rootStart + ":" + rootEnd + "] contains [" + possStart + ":" + possEnd + "] ... " + contains); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+						}
 					}
-
-					if (DEBUG)
-						System.out.println("checking if [" + rootStart + ":" + rootEnd + "] contains [" + possStart + ":" + possEnd + "] ... " + contains); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 				}
 			}
-		}
-		finally {
-			if (sModel != null)
-				sModel.releaseFromRead();
+			finally {
+				if (sModel != null)
+					sModel.releaseFromRead();
+			}
 		}
 		return contains;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.wst.sse.ui.internal.reconcile.DirtyRegionProcessor#getOuterRegion(org.eclipse.jface.text.reconciler.DirtyRegion,
+	 *      org.eclipse.jface.text.reconciler.DirtyRegion)
+	 */
+	protected DirtyRegion getOuterRegion(DirtyRegion root, DirtyRegion possible) {
+		// first try simple region check if one region contains the other
+		DirtyRegion outer = super.getOuterRegion(root, possible);
+		if (outer == null) {
+			// now compare nodes
+			IStructuredModel sModel = getStructuredModelForRead(getDocument());
+			try {
+				if (sModel != null) {
+					IndexedRegion rootRegion = sModel.getIndexedRegion(root.getOffset());
+					IndexedRegion possRegion = sModel.getIndexedRegion(possible.getOffset());
+					if (rootRegion != null && possRegion != null) {
+						int rootStart = rootRegion.getStartOffset();
+						int possStart = possRegion.getStartOffset();
+						// first just check if rootregion starts before
+						// possregion
+						if (rootStart <= possStart) {
+							// check if possregion is inside rootregion
+							outer = _getOuterRegion(root, possible, sModel, rootStart, possStart);
+						}
+						else {
+							// otherwise if rootregion is inside possregion
+							outer = _getOuterRegion(possible, root, sModel, possStart, rootStart);
+						}
+					}
+				}
+			}
+			finally {
+				if (sModel != null)
+					sModel.releaseFromRead();
+			}
+		}
+		return outer;
+	}
+
+	/**
+	 * Assumes that when this method is called, region1's node start offset >=
+	 * region2's node start offset. Determines if region1 contains region2 or
+	 * vice versa. Returns region1 if:
+	 * <ul>
+	 * <li>region1's node region == region2's node region</li>
+	 * <li>region1's node region contains region2's node region</li>
+	 * </ul>
+	 * Returns region2 if:
+	 * <ul>
+	 * <li>region1's node region and region2's node region starts at same
+	 * offset but region2's node region is longer</li>
+	 * </ul>
+	 * Returns null otherwise.
+	 * 
+	 * @param region1
+	 * @param region2
+	 * @param sModel
+	 * @param region1NodeStart
+	 * @param region2NodeStart
+	 * @return outer dirty region or null if none exists.
+	 */
+	private DirtyRegion _getOuterRegion(DirtyRegion region1, DirtyRegion region2, IStructuredModel sModel, int region1NodeStart, int region2NodeStart) {
+		DirtyRegion outer = null;
+		int region1NodeEnd = -1;
+		int region2NodeEnd = -1;
+		// then check if region1's end appears after
+		// region2's end
+		IndexedRegion region1EndNode = sModel.getIndexedRegion(region1.getOffset() + region1.getLength());
+		if (region1EndNode == null) {
+			// if no end, just assume region spans all the
+			// way to the end so it includes other region
+			outer = region1;
+		}
+		else {
+			region1NodeEnd = region1EndNode.getEndOffset();
+			IndexedRegion region2EndNode = sModel.getIndexedRegion(region2.getOffset() + region2.getLength());
+			region2NodeEnd = region2EndNode != null ? region2EndNode.getEndOffset() : getDocument().getLength();
+			if (region1NodeEnd >= region2NodeEnd) {
+				// root contains or is equal to possible
+				outer = region1;
+			}
+			else if (region1NodeStart == region2NodeStart && region2NodeEnd >= region1NodeEnd) {
+				// possible contains root because they
+				// both start at same place but possible
+				// is longer
+				outer = region2;
+			}
+		}
+		if (DEBUG) {
+			if (outer != null)
+				System.out.println("checking if [" + region1NodeStart + ":" + region1NodeEnd + "] contains [" + region2NodeStart + ":" + region2NodeEnd + "] ... " + outer.toString()); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+			else
+				System.out.println("checking if [" + region1NodeStart + ":" + region1NodeEnd + "] contains [" + region2NodeStart + ":" + region2NodeEnd + "] ... NO CONTAIN"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+		}
+		return outer;
 	}
 
 	/**
