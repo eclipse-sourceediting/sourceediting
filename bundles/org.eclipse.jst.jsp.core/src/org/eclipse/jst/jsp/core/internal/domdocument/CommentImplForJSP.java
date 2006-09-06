8c91da6c34c4ce571efee45b5bfd52a363e50986
david_williams <david_williams>
2006-09-06 02:45:17 +0000

    [155747] Document is corrupted after importing comment node.

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/domdocument/CommentImplForJSP.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/domdocument/CommentImplForJSP.java
@@ -15,8 +15,20 @@ package org.eclipse.jst.jsp.core.internal.domdocument;
 import org.eclipse.jst.jsp.core.internal.regions.DOMJSPRegionContexts;
 import org.eclipse.wst.xml.core.internal.document.CommentImpl;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
+/**
+ * CommentImplForJSP
+ */
 public class CommentImplForJSP extends CommentImpl {
+	protected CommentImplForJSP() {
+		super();
+	}
+
+	protected CommentImplForJSP(CommentImpl that) {
+		super(that);
+	}
+
 	protected boolean isNestedCommentClose(String regionType) {
 		boolean result = regionType == DOMJSPRegionContexts.JSP_COMMENT_CLOSE;
 		return result;
@@ -29,5 +41,10 @@ public class CommentImplForJSP extends CommentImpl {
 
 	protected void setOwnerDocument(Document ownerDocument) {
 		super.setOwnerDocument(ownerDocument);
+	}
+	
+	public Node cloneNode(boolean deep) {
+		CommentImpl cloned = new CommentImplForJSP(this);
+		return cloned;
 	}
 }
