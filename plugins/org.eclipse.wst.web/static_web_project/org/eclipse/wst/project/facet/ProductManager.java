e875f9b5ee3470373b49571412c30d65fc7f9b59
cbridgha <cbridgha>
2006-09-07 04:21:42 +0000

    [153822] def runtimes

--- plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/ProductManager.java
+++ plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/ProductManager.java
@@ -2,7 +2,6 @@ package org.eclipse.wst.project.facet;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.IllegalFormatException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
