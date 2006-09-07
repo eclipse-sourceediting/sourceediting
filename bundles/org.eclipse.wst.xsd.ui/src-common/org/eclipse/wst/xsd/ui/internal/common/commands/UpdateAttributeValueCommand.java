2c45fdfe799d20b6bb215d44d13ba6c9951064f0
kchong <kchong>
2006-09-07 10:53:10 +0000

    [153659] Unable to set "Block", "Final" "Abstract" attribute on complex type

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/commands/UpdateAttributeValueCommand.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/commands/UpdateAttributeValueCommand.java
@@ -15,12 +15,19 @@ import org.w3c.dom.Element;
 /*
  * This command is used from the extension view to edit extension elements
  * and attributes which are implemented as DOM objects (not part of the EMF model)
+ * 
+ * (trung) also used in XSDComplexTypeAdvancedSection to change attribute of
+ * specical attributes like block, restriction which is not part of EMF Model
  */
 public class UpdateAttributeValueCommand  extends BaseCommand
 {
   Element element;
   String attributeName;
   String attributeValue;
+  
+  /** Whether the attribute should be deleted if value to 
+   *   be set is an empty String or null  */
+  protected boolean deleteIfValueEmpty = false;
   
   public UpdateAttributeValueCommand(Element element, String attributeName, String attributeValue)
   {
@@ -29,13 +36,25 @@ public class UpdateAttributeValueCommand  extends BaseCommand
     this.attributeValue = attributeValue;
   }
 
+  public void setDeleteIfEmpty(boolean v)
+  {
+	deleteIfValueEmpty = v;
+  }
   
   public void execute()
   {
     try
     {
       beginRecording(element);
-      element.setAttribute(attributeName, attributeValue);
+      if (deleteIfValueEmpty && 
+    		  (attributeValue == null || attributeValue.equals("") ) )
+      {
+    	element.removeAttribute(attributeName);
+      }
+      else
+      {
+        element.setAttribute(attributeName, attributeValue);
+      }
     }
     finally
     {
