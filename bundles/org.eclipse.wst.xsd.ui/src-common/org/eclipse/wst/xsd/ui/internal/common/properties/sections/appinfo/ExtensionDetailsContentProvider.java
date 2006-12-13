086863296b0ac3a3a8e343dbb559d2201998668b
nitind <nitind>
2006-12-13 14:16:29 +0000

    This commit was manufactured by cvs2svn to create tag 'v200612131416'.

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/appinfo/ExtensionDetailsContentProvider.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo;
-
-public interface ExtensionDetailsContentProvider
-{
-  Object[] getItems(Object input);
-  String getName(Object item);
-  String getValue(Object item);
-  String[] getPossibleValues(Object item);
-}
