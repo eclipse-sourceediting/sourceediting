368052c8f632e9519cc24fc589a858147d9caba4
nitind <nitind>
2007-01-03 14:34:24 +0000

    This commit was manufactured by cvs2svn to create tag 'v200701300431'.

--- bundles/org.eclipse.wst.sse.core/DevTimeSupport/SedModel/CSSTokenizer/devel/flex.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set PATH=%PATH%;c:\jdk1.4\bin
-java -Xmx470000000 -cp JFlex\lib\sed-jflex.jar;. JFlex.Main JSPTokenizer -skel skeleton.sed
-java -Xmx470000000 -cp JFlex\lib\sed-jflex.jar;. JFlex.Main XMLTokenizer -skel skeleton.sed
-rm -f JSPTokenizer.java~ JSPTokenizer~ XMLTokenizer.java~ XMLTokenizer~
-copy XMLTokenizer.java ..\..\..\..\sedmodel\com\ibm\sed\parser\internal
-copy JSPTokenizer.java ..\..\..\..\sedmodel\com\ibm\sed\parser\internal
