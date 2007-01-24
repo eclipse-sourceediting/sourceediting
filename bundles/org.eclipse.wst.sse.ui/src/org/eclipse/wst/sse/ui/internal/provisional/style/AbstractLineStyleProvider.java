c39f3465ec6388219b8426fe60128ce0e10236d3
amywu <amywu>
2007-01-24 01:20:19 +0000

    [169734] highlighting preference changes not taken effect after save as

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/provisional/style/AbstractLineStyleProvider.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/provisional/style/AbstractLineStyleProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2004 IBM Corporation and others.
+ * Copyright (c) 2001, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -355,6 +355,7 @@ public abstract class AbstractLineStyleProvider {
 			fTextAttributes.clear();
 			fTextAttributes = null;
 		}
+		setInitialized(false);
 	}
 
 	/**
