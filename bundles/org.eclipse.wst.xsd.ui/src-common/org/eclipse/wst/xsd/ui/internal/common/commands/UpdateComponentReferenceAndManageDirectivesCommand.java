6bce31ab2d721b819818823dc46caf004b2c0fa4
kchong <kchong>
2007-01-29 20:55:31 +0000

    [158409] [Editor] problem browsing for types in 'no target namespace' schemas.  Add comments and format code

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/commands/UpdateComponentReferenceAndManageDirectivesCommand.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/commands/UpdateComponentReferenceAndManageDirectivesCommand.java
@@ -39,85 +39,88 @@ public abstract class UpdateComponentReferenceAndManageDirectivesCommand extends
     XSDComponent result = null;
     XSDSchema schema = concreteComponent.getSchema();
     XSDSchemaDirective directive = null;
-    // TODO (cs) handle case where namespace==null
-    //
-      // lets see if the element is already visible to our schema
-      result = getDefinedComponent(schema, componentName, componentNamespace);
-      if (result == null)
+
+    // lets see if the element is already visible to our schema
+    result = getDefinedComponent(schema, componentName, componentNamespace);
+    if (result == null)
+    {
+      // TODO (cs) we need to provide a separate command to do this part
+      //
+      // apparently the element is not yet visible, we need to add
+      // includes/imports to get to it
+      if (componentNamespace != null && componentNamespace.equals(schema.getTargetNamespace()))
       {
-        // TODO (cs) we need to provide a separate command to do this part
-        //
-        // apparently the element is not yet visible, we need to add
-        // includes/imports to get to it
-        if (componentNamespace != null && componentNamespace.equals(schema.getTargetNamespace()))
+        // we need to add an include
+        // if the component's namespace is not null and matches the schema's
+        // target namespace
+        directive = XSDFactory.eINSTANCE.createXSDInclude();
+      }
+      else if (componentNamespace == null)
+      {
+        // we need to add an include
+        // if the component's namespace is null, then we can just add it
+        // only if the current namespace is not null
+        directive = XSDFactory.eINSTANCE.createXSDInclude();
+
+        // we have to ensure the schema for schema prefix is NOT null
+        if (schema.getSchemaForSchemaQNamePrefix() == null)
         {
-          // we need to add an include
-          // if the component's namespace is not null and matches the schema's target namespace
-          directive = XSDFactory.eINSTANCE.createXSDInclude();
-        }
-        else if (componentNamespace == null)
-        {
-          // we need to add an include
-          // if the component's namespace is null, then we can just add it
-          // only if the current namespace is not null
-          directive = XSDFactory.eINSTANCE.createXSDInclude();
-          
-          if (schema.getSchemaForSchemaQNamePrefix() == null)
-          {
-            String targetNS = schema.getTargetNamespace();
-            if (targetNS == null) targetNS = "";
-            UpdateNamespaceInformationCommand command = new UpdateNamespaceInformationCommand("", schema, "", targetNS);
-            command.execute();
-          }
-        }
-        else
-        {
-          // we need to add an import
-          XSDImport xsdImport = XSDFactory.eINSTANCE.createXSDImport();
-          xsdImport.setNamespace(componentNamespace);
-          directive = xsdImport;
-        }
-     
-        String location = computeNiceLocation(schema.getSchemaLocation(), file);       
-        directive.setSchemaLocation(location);
-        // TODO (cs) we should at the directive 'next' in the list of directives
-        // for now I'm just adding as the first thing in the schema :-(
-        //
-        schema.getContents().add(0, directive);
-        XSDSchema resolvedSchema = directive.getResolvedSchema();
-        if (resolvedSchema == null)
-        {
-          String platformLocation = "platform:/resource" + file.getFullPath();
-          Resource resource = concreteComponent.eResource().getResourceSet().createResource(URI.createURI(platformLocation));
-          if (resource instanceof XSDResourceImpl)
-          {
-            try
-            {
-              resource.load(null);
-              XSDResourceImpl resourceImpl = (XSDResourceImpl) resource;
-              resolvedSchema = resourceImpl.getSchema();
-              if (resolvedSchema != null)
-              {
-                directive.setResolvedSchema(resolvedSchema);
-              }
-            }
-            catch (Exception e)
-            {
-            }
-          }
-        }
-        if (resolvedSchema != null)
-        {
-          result = getDefinedComponent(resolvedSchema, componentName, componentNamespace);
-        }
-        else
-        {
-          // TODO (cs) consider setting some error state so that the client can
-          // provide a pop-dialog error
-          // we should also remove the import/include so save from cluttering
-          // the file with bogus directives
+          String targetNS = schema.getTargetNamespace();
+          if (targetNS == null)
+            targetNS = "";
+          // this will just update the schema for schema prefix to be, say, xsd
+          UpdateNamespaceInformationCommand command = new UpdateNamespaceInformationCommand("", schema, "", targetNS);
+          command.execute();
         }
       }
+      else
+      {
+        // we need to add an import
+        XSDImport xsdImport = XSDFactory.eINSTANCE.createXSDImport();
+        xsdImport.setNamespace(componentNamespace);
+        directive = xsdImport;
+      }
+
+      String location = computeNiceLocation(schema.getSchemaLocation(), file);
+      directive.setSchemaLocation(location);
+      // TODO (cs) we should at the directive 'next' in the list of directives
+      // for now I'm just adding as the first thing in the schema :-(
+      //
+      schema.getContents().add(0, directive);
+      XSDSchema resolvedSchema = directive.getResolvedSchema();
+      if (resolvedSchema == null)
+      {
+        String platformLocation = "platform:/resource" + file.getFullPath();
+        Resource resource = concreteComponent.eResource().getResourceSet().createResource(URI.createURI(platformLocation));
+        if (resource instanceof XSDResourceImpl)
+        {
+          try
+          {
+            resource.load(null);
+            XSDResourceImpl resourceImpl = (XSDResourceImpl) resource;
+            resolvedSchema = resourceImpl.getSchema();
+            if (resolvedSchema != null)
+            {
+              directive.setResolvedSchema(resolvedSchema);
+            }
+          }
+          catch (Exception e)
+          {
+          }
+        }
+      }
+      if (resolvedSchema != null)
+      {
+        result = getDefinedComponent(resolvedSchema, componentName, componentNamespace);
+      }
+      else
+      {
+        // TODO (cs) consider setting some error state so that the client can
+        // provide a pop-dialog error
+        // we should also remove the import/include so save from cluttering
+        // the file with bogus directives
+      }
+    }
 
     return result;
   }
