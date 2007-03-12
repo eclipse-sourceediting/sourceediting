43ae4c02c21104014c72cbf1bc878774362615dc
jsholl <jsholl>
2007-03-12 21:14:18 +0000

    [176647] DataModel API cleanup

--- plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/ISimpleWebFacetInstallDataModelProperties.java
+++ plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/ISimpleWebFacetInstallDataModelProperties.java
@@ -1,6 +1,12 @@
 package org.eclipse.wst.project.facet;
 
-public interface ISimpleWebFacetInstallDataModelProperties {
+import org.eclipse.wst.common.frameworks.datamodel.IDataModelProperties;
+
+public interface ISimpleWebFacetInstallDataModelProperties extends IDataModelProperties {
+	/**
+	 * This field should not be used.  It is not part of the API and may be modified in the future.
+	 */
+	public static Class _provider_class = SimpleWebFacetProjectCreationDataModelProvider.class;
 
 	public static final String CONTENT_DIR = "IStaticWebFacetInstallDataModelProperties.CONTENT_DIR"; //$NON-NLS-1$
 	
