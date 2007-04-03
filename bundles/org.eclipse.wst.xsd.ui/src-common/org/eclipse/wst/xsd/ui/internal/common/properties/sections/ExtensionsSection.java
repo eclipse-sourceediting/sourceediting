f8f467e8d4dcf50f9cf1c9f3f710202186c6a353
kchong <kchong>
2007-04-03 15:25:21 +0000

    [174060] [hotbug_request] Extensions Section doesn't check for null

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/ExtensionsSection.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/ExtensionsSection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -103,7 +103,11 @@ public class ExtensionsSection extends AbstractExtensionsSection
     super.dispose();
     if (adapter != null)
     {
-      adapter.getModelReconcileAdapter().removeListener(internalNodeAdapter);
+      ModelReconcileAdapter modelReconcileAdapter = adapter.getModelReconcileAdapter();
+      if (modelReconcileAdapter != null)
+      {
+        modelReconcileAdapter.removeListener(internalNodeAdapter);
+      }
     }  
   }
 
