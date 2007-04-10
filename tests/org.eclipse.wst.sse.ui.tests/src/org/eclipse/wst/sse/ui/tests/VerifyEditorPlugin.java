6bfa6b870b365ba9b5047744b54fcb3ba517e74f
amywu <amywu>
2007-04-10 20:07:46 +0000

    [181799] update copyright comments for all sse code

--- tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/VerifyEditorPlugin.java
+++ tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/VerifyEditorPlugin.java
@@ -11,15 +11,16 @@ import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.wst.sse.ui.internal.SSEUIPlugin;
 
-/*****************************************************************************
- * Copyright (c) 2004 IBM Corporation and others. All rights reserved. This
- * program and the accompanying materials are made available under the terms
- * of the Eclipse Public License v1.0 which accompanies this distribution, and
- * is available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: IBM Corporation - initial API and implementation
- * 
- ****************************************************************************/
+/*******************************************************************************
+ * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 
 public class VerifyEditorPlugin extends TestCase {
 
