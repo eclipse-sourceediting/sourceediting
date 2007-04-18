6b99f1338ca5035a156d3a4e2dae2cb6086f0d22
amywu <amywu>
2007-04-18 16:54:30 +0000

    [151581] Enable folding menu item is checked although folding isn't enabled (in preference store)

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/FoldingActionGroup.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/FoldingActionGroup.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -92,7 +92,10 @@ class FoldingActionGroup extends ActionGroup {
 				setEnabled(isEnabled);
 			}
 		};
-		fToggle.setChecked(true);
+		
+		IPreferenceStore store = SSEUIPlugin.getDefault().getPreferenceStore();
+		boolean checked = store.getBoolean(IStructuredTextFoldingProvider.FOLDING_ENABLED);
+		fToggle.setChecked(checked);
 		fToggle.setActionDefinitionId(IFoldingCommandIds.FOLDING_TOGGLE);
 		editor.setAction("FoldingToggle", fToggle); //$NON-NLS-1$
 
