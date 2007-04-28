87a842748989044ae2d31a7cb00e9b0d99ad9fd3
nitind <nitind>
2007-04-28 13:30:55 +0000

    This commit was manufactured by cvs2svn to create tag 'v200704281330'.

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/appinfo/custom/NodeEditorProvider.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.wst.xsd.ui.internal.common.properties.sections.appinfo.custom;
-
-
-public abstract class NodeEditorProvider
-{
-  public abstract NodeEditorConfiguration getNodeEditorConfiguration(String parentName, String nodeName); 
-  //public abstract NodeEditorConfiguration getNodeEditorConfiguration(Node node);   
-}
