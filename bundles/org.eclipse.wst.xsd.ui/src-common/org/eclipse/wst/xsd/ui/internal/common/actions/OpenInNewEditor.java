95c2323a7fee75104bb7cc113b562344e82fcab1
kchong <kchong>
2007-07-18 17:14:32 +0000

    [194685] Open on reference doesn't work for inline schemas referencing other inline schemas.  Approved bug for WTP 1.5.5.

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/actions/OpenInNewEditor.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/actions/OpenInNewEditor.java
@@ -15,22 +15,25 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IEditorReference;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.wst.common.uriresolver.internal.util.URIHelper;
+import org.eclipse.wst.xsd.ui.internal.adapters.XSDBaseAdapter;
 import org.eclipse.wst.xsd.ui.internal.adapters.XSDSchemaDirectiveAdapter;
-import org.eclipse.wst.xsd.ui.internal.adapters.XSDTypeDefinitionAdapter;
 import org.eclipse.wst.xsd.ui.internal.adt.actions.BaseSelectionAction;
 import org.eclipse.wst.xsd.ui.internal.common.util.Messages;
 import org.eclipse.wst.xsd.ui.internal.editor.InternalXSDMultiPageEditor;
-import org.eclipse.wst.xsd.ui.internal.utils.OpenOnSelectionHelper;
+import org.eclipse.wst.xsd.ui.internal.editor.XSDEditorPlugin;
+import org.eclipse.wst.xsd.ui.internal.editor.XSDFileEditorInput;
+import org.eclipse.xsd.XSDConcreteComponent;
+import org.eclipse.xsd.XSDSchema;
 import org.eclipse.xsd.XSDSchemaDirective;
-import org.eclipse.xsd.XSDTypeDefinition;
 import org.eclipse.xsd.impl.XSDImportImpl;
 
 public class OpenInNewEditor extends BaseSelectionAction
@@ -53,16 +56,37 @@ public class OpenInNewEditor extends BaseSelectionAction
   {
     Object selection = ((IStructuredSelection) getSelection()).getFirstElement();
 
-    if (selection instanceof XSDTypeDefinitionAdapter)
+    if (selection instanceof XSDBaseAdapter)
     {
-      XSDTypeDefinitionAdapter xsdAdapter = (XSDTypeDefinitionAdapter) selection;
-      XSDTypeDefinition fComponent = (XSDTypeDefinition) xsdAdapter.getTarget();
+      XSDBaseAdapter xsdAdapter = (XSDBaseAdapter) selection;
+      XSDConcreteComponent fComponent = (XSDConcreteComponent) xsdAdapter.getTarget();
+      XSDSchema schema = fComponent.getSchema();
 
       if (fComponent.getSchema() != null)
       {
         String schemaLocation = URIHelper.removePlatformResourceProtocol(fComponent.getSchema().getSchemaLocation());
         IPath schemaPath = new Path(schemaLocation);
         IFile schemaFile = ResourcesPlugin.getWorkspace().getRoot().getFile(schemaPath);
+
+        // Special case any imports/includes
+        if (selection instanceof XSDSchemaDirectiveAdapter)
+        {
+          XSDSchemaDirective dir = (XSDSchemaDirective)((XSDSchemaDirectiveAdapter)selection).getTarget();
+          // force load of imported schema
+          if (dir instanceof XSDImportImpl)
+          {
+            ((XSDImportImpl)dir).importSchema();
+          }
+          if (dir.getResolvedSchema() != null)
+          {
+            schemaLocation = URIHelper.removePlatformResourceProtocol(dir.getResolvedSchema().getSchemaLocation());
+            schemaPath = new Path(schemaLocation);
+            schemaFile = ResourcesPlugin.getWorkspace().getRoot().getFile(schemaPath);
+            schema = dir.getResolvedSchema(); 
+            fComponent = dir.getResolvedSchema();
+          }
+        }
+
         if (schemaFile != null && schemaFile.exists())
         {
           IWorkbenchWindow workbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
@@ -71,40 +95,70 @@ public class OpenInNewEditor extends BaseSelectionAction
             IWorkbenchPage page = workbenchWindow.getActivePage();
             try
             {
-              // TODO: Should use this to open in default editor
-              // IEditorPart editorPart = IDE.openEditor(page, schemaFile, true);
-              IEditorPart editorPart = page.openEditor(new FileEditorInput(schemaFile), "org.eclipse.wst.xsd.ui.internal.editor.InternalXSDMultiPageEditor", true); //$NON-NLS-1$
+              // Get the current editor's schema
+              XSDSchema xsdSchema = (XSDSchema)getWorkbenchPart().getAdapter(XSDSchema.class);
+              
+              IEditorPart editorPart = null;
+              // This first check is to ensure that the schema is actually
+              // different than the current one we are editing against in the editor, and that we
+              // are in the same resource file....hence multiple schemas in the same file.
+              if (xsdSchema != null && fComponent.getRootContainer().eResource() == xsdSchema.eResource())
+              {
+                XSDFileEditorInput xsdFileEditorInput = new XSDFileEditorInput(schemaFile, fComponent.getSchema());
+                IEditorPart activeEditor = page.getActiveEditor();
+                String editorName = null; 
+                
+                // will use FileEditorInput's name if still null
+                // Try to use the same editor name as the current one
+                if (activeEditor != null)
+                {
+                  IEditorInput input = activeEditor.getEditorInput();
+                  if (input != null)
+                  {
+                    editorName = input.getName();
+                    xsdFileEditorInput.setEditorName(editorName);
+                  }
+                }
+                
+                IEditorReference [] refs = page.getEditorReferences();
+                int length = refs.length;
+                for (int i = 0; i < length; i++)
+                {
+                  IEditorInput input = refs[i].getEditorInput();
+                  if (input instanceof XSDFileEditorInput)
+                  {
+                    IFile aFile = ((XSDFileEditorInput)input).getFile();
+                    if (aFile.getFullPath().equals(schemaFile.getFullPath()))
+                    {
+                      if (((XSDFileEditorInput)input).getSchema() == schema)
+                      {
+                        editorPart = refs[i].getEditor(true);
+                        page.activate(refs[i].getPart(true));
+                        break;
+                      }
+                    }
+                  }
+                }
+
+                if (editorPart == null)
+                {
+                  editorPart = page.openEditor(xsdFileEditorInput, XSDEditorPlugin.EDITOR_ID, true, 0);
+                }
+              }
+              else
+              {
+                editorPart = page.openEditor(new FileEditorInput(schemaFile), XSDEditorPlugin.EDITOR_ID);
+              }
 
               if (editorPart instanceof InternalXSDMultiPageEditor)
               {
-                InternalXSDMultiPageEditor xsdEditor = (InternalXSDMultiPageEditor) editorPart;
-
-                xsdEditor.openOnGlobalReference(fComponent);
+                ((InternalXSDMultiPageEditor) editorPart).openOnGlobalReference(fComponent);
               }
-
             }
-            catch (PartInitException pie)
+            catch (Exception e)
             {
             }
           }
-        }
-      }
-    }
-    else if (selection instanceof XSDSchemaDirectiveAdapter)
-    {
-      XSDSchemaDirective dir = (XSDSchemaDirective)((XSDSchemaDirectiveAdapter)selection).getTarget();
-      String schemaLocation = "";
-      // force load of imported schema
-      if (dir instanceof XSDImportImpl)
-      {
-        ((XSDImportImpl)dir).importSchema();
-      }
-      if (dir.getResolvedSchema() != null)
-      {
-        schemaLocation = URIHelper.removePlatformResourceProtocol(dir.getResolvedSchema().getSchemaLocation());
-        if (schemaLocation != null)
-        {
-          OpenOnSelectionHelper.openXSDEditor(schemaLocation);
         }
       }
     }
