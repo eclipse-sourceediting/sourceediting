2c4ee8e7738dcec53eb779d0891bfa91ef958ad1
david_williams <david_williams>
2007-08-17 01:00:52 +0000

    no change, example comments

--- bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/DebugAdapterFactory.java
+++ bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/DebugAdapterFactory.java
@@ -31,7 +31,7 @@ public class DebugAdapterFactory extends AbstractAdapterFactory implements Propa
 	}
 
 	/**
-	 * Constructor for DebugAdapterFactory.
+	 * Constructor  for DebugAdapterFactory.
 	 * 
 	 * @param fAdapterKey
 	 * @param registerAdapters
