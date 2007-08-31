0259137f236060eefd04af6afa42a06002d9bf91
kchong <kchong>
2007-08-31 22:42:44 +0000

    [144984] XSD Editor: Cannot add attribute from context menu off of an existing attribute

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/actions/AddXSDAttributeDeclarationAction.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/actions/AddXSDAttributeDeclarationAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,6 +17,7 @@ import org.eclipse.wst.xsd.ui.internal.adapters.XSDAdapterFactory;
 import org.eclipse.wst.xsd.ui.internal.adapters.XSDBaseAdapter;
 import org.eclipse.wst.xsd.ui.internal.common.commands.AddXSDAttributeDeclarationCommand;
 import org.eclipse.wst.xsd.ui.internal.common.util.Messages;
+import org.eclipse.xsd.XSDAttributeDeclaration;
 import org.eclipse.xsd.XSDAttributeGroupDefinition;
 import org.eclipse.xsd.XSDAttributeUse;
 import org.eclipse.xsd.XSDComplexTypeDefinition;
@@ -53,6 +54,10 @@ public class AddXSDAttributeDeclarationAction extends XSDBaseAction
     if (selection instanceof XSDBaseAdapter)
     {
       selection = ((XSDBaseAdapter) selection).getTarget();
+      if (selection instanceof XSDAttributeDeclaration)
+      {
+        selection = ((XSDAttributeDeclaration) selection).getContainer();
+      }
     }
     AddXSDAttributeDeclarationCommand command = null;
     if (selection instanceof XSDComplexTypeDefinition)
