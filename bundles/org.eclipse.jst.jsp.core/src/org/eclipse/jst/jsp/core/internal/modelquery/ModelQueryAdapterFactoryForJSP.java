807002d1663660ef1ed5e6a0c772e9e6cc243c7d
nitind <nitind>
2007-09-18 17:02:37 +0000

    [203649] xhtml editor thinks uppercase characters are correct and lowercase characters are wrong

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/modelquery/ModelQueryAdapterFactoryForJSP.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/modelquery/ModelQueryAdapterFactoryForJSP.java
@@ -10,8 +10,13 @@
  *******************************************************************************/
 package org.eclipse.jst.jsp.core.internal.modelquery;
 
+import java.io.File;
+import java.net.URI;
+
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.wst.common.uriresolver.internal.provisional.URIResolver;
 import org.eclipse.wst.sse.core.internal.provisional.AbstractAdapterFactory;
@@ -80,6 +85,42 @@ public class ModelQueryAdapterFactoryForJSP extends AbstractAdapterFactory imple
 				URIResolver idResolver = null;
 				if (resolver != null) {
 					idResolver = new XMLCatalogIdResolver(resolver.getFileBaseLocation(), resolver);
+				}
+				else {
+					/*
+					 * 203649 - this block may be necessary due to ordering of
+					 * setting the resolver into the model
+					 */
+					String baseLocation = null;
+					String modelsBaseLocation = model.getBaseLocation();
+					if (modelsBaseLocation != null) {
+						File file = new Path(modelsBaseLocation).toFile();
+						if (file.exists()) {
+							baseLocation = file.getAbsolutePath();
+						}
+						else {
+							IPath basePath = new Path(model.getBaseLocation());
+							IResource derivedResource = null;
+							if (basePath.segmentCount() > 1)
+								derivedResource = ResourcesPlugin.getWorkspace().getRoot().getFile(basePath);
+							else
+								derivedResource = ResourcesPlugin.getWorkspace().getRoot().getProject(basePath.segment(0));
+							IPath derivedPath = derivedResource.getLocation();
+							if (derivedPath != null) {
+								baseLocation = derivedPath.toString();
+							}
+							else {
+								URI uri = derivedResource.getLocationURI();
+								if (uri != null) {
+									baseLocation = uri.toString();
+								}
+							}
+							if(baseLocation == null) {
+								baseLocation = modelsBaseLocation;
+							}
+						}
+					}
+					idResolver = new XMLCatalogIdResolver(baseLocation, null);
 				}
 
 				ModelQuery modelQuery = createModelQuery(model, idResolver);
