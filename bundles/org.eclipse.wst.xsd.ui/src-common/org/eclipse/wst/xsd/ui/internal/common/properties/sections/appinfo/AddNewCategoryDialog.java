016efabea704d6c1fbd3f65fc877220de8aa7561
kchong <kchong>
2007-09-24 20:48:57 +0000

    [202659] Exception when adding extension category

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/appinfo/AddNewCategoryDialog.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/appinfo/AddNewCategoryDialog.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -285,7 +285,7 @@ public class AddNewCategoryDialog extends Dialog
           errDisplayer.setText(""); //$NON-NLS-1$
           errDisplayer.setImage(null);
         }
-        if (!appInfoSchemaLocation.equals("")) //$NON-NLS-1$
+        if (appInfoSchemaLocation != null && !appInfoSchemaLocation.equals("")) //$NON-NLS-1$
         {
           getButton(IDialogConstants.OK_ID).setEnabled(true);
         }
