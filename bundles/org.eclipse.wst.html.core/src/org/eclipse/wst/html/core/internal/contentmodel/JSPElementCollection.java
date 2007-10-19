0e3e73d5f86140580b93f82d163bc0757ba4d3e0
david_williams <david_williams>
2007-10-19 11:02:24 +0000

    185402 JSP validation fails because of defaulted attribute flush on jsp:include

--- bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/contentmodel/JSPElementCollection.java
+++ bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/contentmodel/JSPElementCollection.java
@@ -507,7 +507,7 @@ final class JSPElementCollection extends DeclCollection implements JSP11Namespac
 			// ("flush" ENUM REQUIRED (true|false)); Defect TORO:185241
 			adec = new AttrDecl(ATTR_NAME_FLUSH);
 			adec.type = new HTMLCMDataTypeImpl(CMDataType.ENUM);
-			adec.usage = CMAttributeDeclaration.REQUIRED;
+			adec.usage = CMAttributeDeclaration.OPTIONAL;
 			String[] values = {ATTR_VALUE_TRUE, ATTR_VALUE_FALSE};
 			adec.type.setEnumValues(values);
 			declarations.putNamedItem(ATTR_NAME_FLUSH, adec);
