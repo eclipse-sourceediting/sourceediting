4ff346afc9f9c532a1e2c3a58f583e0efcad8964
amywu <amywu>
2007-11-05 22:20:23 +0000

    [208820] extensible autoedit and doubleclick

--- tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/tests/TestEditorConfigurationXML.java
+++ tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/tests/TestEditorConfigurationXML.java
@@ -17,6 +17,7 @@ import org.eclipse.wst.sse.ui.internal.ExtendedConfigurationBuilder;
 import org.eclipse.wst.xml.core.internal.provisional.contenttype.ContentTypeIdForXML;
 import org.eclipse.wst.xml.core.text.IXMLPartitions;
 import org.eclipse.wst.xml.ui.StructuredTextViewerConfigurationXML;
+import org.eclipse.wst.xml.ui.internal.doubleclick.XMLDoubleClickStrategy;
 import org.eclipse.wst.xml.ui.internal.taginfo.XMLTagInfoHoverProcessor;
 import org.eclipse.wst.xml.ui.views.contentoutline.XMLContentOutlineConfiguration;
 import org.eclipse.wst.xml.ui.views.properties.XMLPropertySheetConfiguration;
@@ -51,5 +52,12 @@ public class TestEditorConfigurationXML extends TestCase {
 		assertNotNull("no documentation text hover for " + IXMLPartitions.XML_DEFAULT, o);
 		// check for over-qualified subclasses
 		assertEquals("unexpected documentation text hover for " + IXMLPartitions.XML_DEFAULT, o.getClass(), XMLTagInfoHoverProcessor.class);
+	}
+	
+	public void testGetDoubleClickStrategy() {
+		Object o = ExtendedConfigurationBuilder.getInstance().getConfiguration(ExtendedConfigurationBuilder.DOUBLECLICKSTRATEGY, IXMLPartitions.XML_DEFAULT);
+		assertNotNull("no doubleclick strategy for " + IXMLPartitions.XML_DEFAULT, o);
+		// check for over-qualified subclasses
+		assertEquals("unexpected doubleclick strategy for " + IXMLPartitions.XML_DEFAULT, o.getClass(), XMLDoubleClickStrategy.class);
 	}
 }
