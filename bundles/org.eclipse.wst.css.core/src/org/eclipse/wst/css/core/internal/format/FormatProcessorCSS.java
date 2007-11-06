9cc04a6aa17bb0af861b40cd6735c5fb94911ff3
amywu <amywu>
2007-11-06 00:54:41 +0000

    [208496] IllegalStateException formatting

--- bundles/org.eclipse.wst.css.core/src/org/eclipse/wst/css/core/internal/format/FormatProcessorCSS.java
+++ bundles/org.eclipse.wst.css.core/src/org/eclipse/wst/css/core/internal/format/FormatProcessorCSS.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,73 +32,80 @@ import org.w3c.dom.Node;
 
 
 public class FormatProcessorCSS extends AbstractStructuredFormatProcessor {
-
+	/*
+	 * Max length of text to be formatted to be considered a "small change"
+	 * Used for document rewrite session type.
+	 */
+	private final int MAX_SMALL_FORMAT_SIZE = 1000;
 
 	protected String getFileExtension() {
 		return "css"; //$NON-NLS-1$
 	}
 
-	public void formatModel(IStructuredModel structuredModel) {
-		int start = 0;
-		int length = structuredModel.getStructuredDocument().getLength();
-
-		formatModel(structuredModel, start, length);
-	}
-
 	public void formatModel(IStructuredModel structuredModel, int start, int length) {
 		CSSFormatUtil formatUtil = CSSFormatUtil.getInstance();
 		if (structuredModel instanceof ICSSModel) {
-			//BUG102822 take advantage of IDocumentExtension4
+			// BUG102822 take advantage of IDocumentExtension4
 			IDocumentExtension4 docExt4 = null;
 			if (structuredModel.getStructuredDocument() instanceof IDocumentExtension4) {
-				docExt4 = (IDocumentExtension4)structuredModel.getStructuredDocument();
+				docExt4 = (IDocumentExtension4) structuredModel.getStructuredDocument();
 			}
-			DocumentRewriteSession rewriteSession = (docExt4 == null) ? null :
-				docExt4.startRewriteSession(DocumentRewriteSessionType.UNRESTRICTED);
-			
-			ICSSDocument doc = ((ICSSModel) structuredModel).getDocument();
-			CSSSourceFormatter formatter = CSSSourceFormatterFactory.getInstance().getSourceFormatter((INodeNotifier) doc);
-			StringBuffer buf = formatter.format(doc);
-			if (buf != null) {
-				int startOffset = ((IndexedRegion) doc).getStartOffset();
-				int endOffset = ((IndexedRegion) doc).getEndOffset();
-				formatUtil.replaceSource(doc.getModel(), startOffset, endOffset - startOffset, buf.toString());
+			DocumentRewriteSession rewriteSession = null;
+
+			try {
+				DocumentRewriteSessionType rewriteType = (length > MAX_SMALL_FORMAT_SIZE) ? DocumentRewriteSessionType.UNRESTRICTED : DocumentRewriteSessionType.UNRESTRICTED_SMALL;
+				rewriteSession = (docExt4 == null || docExt4.getActiveRewriteSession() != null) ? null : docExt4.startRewriteSession(rewriteType);
+
+				ICSSDocument doc = ((ICSSModel) structuredModel).getDocument();
+				CSSSourceFormatter formatter = CSSSourceFormatterFactory.getInstance().getSourceFormatter((INodeNotifier) doc);
+				StringBuffer buf = formatter.format(doc);
+				if (buf != null) {
+					int startOffset = ((IndexedRegion) doc).getStartOffset();
+					int endOffset = ((IndexedRegion) doc).getEndOffset();
+					formatUtil.replaceSource(doc.getModel(), startOffset, endOffset - startOffset, buf.toString());
+				}
 			}
-			
-			//BUG102822 take advantage of IDocumentExtension4
-			if (docExt4 != null && rewriteSession != null)
-				docExt4.stopRewriteSession(rewriteSession);
+			finally {
+				// BUG102822 take advantage of IDocumentExtension4
+				if (docExt4 != null && rewriteSession != null)
+					docExt4.stopRewriteSession(rewriteSession);
+			}
 		}
 		else if (structuredModel instanceof IDOMModel) {
 			List cssnodes = formatUtil.collectCSSNodes(structuredModel, start, length);
 			if (cssnodes != null && !cssnodes.isEmpty()) {
 				ICSSModel model = null;
-				
-				//BUG102822 take advantage of IDocumentExtension4
+
+				// BUG102822 take advantage of IDocumentExtension4
 				IDocumentExtension4 docExt4 = null;
 				if (structuredModel.getStructuredDocument() instanceof IDocumentExtension4) {
-					docExt4 = (IDocumentExtension4)structuredModel.getStructuredDocument();
+					docExt4 = (IDocumentExtension4) structuredModel.getStructuredDocument();
 				}
-				DocumentRewriteSession rewriteSession = (docExt4 == null) ? null :
-					docExt4.startRewriteSession(DocumentRewriteSessionType.UNRESTRICTED);
-				
-				for (int i = 0; i < cssnodes.size(); i++) {
-					ICSSNode node = (ICSSNode) cssnodes.get(i);
-					CSSSourceFormatter formatter = CSSSourceFormatterFactory.getInstance().getSourceFormatter((INodeNotifier) node);
-					StringBuffer buf = formatter.format(node);
-					if (buf != null) {
-						int startOffset = ((IndexedRegion) node).getStartOffset();
-						int endOffset = ((IndexedRegion) node).getEndOffset();
-						if (model == null) {
-							model = node.getOwnerDocument().getModel();
+				DocumentRewriteSession rewriteSession = null;
+
+				try {
+					DocumentRewriteSessionType rewriteType = (length > MAX_SMALL_FORMAT_SIZE) ? DocumentRewriteSessionType.UNRESTRICTED : DocumentRewriteSessionType.UNRESTRICTED_SMALL;
+					rewriteSession = (docExt4 == null || docExt4.getActiveRewriteSession() != null) ? null : docExt4.startRewriteSession(rewriteType);
+
+					for (int i = 0; i < cssnodes.size(); i++) {
+						ICSSNode node = (ICSSNode) cssnodes.get(i);
+						CSSSourceFormatter formatter = CSSSourceFormatterFactory.getInstance().getSourceFormatter((INodeNotifier) node);
+						StringBuffer buf = formatter.format(node);
+						if (buf != null) {
+							int startOffset = ((IndexedRegion) node).getStartOffset();
+							int endOffset = ((IndexedRegion) node).getEndOffset();
+							if (model == null) {
+								model = node.getOwnerDocument().getModel();
+							}
+							formatUtil.replaceSource(model, startOffset, endOffset - startOffset, buf.toString());
 						}
-						formatUtil.replaceSource(model, startOffset, endOffset - startOffset, buf.toString());
 					}
 				}
-				
-				//BUG102822 take advantage of IDocumentExtension4
-				if (docExt4 != null && rewriteSession != null)
-					docExt4.stopRewriteSession(rewriteSession);
+				finally {
+					// BUG102822 take advantage of IDocumentExtension4
+					if (docExt4 != null && rewriteSession != null)
+						docExt4.stopRewriteSession(rewriteSession);
+				}
 			}
 		}
 	}
