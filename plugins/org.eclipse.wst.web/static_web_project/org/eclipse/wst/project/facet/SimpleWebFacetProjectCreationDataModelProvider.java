8e04c9e452e4367050750025a41e0abc16a15cf8
kkomissarchik <kkomissarchik>
2007-11-10 22:07:40 +0000

    Fixes for some of the failing JUnit tests.

--- plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/SimpleWebFacetProjectCreationDataModelProvider.java
+++ plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/project/facet/SimpleWebFacetProjectCreationDataModelProvider.java
@@ -14,9 +14,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import org.eclipse.wst.common.componentcore.datamodel.FacetProjectCreationDataModelProvider;
-import org.eclipse.wst.common.componentcore.datamodel.properties.IFacetDataModelProperties;
-import org.eclipse.wst.common.frameworks.datamodel.DataModelFactory;
-import org.eclipse.wst.common.frameworks.datamodel.IDataModel;
+import org.eclipse.wst.common.componentcore.internal.util.IModuleConstants;
+import org.eclipse.wst.common.project.facet.core.IProjectFacet;
 import org.eclipse.wst.common.project.facet.core.ProjectFacetsManager;
 
 public class SimpleWebFacetProjectCreationDataModelProvider extends FacetProjectCreationDataModelProvider {
@@ -27,13 +26,10 @@ public class SimpleWebFacetProjectCreationDataModelProvider extends FacetProject
 	
 	public void init() {
 		super.init();
-		FacetDataModelMap map = (FacetDataModelMap) getProperty(FACET_DM_MAP);
-		IDataModel simpleWebFacet = DataModelFactory.createDataModel(new SimpleWebFacetInstallDataModelProvider());
-		map.add(simpleWebFacet);
-		
-		Collection requiredFacets = new ArrayList();
-		requiredFacets.add(ProjectFacetsManager.getProjectFacet(simpleWebFacet.getStringProperty(IFacetDataModelProperties.FACET_ID)));
-		setProperty(REQUIRED_FACETS_COLLECTION, requiredFacets);
+
+        Collection<IProjectFacet> requiredFacets = new ArrayList<IProjectFacet>();
+        requiredFacets.add(ProjectFacetsManager.getProjectFacet(IModuleConstants.WST_WEB_MODULE));
+        setProperty(REQUIRED_FACETS_COLLECTION, requiredFacets);
 	}
 
 }
