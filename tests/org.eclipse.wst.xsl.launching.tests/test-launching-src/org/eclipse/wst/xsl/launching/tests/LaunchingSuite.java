07285d0cb649e2f3b937d574359e5eba6475f901
david_williams <david_williams>
2008-01-10 15:19:39 +0000

    some automated test infrastructure

---
+++ tests/org.eclipse.wst.xsl.launching.tests/test-launching-src/org/eclipse/wst/xsl/launching/tests/LaunchingSuite.java
@@ -0,0 +1,50 @@
+package org.eclipse.wst.xsl.launching.tests;
+
+import junit.framework.TestSuite;
+
+import org.eclipse.wst.xsl.launching.tests.testcase.XSLLaunchingTests;
+
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+/**
+ * This class does specifies all the classes in this bundle
+ * that provide tests. It is primarily for the convenience of 
+ * the AllTestsSuite.
+ *  
+ */
+public class LaunchingSuite extends TestSuite {
+
+	private static Class[] classes = new Class[]{XSLLaunchingTests.class};
+
+	public static TestSuite suite() {
+		return new LaunchingSuite();
+	}
+
+	public LaunchingSuite() {
+		super("XSL Launching Test Suite");
+		for (int i = 0; i < classes.length; i++) {
+			addTest(new TestSuite(classes[i], classes[i].getName()));
+		}
+	}
+
+	public LaunchingSuite(Class theClass, String name) {
+		super(theClass, name);
+	}
+
+	public LaunchingSuite(Class theClass) {
+		super(theClass);
+	}
+
+	public LaunchingSuite(String name) {
+		super(name);
+	}
+}
--- tests/org.eclipse.wst.xsl.tests/src/org/eclipse/wst/xsl/tests/AllTestsSuite.java
+++ tests/org.eclipse.wst.xsl.tests/src/org/eclipse/wst/xsl/tests/AllTestsSuite.java
@@ -1,15 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.wst.xsl.tests;
+
+import junit.framework.TestSuite;
 
 import org.eclipse.wst.xsl.launching.tests.LaunchingSuite;
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+/**
+ * This class specifies all the bundles of this component that provide a test
+ * suite to run during automated testing.
+ */
+public class AllTestsSuite extends TestSuite {
 
-public class AllTestsSuite extends TestCase {
 
-	public static TestSuite suite() {
-		TestSuite completeTestSuite = new TestSuite();
-		completeTestSuite.addTest(LaunchingSuite.suite());
-		return completeTestSuite;
+	public AllTestsSuite() {
+		super("All XSL Test Suites");
+		addTest(LaunchingSuite.suite());
+	}
+
+	/**
+	 * This is just need to run in development environment workbench.
+	 */
+	public void testAll() {
+		// this method needs to exist, but doesn't really do anything
+		// other than to signal to create an instance of this class.
+		// The rest it automatic from the tests added in constructor.
+
 	}
 }
