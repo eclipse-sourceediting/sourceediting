efeff96c93b69c950238e8392545951aae171de0
nitind <nitind>
2008-01-15 02:07:13 +0000

    add dtd.core.tests to repository

---
+++ tests/org.eclipse.wst.dtd.core.tests/src/org/eclipse/wst/dtd/core/tests/internal/DTDCoreTestsPlugin.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2008 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.dtd.core.tests.internal;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class DTDCoreTestsPlugin extends Plugin {
+
+	private static DTDCoreTestsPlugin plugin;
+
+	public static DTDCoreTestsPlugin getDefault() {
+		return plugin;
+	}
+
+	public DTDCoreTestsPlugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+}
