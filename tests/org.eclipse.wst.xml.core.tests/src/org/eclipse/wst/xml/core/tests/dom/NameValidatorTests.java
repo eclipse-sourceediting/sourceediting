ab3f0bf5c3cbe68050cd39d89b93bdb04db63465
david_williams <david_williams>
2008-02-25 15:55:11 +0000

    214416 Dot char is not escaped in XML10Names.jFlex

--- tests/org.eclipse.wst.xml.core.tests/src/org/eclipse/wst/xml/core/tests/dom/NameValidatorTests.java
+++ tests/org.eclipse.wst.xml.core.tests/src/org/eclipse/wst/xml/core/tests/dom/NameValidatorTests.java
@@ -25,29 +25,35 @@ public class NameValidatorTests extends TestCase {
 	}
 
 	private void doTest(int testNumber, String testString, Boolean expectedValidity) {
+		System.out.println();
 		boolean isValid = NameValidator.isValid(testString);
 		assertEquals("testNumber: " + testNumber, expectedValidity.booleanValue(), isValid);
+		System.out.println();
 	}
 	
 	public void testIsValid() {
 		Object[][] testees = new Object[][]{
-					{"initial",Boolean.TRUE},
-					{"foo",Boolean.TRUE},
-					{"4",Boolean.FALSE},
-					{"9999", Boolean.FALSE},
-					{"f9999", Boolean.TRUE},
-					{"", Boolean.FALSE},
-					{"got space", Boolean.FALSE},
-					{" spacebefore", Boolean.FALSE},
-					{"spaceafter ", Boolean.FALSE},
-					{"ns:namespace", Boolean.TRUE},
-					{":funnyns", Boolean.TRUE},
+					{"initial",Boolean.TRUE}, //0
+					{"foo",Boolean.TRUE},	  //1
+					{"4",Boolean.FALSE},	  //2
+					{"9999", Boolean.FALSE},  //3
+					{"f9999", Boolean.TRUE},  //4
+					{"", Boolean.FALSE},      //5
+					{"got space", Boolean.FALSE}, //6
+					{" spacebefore", Boolean.FALSE}, //7
+					{"spaceafter ", Boolean.FALSE},  //8
+					{"ns:namespace", Boolean.TRUE},  //9
+					{":funnyns", Boolean.TRUE},     //10
 					/* ISSUE: is "endns:" really valid name */
-					{"endns:", Boolean.TRUE},
-					{"us_underscore", Boolean.TRUE},
-					{"_underscore", Boolean.TRUE},
-					{"underscore_", Boolean.TRUE},
-					{"averylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersize", Boolean.TRUE},
+					{"endns:", Boolean.TRUE},   //11
+					{"us_underscore", Boolean.TRUE},  //12
+					{"_underscore", Boolean.TRUE},   //13
+					{"underscore_", Boolean.TRUE},    //14
+					{"averylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersizeaverylongnamethatshouldgooverbuffersize", Boolean.TRUE}, //15
+					{"<bracket", Boolean.FALSE}, //16
+					{"bracket<", Boolean.FALSE}, //17
+					{"bracket", Boolean.TRUE}, //18
+					{"per.iod", Boolean.TRUE}, //19
 					};
 		for (int i = 0; i < testees.length; i++) {
 			for (int j = 0; j < nTrials; j++) {
