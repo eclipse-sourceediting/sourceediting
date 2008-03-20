ea1f1c26b52206721fc852d7efdab51988e9f314
dacarver <dacarver>
2008-03-20 18:00:57 +0000

    Added copyright headers.

--- bundles/org.eclipse.wst.xsl.ui/src/org/eclipse/wst/xsl/ui/internal/validation/XSLErrorListener.java
+++ bundles/org.eclipse.wst.xsl.ui/src/org/eclipse/wst/xsl/ui/internal/validation/XSLErrorListener.java
@@ -1,9 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Standards for Technology in Automotive Retail and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Carver - STAR - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.wst.xsl.ui.internal.validation;
 
 import javax.xml.transform.*;
 import java.util.logging.*;
 
 
+/**
+ * This class listens for Errors tossed by a XSL Processor.
+ * 
+ * @author dcarver
+ *
+ */
 public class XSLErrorListener implements ErrorListener {
 
   private Logger logger;
