8ab2395a349a28fb2701e1f907a34253c26d1ff5
dacarver <dacarver>
2008-04-03 02:41:22 +0000

    *** empty log message ***

--- bundles/org.eclipse.wst.xsl.ui/src/org/eclipse/wst/xsl/ui/internal/contentassist/XSLVariableCustomCompletionProposal.java
+++ bundles/org.eclipse.wst.xsl.ui/src/org/eclipse/wst/xsl/ui/internal/contentassist/XSLVariableCustomCompletionProposal.java
@@ -11,12 +11,15 @@
  *******************************************************************************/
 package org.eclipse.wst.xsl.ui.internal.contentassist;
 
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.ITextViewerExtension5;
 import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.contentassist.IContextInformation;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
+import org.eclipse.wst.sse.core.internal.util.Debug;
 import org.eclipse.wst.sse.ui.internal.contentassist.CustomCompletionProposal;
 
 /**
@@ -86,6 +89,54 @@ public class XSLVariableCustomCompletionProposal extends CustomCompletionProposa
 	    int currentPosition =  getCursorPosition();
 	    int startOffset = document.getRegionAtCharacterOffset(offset).getStart();
 	    int existingLength = offset - startOffset;
+	    
+//		DC: Allows for replacement of text typed to this point.		
+//	    try {
+//			int caretOffset = viewer.getTextWidget().getCaretOffset();
+//			if (viewer instanceof ITextViewerExtension5) {
+//				ITextViewerExtension5 extension = (ITextViewerExtension5) viewer;
+//				caretOffset = extension.widgetOffset2ModelOffset(caretOffset);
+//			}
+//			else {
+//				caretOffset = viewer.getTextWidget().getCaretOffset() + viewer.getVisibleRegion().getOffset();
+//			}
+//
+//
+//			int endOffsetOfChanges = getReplacementString().length() + getReplacementOffset();
+//			// Insert the portion of the new text that comes after the
+//			// current caret position
+//			if (endOffsetOfChanges >= caretOffset) {
+//				int postCaretReplacementLength = getReplacementOffset() + getReplacementLength() - caretOffset;
+//				int preCaretReplacementLength = getReplacementString().length() - (endOffsetOfChanges - caretOffset);
+//				if (postCaretReplacementLength < 0) {
+//					if (Debug.displayWarnings) {
+//						System.out.println("** postCaretReplacementLength was negative: " + postCaretReplacementLength); //$NON-NLS-1$
+//					}
+//					// This is just a quick fix while I figure out what
+//					// replacement length is supposed to be
+//					// in each case, otherwise we'll get negative
+//					// replacment length sometimes
+//					postCaretReplacementLength = 0;
+//				}
+//				document.replace(caretOffset, postCaretReplacementLength, getReplacementString().substring(preCaretReplacementLength));
+//			}
+//			// Insert the portion of the new text that comes before the
+//			// current caret position
+//			// Done second since offsets would change for the post text
+//			// otherwise
+//			// Outright insertions are handled here
+//			if (caretOffset > getReplacementOffset()) {
+//				int preCaretTextLength = caretOffset - getReplacementOffset();
+//				document.replace(getReplacementOffset(), preCaretTextLength, getReplacementString().substring(0, preCaretTextLength));
+//			}
+//		}
+//		catch (BadLocationException x) {
+//			apply(document);
+//		}
+//		catch (StringIndexOutOfBoundsException e) {
+//			apply(document);
+//		}
+//
 	    
 		PositionBasedCompletionProposal proposal = 
 			new PositionBasedCompletionProposal(getReplacementString(), position, existingLength + getReplacementString().length());
