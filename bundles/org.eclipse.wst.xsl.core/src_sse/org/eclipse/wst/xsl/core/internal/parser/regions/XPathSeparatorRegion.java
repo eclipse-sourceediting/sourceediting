ca27d7fa72b438253deedec7dabae70c23f72fd4
dacarver <dacarver>
2008-04-08 19:15:39 +0000

    Corrected copyright headers.

--- bundles/org.eclipse.wst.xsl.core/src_sse/org/eclipse/wst/xsl/core/internal/parser/regions/XPathSeparatorRegion.java
+++ bundles/org.eclipse.wst.xsl.core/src_sse/org/eclipse/wst/xsl/core/internal/parser/regions/XPathSeparatorRegion.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2005 IBM Corporation and others.
+ * Copyright (c) 2008 Standards for Technology in Automotive Retail and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *     Jens Lukowski/Innoopract - initial renaming/restructuring
+ *     David Carver - initial API and implementation
  *     
  *******************************************************************************/
 package org.eclipse.wst.xsl.core.internal.parser.regions;
