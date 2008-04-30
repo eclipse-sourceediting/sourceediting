2526d92ffa5a10fefb876e64fad3531fa0d63f26
bchilds <bchilds>
2008-04-30 15:41:55 +0000

    turned debuging off

--- bundles/org.eclipse.wst.jsdt.web.core/src/org/eclipse/wst/jsdt/web/core/javascript/search/JsSearchSupport.java
+++ bundles/org.eclipse.wst.jsdt.web.core/src/org/eclipse/wst/jsdt/web/core/javascript/search/JsSearchSupport.java
@@ -55,8 +55,8 @@ public class JsSearchSupport {
     static final boolean DEBUG;
     static {
         String value = Platform.getDebugOption("org.eclipse.jst.jsp.core/debug/jspsearch"); //$NON-NLS-1$
-        DEBUG=true;
-        //  DEBUG = (value != null) && value.equalsIgnoreCase("true"); //$NON-NLS-1$
+       
+         DEBUG = (value != null) && value.equalsIgnoreCase("true"); //$NON-NLS-1$
     }
 
     private static JsSearchSupport singleton = null;
