3e1c830dbc13e00bb8823c3a7b99c3f1aaee5e5f
david_williams <david_williams>
2008-06-05 03:18:01 +0000

    235147 [acc] XML Design View doesn't show node-value content in high-contrast

--- bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/XMLTreeExtension.java
+++ bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/XMLTreeExtension.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2006 IBM Corporation and others.
+ * Copyright (c) 2004, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -50,7 +50,8 @@ public class XMLTreeExtension extends TreeExtension {
 		String[] properties = {STRUCTURE_PROPERTY, VALUE_PROPERTY};
 		setColumnProperties(properties);
 
-		f1 = tree.getDisplay().getSystemColor(SWT.COLOR_BLACK);
+		/* https://bugs.eclipse.org/bugs/show_bug.cgi?id=185851 - Used to be hardcoded as COLOR_BLACK */
+		f1 = tree.getDisplay().getSystemColor(SWT.COLOR_LIST_FOREGROUND);
 		Color background = tree.getDisplay().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
 
 		int r = Math.abs(background.getRed() - 125);
