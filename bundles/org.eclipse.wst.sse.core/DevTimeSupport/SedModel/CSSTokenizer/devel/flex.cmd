c4e77166f8dfd6741043bdf1508bd1c809831a80
nitind <nitind>
2008-10-02 03:13:29 +0000

    This commit was manufactured by cvs2svn to create tag 'v200810020300'.

--- bundles/org.eclipse.wst.sse.core/DevTimeSupport/SedModel/CSSTokenizer/devel/flex.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set JAVA_HOME=d:\jdk6_03
-set JFLEX_HOME=D:\JFlex\jflex-1.4.2
-
-%JAVA_HOME%\bin\java -Xmx470M -jar %JFLEX_HOME%\lib\JFlex.jar CSSTokenizer.jflex
-move CSSTokenizer.java ..\..\..\..\..\org.eclipse.wst.css.core\src\org\eclipse\wst\css\core\internal\parser\ && del CSSTokenizer.java*
