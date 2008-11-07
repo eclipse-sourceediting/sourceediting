4143f538774195a5905c6695e2977c6b14c3c234
nitind <nitind>
2008-11-07 23:30:39 +0000

    Roll back for [253954] Regression in Dali persistence.xml 'Synchronize Class List' and 'New Entity' actions

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/EditorExecutionContext.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/EditorExecutionContext.java
@@ -12,21 +12,15 @@
  *******************************************************************************/
 package org.eclipse.wst.sse.ui;
 
-import java.lang.reflect.InvocationTargetException;
-
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.jobs.ISchedulingRule;
-import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.progress.IWorkbenchSiteProgressService;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.wst.sse.core.internal.IExecutionDelegate;
-import org.eclipse.wst.sse.ui.internal.Logger;
-import org.eclipse.wst.sse.ui.internal.SSEUIPlugin;
+
+
 
 class EditorExecutionContext implements IExecutionDelegate {
+
 
 	StructuredTextEditor fEditor;
 
@@ -38,85 +32,54 @@ class EditorExecutionContext implements IExecutionDelegate {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * org.eclipse.wst.sse.core.internal.IExecutionDelegate#execute(java.lang
-	 * .Runnable)
+	 * @see org.eclipse.wst.sse.core.internal.document.IExecutionDelegate#execute(java.lang.Runnable)
 	 */
 	public void execute(final Runnable runnable) {
-		IWorkbench workbench = SSEUIPlugin.getInstance().getWorkbench();
+		IWorkbench workbench = PlatformUI.getWorkbench();
 		final Display display = workbench.getDisplay();
-		if (display.getThread() == Thread.currentThread()) { 
-			// *If already in display thread, we can simply run, "as usual"*/
+		if (display.getThread() == Thread.currentThread()) {
+			// if already in display thread, we can simply run, "as usual"
 			runnable.run();
-		}
-		else {
-			/*
-			 * This is the part that's really new, that accommodates a change
-			 * in a document from background thread, by forcing it on the
-			 * display thread.
-			 */
+		} else {
+			// this is the part that's really new, that
+			// accomidate's a change in a document
+			// from background thread, by forcing it on
+			// the display thread.
 			final StructuredTextEditor editor = fEditor;
-			IWorkbenchWindow workbenchWindow = editor.getEditorPart().getEditorSite().getWorkbenchWindow();
-			IWorkbenchSiteProgressService jobService = (IWorkbenchSiteProgressService) editor.getEditorPart().getSite().getAdapter(IWorkbenchSiteProgressService.class);
-			if (workbenchWindow != null && jobService != null) {
-				try {
-					jobService.runInUI(editor.getEditorPart().getEditorSite().getWorkbenchWindow(), wrapWithProgress(runnable), (ISchedulingRule) editor.getEditorInput().getAdapter(IResource.class));
-				}
-				catch (InvocationTargetException e) {
-					Logger.logException(e);
-				}
-				catch (InterruptedException e) {
-					Logger.logException(e);
-				}
-			}
-			else {
-				/*
-				 * If not in Display Thread, we "force" to run on display
-				 * thread. see editors begin/end background job for other
-				 * activities to best accommodate (for example, there is a
-				 * "timed delay" before the editor itself leaves
-				 * background-update mode). NOTE: this execute method itself
-				 * is always called from inside of an ILock block, so another
-				 * block is not not needed here for all these sycnExec's.
-				 */
-				display.syncExec(new Runnable() {
-					public void run() {
-						if (display != null && !display.isDisposed()) {
-							editor.beginBackgroundOperation();
-							try {
-								/*
-								 * Here's where the document
-								 * update/modification occurs
-								 */
-								runnable.run();
-							}
-							finally {
-								/*
-								 * This 'end' is just a signal to editor that
-								 * this particular update is done. Its up to
-								 * the editor to decide exactly when to leave
-								 * its "background mode"
-								 */
-								editor.endBackgroundOperation();
-							}
+			// if not in display thread, we "force" to run on display thread.
+			// see editors begin/end background job for other
+			// activities to best accomidate (for example, there
+			// is a "timed delay" before the editor itself leaves
+			// background-update mode).
+			// NOTE: this execute method itself is always called from
+			// inside of an ILock block, so another
+			// block is not not needed here for all these sycnExec's
+			display.syncExec(new Runnable() {
+				public void run() {
+					if (display != null && !display.isDisposed()) {
+						editor.beginBackgroundOperation();
+						try {
+							// here's where the document update/modification
+							// occurs
+							runnable.run();
+
+							// for future, possibly explore solutions such as
+							// this
+							//							IWorkbenchSiteProgressService jobService =
+							// (IWorkbenchSiteProgressService)
+							// editor.getEditorPart().getSite().getAdapter(IWorkbenchSiteProgressService.class);
+							//							jobService.runInUI(xxxxx)
+						} finally {
+							// this 'end' is just a signal to editor that this
+							// particular update is done. Its up to the editor
+							// to decide exactly when to leave its "background
+							// mode"
+							editor.endBackgroundOperation();
 						}
 					}
-				});
-			}
+				}
+			});
 		}
 	}
-
-	/**
-	 * @param runnable
-	 * @return
-	 */
-	private IRunnableWithProgress wrapWithProgress(final Runnable runnable) {
-		return new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-				if (!monitor.isCanceled()) {
-					runnable.run();
-				}
-			}
-		};
-	}
 }
+
