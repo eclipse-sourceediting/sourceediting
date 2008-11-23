fb76e2fbaf9f53249f78e006bae6b1a17cbcdbed
dacarver <dacarver>
2008-11-23 03:45:32 +0000

    added XpathToNode tests.

--- tests/org.eclipse.wst.xml.xpath.core.tests/src/org/eclipse/wst/xsl/internal/core/xpath/tests/TestXSLXPathHelper.java
+++ tests/org.eclipse.wst.xml.xpath.core.tests/src/org/eclipse/wst/xsl/internal/core/xpath/tests/TestXSLXPathHelper.java
@@ -1,9 +1,16 @@
 package org.eclipse.wst.xsl.internal.core.xpath.tests;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 
 import junit.framework.TestCase;
 import org.eclipse.wst.xml.xpath.core.util.XSLTXPathHelper;
+import org.w3c.dom.Attr;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 public class TestXSLXPathHelper extends TestCase {
 
@@ -24,4 +31,44 @@ public class TestXSLXPathHelper extends TestCase {
 			throw new Exception(ex.getMessage());
 		}
 	}
+	
+	public void testcreateXPathFromNode() throws Exception {
+		DOMImplementation domImpl = createDOMImpl();
+		Document document = domImpl.createDocument(null, "test", null);
+		assertEquals("Unexpected XPath value", "/test", XSLTXPathHelper.calculateXPathToNode(document.getFirstChild()));
+	}
+
+	private DOMImplementation createDOMImpl()
+			throws ParserConfigurationException {
+		DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = documentFactory.newDocumentBuilder();
+		DOMImplementation domImpl =  builder.getDOMImplementation();
+		return domImpl;
+	}
+	
+	public void testCreateXPathFromNodeMultiple() throws Exception {
+		DOMImplementation domImpl = createDOMImpl();
+		Document document = domImpl.createDocument(null, "test", null);
+		Element rootelem = document.getDocumentElement();
+		Element elem1 = document.createElement("testNode1");
+		Element elem2 = document.createElement("testNode1");
+		rootelem.appendChild(elem1);
+		rootelem.appendChild(elem2);
+		assertEquals("Unexepected XPath value", "/test/testNode1[2]", XSLTXPathHelper.calculateXPathToNode(elem2));
+	}
+	
+	public void testCreateXPathFromNodeAttribute() throws Exception {
+		DOMImplementation domImpl = createDOMImpl();
+		Document document = domImpl.createDocument(null, "test", null);
+		Element rootelem = document.getDocumentElement();
+		Element elem1 = document.createElement("testNode1");
+		Element elem2 = document.createElement("testNode1");
+		rootelem.appendChild(elem1);
+		rootelem.appendChild(elem2);
+		Attr attribute = document.createAttribute("attr");
+		attribute.setValue("some value");
+		elem2.setAttributeNode(attribute);
+		assertEquals("Unexpected XPath value", "/test/testNode1[2]/@attr", XSLTXPathHelper.calculateXPathToNode(attribute));
+	}
+
 }
