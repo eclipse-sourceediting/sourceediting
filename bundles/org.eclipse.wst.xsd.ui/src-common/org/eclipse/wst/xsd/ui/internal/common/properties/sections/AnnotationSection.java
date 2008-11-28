925917660b8e05769e2c6d571b3edb3ec1147b5c
kchong <kchong>
2008-11-28 23:19:09 +0000

    [248518] [Properties] The deleted string remains in Documentation tab of Properties view.

--- bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/AnnotationSection.java
+++ bundles/org.eclipse.wst.xsd.ui/src-common/org/eclipse/wst/xsd/ui/internal/common/properties/sections/AnnotationSection.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -115,6 +115,10 @@ public class AnnotationSection extends AbstractSection
           {
             simpleText.setText(doSerialize(docElement));
           }
+        }
+        else
+        {
+            simpleText.setText("");
         }
       }
       catch (Exception e)
