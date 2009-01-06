d364bb017c1b24942d4f123831d8b1b4a147b7b0
nitind <nitind>
2009-01-06 04:52:14 +0000

    [259202] jsp:useBean needs scope around contents

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/JSPCoreMessages.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/JSPCoreMessages.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -49,6 +49,8 @@ public class JSPCoreMessages extends NLS {
 	public static String JSPTranslator_1;
 	public static String JSPTranslator_2;
 	public static String JSPTranslator_3;
+	public static String JSPTranslator_4;
+	public static String JSPTranslator_5;
 	
 	/**
 	 * @deprecated
--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/java/IJSPProblem.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/java/IJSPProblem.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.jst.jsp.core.internal.java;
 
 import org.eclipse.jdt.core.compiler.IProblem;
@@ -16,6 +26,8 @@ public interface IJSPProblem extends IProblem {
 	int UseBeanAmbiguousType  = F_PROBLEM_ID_LITERAL + 8;
 	int StartCustomTagMissing  = F_PROBLEM_ID_LITERAL + 9;
 	int EndCustomTagMissing  = F_PROBLEM_ID_LITERAL + 10;
+	int UseBeanStartTagMissing  = F_PROBLEM_ID_LITERAL + 11;
+	int UseBeanEndTagMissing  = F_PROBLEM_ID_LITERAL + 12;
 	
 	/**
 	 * @return the ID of this JSP problem
