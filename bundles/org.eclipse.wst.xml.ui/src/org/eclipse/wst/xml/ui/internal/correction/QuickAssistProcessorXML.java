edcfad71b48428d648067e40714ccf79ff970a01
nitind <nitind>
2009-01-07 20:21:49 +0000

    [nobug] doc

--- bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/correction/QuickAssistProcessorXML.java
+++ bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/correction/QuickAssistProcessorXML.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2008 IBM Corporation and others.
+ * Copyright (c) 2001, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,7 +39,7 @@ import org.w3c.dom.Node;
 
 /**
  * @deprecated since 2.0 RC0 Use
- *             org.eclipse.jface.text.quickassist.IQuickAssistProcessor;
+ *             org.eclipse.jface.text.quickassist.IQuickAssistProcessor and XMLQuickAssistProcessor
  */
 public class QuickAssistProcessorXML implements IQuickAssistProcessor {
 	/*
