a64c79c4fbf337a749ace6744cad3b32e8434b6d
dacarver <dacarver>
2009-01-30 07:12:55 +0000

    Add some unit tests and fix Xpath 2.0 plugin.

---
+++ bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/XPath2Plugin.java
@@ -0,0 +1,11 @@
+package org.eclipse.wst.xml.xpath2.processor;
+
+import org.eclipse.core.runtime.Plugin;
+
+public class XPath2Plugin extends Plugin {
+
+	public XPath2Plugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
