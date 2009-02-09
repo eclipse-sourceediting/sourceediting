70ce957fe459a8ea3b0e9de57a953ca99f53dbb2
nsandonato <nsandonato>
2009-02-09 19:33:34 +0000

    [192572] Selectors should not be spell-checked

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/spelling/SpellcheckStrategy.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/spelling/SpellcheckStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2008 IBM Corporation and others.
+ * Copyright (c) 2006, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -235,8 +235,9 @@ public class SpellcheckStrategy extends StructuredTextReconcilingStrategy {
 				}
 				if (documentRegion.getFirstRegion() instanceof ForeignRegion)
 					return false;
-				if (documentRegion.getRegions().size() == 1)
-					return true;
+//				[192572] Simple regions were being spellchecked just for the sake of them being simple
+//				if (documentRegion.getRegions().size() == 1)
+//					return true;
 				return false;
 			}
 		}
