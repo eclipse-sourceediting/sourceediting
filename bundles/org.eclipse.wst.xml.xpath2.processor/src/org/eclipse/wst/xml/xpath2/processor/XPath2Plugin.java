b5cd33f4739e1ee7393f2ee63c25989e29cd19b9
dacarver <dacarver>
2009-02-09 21:35:17 +0000

    Fixed copyright headers.

--- bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/XPath2Plugin.java
+++ bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/XPath2Plugin.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Standards for Technology in Automotive Retail and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Carver (STAR) - initial api and implementation 
+ *******************************************************************************/
+
 package org.eclipse.wst.xml.xpath2.processor;
 
 import org.eclipse.core.runtime.Plugin;
