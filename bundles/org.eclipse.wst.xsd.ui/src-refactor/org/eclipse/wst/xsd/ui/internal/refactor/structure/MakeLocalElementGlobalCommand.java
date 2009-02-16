1cf66746a806ecfd9364c89ee622ed4fb37560f8
vbaciu <vbaciu>
2009-02-16 22:50:47 +0000

    [197298] [Editor] Undo local to global change is not reflected in the design view

--- bundles/org.eclipse.wst.xsd.ui/src-refactor/org/eclipse/wst/xsd/ui/internal/refactor/structure/MakeLocalElementGlobalCommand.java
+++ bundles/org.eclipse.wst.xsd.ui/src-refactor/org/eclipse/wst/xsd/ui/internal/refactor/structure/MakeLocalElementGlobalCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,89 +10,72 @@
  *******************************************************************************/
 package org.eclipse.wst.xsd.ui.internal.refactor.structure;
 
-import org.eclipse.xsd.XSDComplexTypeContent;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.wst.xsd.ui.internal.common.util.XSDCommonUIUtils;
 import org.eclipse.xsd.XSDConcreteComponent;
 import org.eclipse.xsd.XSDElementDeclaration;
-import org.eclipse.xsd.XSDFactory;
 import org.eclipse.xsd.XSDModelGroup;
-import org.eclipse.xsd.XSDParticle;
-import org.eclipse.xsd.XSDTypeDefinition;
+import org.eclipse.xsd.XSDSchema;
+
 
 public final class MakeLocalElementGlobalCommand extends AbstractCommand
 {
-	
-  public MakeLocalElementGlobalCommand
-    (XSDConcreteComponent element)
+  public MakeLocalElementGlobalCommand(XSDConcreteComponent element)
   {
     super(element.getContainer());
     setModelObject(element);
   }
-  
+
   public void run()
   {
-    
-   if(getModelObject() instanceof XSDElementDeclaration){
-   
-	   XSDElementDeclaration element = (XSDElementDeclaration)getModelObject();
- 	XSDConcreteComponent parent = getParent();
- 	XSDConcreteComponent container = parent.getContainer();
- 	
- 	// clone element with it's content and set it global
-	XSDConcreteComponent  elementDecl = ((XSDElementDeclaration)getModelObject()).cloneConcreteComponent(true, true);
- 	container.getSchema().getContents().add(elementDecl);
- 	
- 	// create local element and set it's reference to the global one
- 	XSDElementDeclaration elementRef = 
-	      XSDFactory.eINSTANCE.createXSDElementDeclaration();
-	elementRef.setValue(element.getValue());
-    elementRef.setResolvedElementDeclaration((XSDElementDeclaration)elementDecl); 
-    
-    // now set content models
- 	if(parent instanceof XSDComplexTypeContent){
- 		if(container instanceof XSDModelGroup){
- 			XSDModelGroup modelGroup = (XSDModelGroup)container;
- 			// disconnect parent from its container
- 			int index = modelGroup.getContents().indexOf(parent);
- 			 XSDParticle particle = 
- 			      XSDFactory.eINSTANCE.createXSDParticle();
- 		    particle.setContent(elementRef);
- 		    modelGroup.getContents().add(index, particle); 
-            // Copy over the max/minOccurs from the old local to the element ref
-            if (parent instanceof XSDParticle) {
-              XSDParticle parentParticle = (XSDParticle)parent;
-              
-              if (parentParticle.isSetMinOccurs()) {
-                particle.setMinOccurs(parentParticle.getMinOccurs());
-                parentParticle.unsetMinOccurs();
-              }
-              
-              if (parentParticle.isSetMaxOccurs()) {
-                particle.setMaxOccurs(parentParticle.getMaxOccurs());
-                parentParticle.unsetMaxOccurs();
-              }
-            }          
-            element.unsetForm();
- 		   
- 			modelGroup.getContents().remove(parent);
- 		    modelGroup.updateElement(true);
-  		    formatChild(modelGroup.getElement());
- 		}
- 	}
- 	else if(parent instanceof XSDTypeDefinition){
-		 		
- 	}
- 	
- 	container.getSchema().updateElement(true);
-    formatChild(elementDecl.getElement());
-  
-   }
+    if (getModelObject() instanceof XSDElementDeclaration)
+    {
 
+      XSDElementDeclaration localElementDeclaration = (XSDElementDeclaration)getModelObject();
+      XSDConcreteComponent parent = getParent();
+      XSDConcreteComponent container = parent.getContainer();
+
+      // Clone the local element with its content and set it global
+
+      XSDElementDeclaration globalElementDeclaration = (XSDElementDeclaration)localElementDeclaration.cloneConcreteComponent(true, false);
+
+      // The schema may already have a global element declaration with this name. In that case, ensure the name of the newly created
+      // element does not collide with an existing one.
+
+      XSDSchema schema = container.getSchema();
+      String localElementName = localElementDeclaration.getName();
+      XSDElementDeclaration existingGlobalElement = schema.resolveElementDeclaration(localElementName);
+      boolean elementExists = existingGlobalElement != null && existingGlobalElement.getSchema() != null;
+      if (elementExists)
+      {
+        String newElementName = XSDCommonUIUtils.createUniqueElementName(localElementName, schema.getElementDeclarations());
+        globalElementDeclaration.setName(newElementName);
+      }
+
+      EList schemaContents = schema.getContents();
+      schemaContents.add(globalElementDeclaration);
+
+      // Modify the local element to become a reference to the global element.
+
+      localElementDeclaration.setName(null);
+      localElementDeclaration.setTypeDefinition(null);
+      localElementDeclaration.setAnonymousTypeDefinition(null);
+      localElementDeclaration.setResolvedElementDeclaration(globalElementDeclaration);
+      XSDModelGroup modelGroup = (XSDModelGroup)container;
+
+      // Format the markup. 
+
+      formatChild(modelGroup.getElement());
+      formatChild(globalElementDeclaration.getElement());
+    }
   }
-	/* (non-Javadoc)
-	 * @see org.eclipse.wst.xsd.ui.internal.commands.AbstractCommand#adopt(org.eclipse.xsd.XSDConcreteComponent)
-	 */
-	protected boolean adopt(XSDConcreteComponent model) {
-		// TODO Auto-generated method stub
-		return true;
-	}
+
+  /* (non-Javadoc)
+   * @see org.eclipse.wst.xsd.ui.internal.commands.AbstractCommand#adopt(org.eclipse.xsd.XSDConcreteComponent)
+   */
+  protected boolean adopt(XSDConcreteComponent model)
+  {
+    return true;
+  }
 }
