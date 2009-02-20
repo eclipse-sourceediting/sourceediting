603009d3a6f4d7d2f0e2ff14534e98db428a2fe5
dacarver <dacarver>
2009-02-20 21:52:00 +0000

    Activate plugin when loaded.

--- tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/test/XPath20TestPlugin.java
+++ tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/test/XPath20TestPlugin.java
@@ -1,11 +1,46 @@
 package org.eclipse.wst.xml.xpath2.processor.test;
 
 import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
 
 public class XPath20TestPlugin extends Plugin {
 
+	// The shared instance
+	private static XPath20TestPlugin plugin;
+	
 	public XPath20TestPlugin() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+	
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static XPath20TestPlugin getDefault() {
+		return plugin;
+	}
+	
 }
