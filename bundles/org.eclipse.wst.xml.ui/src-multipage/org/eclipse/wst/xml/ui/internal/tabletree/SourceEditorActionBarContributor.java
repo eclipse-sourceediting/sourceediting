e0152b44b4006b0bfe6d75c6bbf985ce994bbf24
nsandonato <nsandonato>
2009-03-12 02:08:17 +0000

    [267294] SourceEditorActionBarContributor holds on to the editor

--- bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/SourceEditorActionBarContributor.java
+++ bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/SourceEditorActionBarContributor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -79,6 +79,8 @@ public class SourceEditorActionBarContributor extends MultiPageEditorActionBarCo
 		if (extendedContributor != null) {
 			extendedContributor.dispose();
 		}
+		
+		multiPageEditor = null;
 	}
 
 	/**
