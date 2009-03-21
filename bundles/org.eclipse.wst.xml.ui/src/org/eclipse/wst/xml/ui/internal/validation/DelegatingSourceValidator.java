48d0e080ae21c7f176b404192987a9471d62fac1
vbaciu <vbaciu>
2009-03-21 23:10:20 +0000

    [268803] [Validation] Wrong start index in TEXT squiggle computation

--- bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/validation/DelegatingSourceValidator.java
+++ bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/validation/DelegatingSourceValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2008 IBM Corporation and others.
+ * Copyright (c) 2005, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -464,7 +464,7 @@ public abstract class DelegatingSourceValidator implements IValidator {
 
 						}
 						start = start + index;
-						startEndPositions[0] = start + index;
+						startEndPositions[0] = start;
 						startEndPositions[1] = start + value.trim().length();
 					}
 					else if (node.getNodeType() == Node.ELEMENT_NODE) {
