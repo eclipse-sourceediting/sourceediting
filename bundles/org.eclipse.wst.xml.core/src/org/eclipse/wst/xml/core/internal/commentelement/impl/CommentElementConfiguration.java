96b22362ae5a13976d3831e507368142ac385971
nitind <nitind>
2009-04-10 20:22:54 +0000

    [255582] error in log importing using projects

--- bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/commentelement/impl/CommentElementConfiguration.java
+++ bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/commentelement/impl/CommentElementConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -200,7 +200,7 @@ public class CommentElementConfiguration {
 			}
 		}
 		if (fPrefix == null) {
-			fPrefix = new String[]{""}; //$NON-NLS-1$
+			fPrefix = new String[0];
 		}
 		return fPrefix;
 	}
