560575df339b813859a65044c9ad2b202ecb9e05
nitind <nitind>
2009-04-10 23:08:19 +0000

    [nobug] doc

--- bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/XMLMultiPageEditorPart.java
+++ bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/XMLMultiPageEditorPart.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others. All rights reserved. This
+ * Copyright (c) 2004, 2009 IBM Corporation and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and
  * is available at http://www.eclipse.org/legal/epl-v10.html
@@ -265,7 +265,7 @@ public class XMLMultiPageEditorPart extends MultiPageEditorPart {
 	}
 
 	/**
-	 * Internal IPropertyListener
+	 * Internal IPropertyListener on the source page
 	 */
 	class PropertyListener implements IPropertyListener {
 		public void propertyChanged(Object source, int propId) {
