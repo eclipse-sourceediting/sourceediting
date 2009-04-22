08335c94494db5568193cc12659fd62c01699119
nitind <nitind>
2009-04-22 06:09:09 +0000

    [nobug] avoid Iterator object creation

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/contentmodel/tld/TLDCMDocumentManager.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/contentmodel/tld/TLDCMDocumentManager.java
@@ -813,9 +813,9 @@ public class TLDCMDocumentManager implements ITaglibIndexListener {
 
 	public List getCMDocumentTrackers(int offset) {
 		List validDocs = new ArrayList();
-		Iterator alldocs = getTaglibTrackers().iterator();
-		while (alldocs.hasNext()) {
-			TaglibTracker aTracker = (TaglibTracker) alldocs.next();
+		Object[] alldocs = getTaglibTrackers().toArray();
+		for (int i = 0; i < alldocs.length; i++) {
+			TaglibTracker aTracker = (TaglibTracker) alldocs[i];
 			if (aTracker.getStructuredDocumentRegion().getStartOffset() <= offset || offset < 0) {
 				validDocs.add(aTracker);
 			}
@@ -825,11 +825,11 @@ public class TLDCMDocumentManager implements ITaglibIndexListener {
 
 	public List getCMDocumentTrackers(String prefix, int offset) {
 		List validDocs = new ArrayList();
-		Iterator alldocs = getTaglibTrackers().iterator();
-		while (alldocs.hasNext()) {
-			TaglibTracker aTracker = (TaglibTracker) alldocs.next();
+		Object[] alldocs = getTaglibTrackers().toArray();
+		for (int i = 0; i < alldocs.length; i++) {
+			TaglibTracker aTracker = (TaglibTracker) alldocs[i];
 			/**
-			 * '<' is used to support the immediate use of a custom tag in jspx files
+			 * '<' is used to support the immediate use of a custom tag in jspx files (instead of '<=')
 			 */
 			if ((aTracker.getStructuredDocumentRegion().getStartOffset() <= offset || offset < 0) && aTracker.getPrefix().equals(prefix)) {
 				validDocs.add(aTracker);
