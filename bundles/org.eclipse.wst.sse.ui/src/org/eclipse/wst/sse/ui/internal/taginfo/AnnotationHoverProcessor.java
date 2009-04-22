1d5055bd5fdca710aa49fa1c52ad4073606c25ce
nitind <nitind>
2009-04-22 04:00:33 +0000

    [nobug] avoid possible NPE

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/taginfo/AnnotationHoverProcessor.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/taginfo/AnnotationHoverProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -167,7 +167,7 @@ public class AnnotationHoverProcessor extends AbstractHoverProcessor {
 				if (!isAnnotationValid(a))
 					continue;
 				Position p = model.getPosition(a);
-				if (p.includes(offset)) {
+				if (p != null && p.includes(offset)) {
 					// find the smallest region containing offset
 					if ((hoverRegion == null) || (hoverRegion.getLength() > p.getLength())) {
 						hoverRegion = new Region(p.getOffset(), p.getLength());
