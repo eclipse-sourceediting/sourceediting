a632ccb60d1c42c966e5b1720ff4704d0e86067c
nsandonato <nsandonato>
2009-04-24 21:02:39 +0000

    [273651] NullPointerException in LineStyleProviderForEmbeddedCSS

--- bundles/org.eclipse.wst.css.ui/src/org/eclipse/wst/css/ui/internal/style/LineStyleProviderForEmbeddedCSS.java
+++ bundles/org.eclipse.wst.css.ui/src/org/eclipse/wst/css/ui/internal/style/LineStyleProviderForEmbeddedCSS.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,6 +28,9 @@ public class LineStyleProviderForEmbeddedCSS extends LineStyleProviderForCSS {
 		int regionStart = typedRegion.getOffset();
 		int regionEnd = regionStart + typedRegion.getLength();
 		IStructuredDocumentRegion wholeRegion = getDocument().getRegionAtCharacterOffset(regionStart);
+
+		if (wholeRegion == null)
+			return false;
 
 		List tokens;
 		int offset;
