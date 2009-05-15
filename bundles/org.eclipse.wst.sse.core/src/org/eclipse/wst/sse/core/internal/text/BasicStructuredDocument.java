bb86a58c4379b45b33b2231c544f285e7ebefbd2
nsandonato <nsandonato>
2009-05-15 14:55:39 +0000

    [276199] BasicStructuredDocument implementation of stopRewriteSession inconsistent with AbstractDocument

--- bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/text/BasicStructuredDocument.java
+++ bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/text/BasicStructuredDocument.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2008 IBM Corporation and others.
+ * Copyright (c) 2001, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2844,9 +2844,9 @@ public class BasicStructuredDocument implements IStructuredDocument, IDocumentEx
 				extension.stopRewriteSession(session, get());
 			}
 
+			fActiveRewriteSession = null;
 			DocumentRewriteSessionEvent event = new DocumentRewriteSessionEvent(this, session, DocumentRewriteSessionEvent.SESSION_STOP);
 			fireDocumentRewriteSessionEvent(event);
-			fActiveRewriteSession = null;
 		}
 	}
 
