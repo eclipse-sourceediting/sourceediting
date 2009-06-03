082e934be90f02fab1a373204b3941763221174d
cjaun <cjaun>
2009-06-03 19:07:40 +0000

    [279001] Clean up of IIncludePathEntry, IJavaScriptElement, and JavaScriptCore

--- bundles/org.eclipse.wst.jsdt.web.ui/src/org/eclipse/wst/jsdt/web/ui/internal/taginfo/JSDTHoverProcessor.java
+++ bundles/org.eclipse.wst.jsdt.web.ui/src/org/eclipse/wst/jsdt/web/ui/internal/taginfo/JSDTHoverProcessor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -74,7 +74,7 @@ public class JSDTHoverProcessor extends AbstractHoverProcessor {
 				if (reader != null) {
 					HTMLPrinter.addParagraph(buffer, reader);
 				}
-			} else if (curr.getElementType() == IJavaScriptElement.LOCAL_VARIABLE || curr.getElementType() == IJavaScriptElement.TYPE_PARAMETER) {
+			} else if (curr.getElementType() == IJavaScriptElement.LOCAL_VARIABLE) {
 				HTMLPrinter.addSmallHeader(buffer, getInfoText(curr));
 			}
 		}
