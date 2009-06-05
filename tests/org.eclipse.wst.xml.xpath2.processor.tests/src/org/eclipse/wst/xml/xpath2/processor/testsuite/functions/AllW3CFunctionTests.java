9b9c3a8333a075fb14a393ee2f0b94c755a05df2
dacarver <dacarver>
2009-06-05 01:41:25 +0000

    [262765] [xpath2] W3C Test Suite - Additional W3C Tests. https://bugs.eclipse.org/bugs/show_bug.cgi?id=262765

--- tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/testsuite/functions/AllW3CFunctionTests.java
+++ tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/testsuite/functions/AllW3CFunctionTests.java
@@ -9,17 +9,21 @@ public class AllW3CFunctionTests {
 		TestSuite suite = new TestSuite(
 				"Test for org.eclipse.wst.xml.xpath2.processor.testsuite.functions");
 		//$JUnit-BEGIN$
+		suite.addTestSuite(SubstringFuncTest.class);
 		suite.addTestSuite(ErrorFuncTest.class);
 		suite.addTestSuite(StaticBaseURIFuncTest.class);
 		suite.addTestSuite(compareFuncTest.class);
 		suite.addTestSuite(BaseURIFuncTest.class);
+		suite.addTestSuite(StringJoinFuncTest.class);
 		suite.addTestSuite(TraceFuncTest.class);
+		suite.addTestSuite(ConcatFuncTest.class);
 		suite.addTestSuite(StringToCodepointFuncTest.class);
 		suite.addTestSuite(StringFuncTest.class);
 		suite.addTestSuite(CeilingFuncTest.class);
 		suite.addTestSuite(CodepointToStringFuncTest.class);
 		suite.addTestSuite(FloorFuncTest.class);
 		suite.addTestSuite(RoundEvenFuncTest.class);
+		suite.addTestSuite(CodepointEqualTest.class);
 		suite.addTestSuite(ABSFuncTest.class);
 		suite.addTestSuite(NilledFuncTest.class);
 		suite.addTestSuite(DataFuncTest.class);
