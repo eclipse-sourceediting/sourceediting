47a45165e87a248a703bc973ae9c24307a95dab6
dacarver <dacarver>
2009-06-14 03:21:37 +0000

    [262765] [xpath2] Fixed parsing and creation of date based on gMonth. https://bugs.eclipse.org/bugs/show_bug.cgi?id=262765

--- bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/types/XSGMonth.java
+++ bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/types/XSGMonth.java
@@ -6,7 +6,8 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Andrea Bittau - initial API and implementation from the PsychoPath XPath 2.0 
+ *     Andrea Bittau - initial API and implementation from the PsychoPath XPath 2.0
+ *     David Carver (STAR) - bug 262765 - Fixed parsing of gMonth values 
  *******************************************************************************/
 
 package org.eclipse.wst.xml.xpath2.processor.internal.types;
@@ -68,25 +69,46 @@ public class XSGMonth extends CalendarType implements CmpEq {
 	public static XSGMonth parse_gMonth(String str) {
 		// XXX
 
-		String lame = "1983-";
-		String lame2 = "-29T00:00:00.0";
+		String startdate = "1972-";
+		String starttime = "T00:00:00";
 		boolean tz = false;
 
-		int index = str.indexOf('+', 0);
+		int index = str.lastIndexOf('+', str.length());
+		
 		if (index == -1)
-			index = str.indexOf('-', 0);
+			index = str.lastIndexOf('-');
 		if (index == -1)
-			index = str.indexOf('Z', 0);
+			index = str.lastIndexOf('Z', str.length());
 		if (index != -1) {
-			lame += str.substring(0, index);
-			lame += lame2;
-			lame += str.substring(index, str.length());
-			tz = true;
+			int zIndex = str.lastIndexOf('Z', str.length());
+			
+			String[] split = str.split("-");
+			startdate += split[2].replace("Z", "") + "-01";
+			
+			if (split.length > 3) {
+				String[] timesplit = split[3].split(":");
+				if (timesplit.length < 3) {
+					starttime = "T";
+					for (int cnt = 0; cnt < timesplit.length; cnt++) {
+						starttime += timesplit[cnt] + ":";
+					}
+					starttime += "00";
+				} else {
+					starttime += timesplit[0] + ":" + timesplit[1] + ":" + timesplit[2];
+				}
+			}
+			startdate = startdate.trim();
+			startdate += starttime;
+
+			if (zIndex != -1) {
+				startdate += str.substring(zIndex);
+				tz = true;
+			}
 		} else {
-			lame += str + lame2;
+			startdate += str + starttime;
 		}
 
-		XSDateTime dt = XSDateTime.parseDateTime(lame);
+		XSDateTime dt = XSDateTime.parseDateTime(startdate);
 		if (dt == null)
 			return null;
 
