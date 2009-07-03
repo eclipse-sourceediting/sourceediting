cf70c60cba788d6fbedcd75c4caca433ac44d96d
dacarver <dacarver>
2009-07-03 06:37:58 +0000

    [280987] [xpath2] regenerated grammar to handle precision issues with IntegerLiteral and DecimalLiteral https://bugs.eclipse.org/bugs/show_bug.cgi?id=280987

--- bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/XPathFlex.java
+++ bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/XPathFlex.java
@@ -8,7 +8,8 @@
 // * http://www.eclipse.org/legal/epl-v10.html
 // *
 // * Contributors:
-// *     Andrea Bittau - initial API and implementation from the PsychoPath XPath 2.0 
+// *     Andrea Bittau - initial API and implementation from the PsychoPath XPath 2.0
+// *     David Carver - bug 280987 - fixed literal issues for integer and decimal
 // *******************************************************************************/
 
 
