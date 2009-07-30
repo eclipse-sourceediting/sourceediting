ccabd32304f49887e458202d4395a6074970f0e5
dacarver <dacarver>
2009-07-30 01:03:40 +0000

    [262765] Added Passing Core Tests to Testsuite for Build https://bugs.eclipse.org/bugs/show_bug.cgi?id=262765

--- tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/test/AllW3CXPath20Tests.java
+++ tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/test/AllW3CXPath20Tests.java
@@ -11,27 +11,12 @@
  *******************************************************************************/
 package org.eclipse.wst.xml.xpath2.processor.test;
 
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.AbbrAxesTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.AxesTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.BooleanEqualTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.BooleanGTTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.BooleanLTTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.CombNodeSeqTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.NameTestTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.NodeTestTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.ParenExprTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.SequenceTypeSyntaxTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.core.UnabbrAxesTest;
 import org.eclipse.wst.xml.xpath2.processor.testsuite.dates.DateAddYMDTest;
 import org.eclipse.wst.xml.xpath2.processor.testsuite.dates.DateTimeSubtractYMDTest;
 import org.eclipse.wst.xml.xpath2.processor.testsuite.dates.YearMonthDurationAddDTTest;
 import org.eclipse.wst.xml.xpath2.processor.testsuite.dates.YearMonthDurationSubtractTest;
 import org.eclipse.wst.xml.xpath2.processor.testsuite.numeric.AllW3CNumericTests;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.numeric.NumericEqualTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.numeric.NumericGTTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.numeric.NumericLTTest;
-import org.eclipse.wst.xml.xpath2.processor.testsuite.numeric.NumericUnaryPlusTest;
-
+import org.eclipse.wst.xml.xpath2.processor.test.AllW3CCoreTests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -41,21 +26,11 @@ public class AllW3CXPath20Tests {
 		TestSuite suite = new TestSuite("Test for W3C XPath 2.0 test suite.");
 		//$JUnit-BEGIN$
 		suite.addTest(AllW3CNumericTests.suite());
-		suite.addTestSuite(SequenceTypeSyntaxTest.class);
-		suite.addTestSuite(CombNodeSeqTest.class);
-		suite.addTestSuite(BooleanLTTest.class);
-		suite.addTestSuite(AxesTest.class);
-		suite.addTestSuite(NodeTestTest.class);
-		suite.addTestSuite(BooleanEqualTest.class);
-		suite.addTestSuite(AbbrAxesTest.class);
-		suite.addTestSuite(NameTestTest.class);
+		suite.addTest(AllW3CCoreTests.suite());
 		suite.addTestSuite(YearMonthDurationSubtractTest.class);
-		suite.addTestSuite(BooleanGTTest.class);
 		suite.addTestSuite(DateTimeSubtractYMDTest.class);
 		suite.addTestSuite(YearMonthDurationAddDTTest.class);
 		suite.addTestSuite(DateAddYMDTest.class);
-		suite.addTestSuite(ParenExprTest.class);
-		suite.addTestSuite(UnabbrAxesTest.class);
 		//$JUnit-END$
 		return suite;
 	}
