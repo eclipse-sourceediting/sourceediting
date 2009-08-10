c656a5e46e24b82e0e31b8b0baf32c13f79123f9
nsandonato <nsandonato>
2009-08-10 15:43:01 +0000

    [105361] occurrences in file message has no number

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/search/OccurrencesSearchQuery.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/search/OccurrencesSearchQuery.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -169,9 +169,7 @@ public class OccurrencesSearchQuery extends BasicSearchQuery {
 	 * @return
 	 */
 	private String getOccurrencesCountText() {
-		String count = ""; //$NON-NLS-1$
-		// pa_TODO dynamically change count
-		return count;
+		return "" + this.getMatches().length; //$NON-NLS-1$
 	}
 
 	public ISearchResult getSearchResult() {
