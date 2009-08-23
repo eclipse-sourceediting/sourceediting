e23c669886b2e20b647339c378ed184af221a960
dacarver <dacarver>
2009-08-23 20:55:54 +0000

    [nobug] removed unused plugin dependency org.apache.common.lang

--- bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/types/XSString.java
+++ bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/internal/types/XSString.java
@@ -13,14 +13,11 @@
 
 package org.eclipse.wst.xml.xpath2.processor.internal.types;
 
-import org.apache.commons.lang.CharSet;
 import org.eclipse.wst.xml.xpath2.processor.DynamicError;
 import org.eclipse.wst.xml.xpath2.processor.ResultSequence;
 import org.eclipse.wst.xml.xpath2.processor.ResultSequenceFactory;
-import org.eclipse.wst.xml.xpath2.processor.internal.*;
 import org.eclipse.wst.xml.xpath2.processor.internal.function.*;
 
-import java.io.UnsupportedEncodingException;
 import java.util.*;
 
 /**
