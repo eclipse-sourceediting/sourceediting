c1d9ceb2f77f6bebe440f433cca3a82290a64d85
nitind <nitind>
2009-08-25 23:48:41 +0000

    [nobug] Remove UI dependency, make ModuleCore optional

--- bundles/org.eclipse.wst.jsdt.web.core/src/org/eclipse/wst/jsdt/web/core/javascript/WebRootFinder.java
+++ bundles/org.eclipse.wst.jsdt.web.core/src/org/eclipse/wst/jsdt/web/core/javascript/WebRootFinder.java
@@ -23,11 +23,7 @@ package org.eclipse.wst.jsdt.web.core.javascript;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.wst.common.componentcore.ComponentCore;
-import org.eclipse.wst.common.componentcore.ModuleCoreNature;
-import org.eclipse.wst.common.componentcore.internal.util.ComponentUtilities;
-import org.eclipse.wst.common.componentcore.resources.IVirtualComponent;
-import org.eclipse.wst.common.componentcore.resources.IVirtualFolder;
+import org.eclipse.wst.jsdt.web.core.internal.project.ModuleCoreSupport;
 /**
 *
 
@@ -38,23 +34,16 @@ import org.eclipse.wst.common.componentcore.resources.IVirtualFolder;
 */
 public class WebRootFinder {
 	public static IPath getServerContextRoot(IProject project) {
-		if (ModuleCoreNature.isFlexibleProject(project)) {
-			String contextRoot = ComponentUtilities.getServerContextRoot(project);
-			if (contextRoot != null) {
-				return new Path(contextRoot);
-			}
-		}
+		IPath root = ModuleCoreSupport.getWebContentRootPath(project);
+		if (root != null)
+			return root;
 		return Path.ROOT;
 	}
 	
 	public static IPath getWebContentFolder(IProject project) {
-		if (ModuleCoreNature.isFlexibleProject(project)) {
-			IVirtualComponent comp = ComponentCore.createComponent(project);
-			if (comp != null) {
-				IVirtualFolder rootFolder = comp.getRootFolder();
-				return rootFolder.getUnderlyingFolder().getProjectRelativePath();
-			}
-		}
+		IPath root = ModuleCoreSupport.getWebContentRootPath(project);
+		if (root != null)
+			return root.removeFirstSegments(1);
 		return Path.ROOT;
 	}
 	
