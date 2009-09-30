08267ce2ac4de50568f1c784be41e7d39f280fba
nsandonato <nsandonato>
2009-09-30 19:46:36 +0000

    [236311] changeAttrName not setting old attr mof value to null

--- bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/AttrImpl.java
+++ bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/AttrImpl.java
@@ -656,11 +656,11 @@ public class AttrImpl extends NodeImpl implements IDOMAttr {
 		if (this.ownerElement != null) {
 			value = getValue();
 			startOffset = this.ownerElement.getStartOffset();
-			this.ownerElement.notify(CHANGE, this, value, null, startOffset);
+			this.ownerElement.notify(REMOVE, this, value, null, startOffset);
 		}
 		this.fName = name.toCharArray();
 		if (this.ownerElement != null) {
-			this.ownerElement.notify(CHANGE, this, null, value, startOffset);
+			this.ownerElement.notify(ADD, this, null, value, startOffset);
 		}
 	}
 
