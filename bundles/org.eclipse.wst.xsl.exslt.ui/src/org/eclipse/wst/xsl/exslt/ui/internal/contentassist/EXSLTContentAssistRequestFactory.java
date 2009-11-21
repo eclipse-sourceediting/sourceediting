9c65b451f5cfef02693c6bbbc37f0de1a0bdc872
dacarver <dacarver>
2009-11-21 02:18:03 +0000

    [294079]: [editor] Content Assist for xpath expressions inside curly brackets https://bugs.eclipse.org/bugs/show_bug.cgi?id=294079

--- bundles/org.eclipse.wst.xsl.exslt.ui/src/org/eclipse/wst/xsl/exslt/ui/internal/contentassist/EXSLTContentAssistRequestFactory.java
+++ bundles/org.eclipse.wst.xsl.exslt.ui/src/org/eclipse/wst/xsl/exslt/ui/internal/contentassist/EXSLTContentAssistRequestFactory.java
@@ -16,11 +16,11 @@ import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMAttr;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMElement;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
+import org.eclipse.wst.xsl.ui.internal.contentassist.SelectAttributeContentAssist;
+import org.eclipse.wst.xsl.ui.internal.contentassist.TestAttributeContentAssist;
 import org.eclipse.wst.xsl.ui.internal.contentassist.href.HrefContentAssistRequest;
 import org.eclipse.wst.xsl.ui.provisional.contentassist.IContentAssistProposalRequest;
 import org.eclipse.wst.xsl.ui.provisional.contentassist.NullContentAssistRequest;
-import org.eclipse.wst.xsl.ui.provisional.contentassist.SelectAttributeContentAssist;
-import org.eclipse.wst.xsl.ui.provisional.contentassist.TestAttributeContentAssist;
 import org.w3c.dom.NamedNodeMap;
 
 /**
