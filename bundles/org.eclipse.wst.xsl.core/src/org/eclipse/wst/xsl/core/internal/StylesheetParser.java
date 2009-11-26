7699d762d226c02985d8678f9f51560413787079
dacarver <dacarver>
2009-11-26 22:39:54 +0000

    [nobug] - Refactored code for easier maintenance

--- bundles/org.eclipse.wst.xsl.core/src/org/eclipse/wst/xsl/core/internal/StylesheetParser.java
+++ bundles/org.eclipse.wst.xsl.core/src/org/eclipse/wst/xsl/core/internal/StylesheetParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 Chase Technology Ltd - http://www.chasetechnology.co.uk
+ * Copyright (c) 2007, 2009 Chase Technology Ltd - http://www.chasetechnology.co.uk
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,8 @@
  *
  * Contributors:
  *     Doug Satchwell (Chase Technology Ltd) - initial API and implementation
- *     David Carver (STAR) - bug 290286 - Model loading of parameters not respecting functions. 
+ *     David Carver (STAR) - bug 290286 - Model loading of parameters not respecting functions.
+ *     David Carver (STAR) - no bug - Refactored Code for easier maintenance 
  *******************************************************************************/
 
 package org.eclipse.wst.xsl.core.internal;
@@ -42,93 +43,7 @@ public class StylesheetParser {
 	private void recurse(Element element) {
 		XSLElement xslEl = null;
 		if (XSLCore.XSL_NAMESPACE_URI.equals(element.getNamespaceURI())) {
-			String elName = element.getLocalName();
-			if ("stylesheet".equals(elName) && elementStack.size() == 0) //$NON-NLS-1$
-			{
-				NamedNodeMap map = element.getAttributes();
-				String version = element.getAttribute("version"); //$NON-NLS-1$
-				sf.setVersion(version);
-				xslEl = sf;
-			} else if ("include".equals(elName) && elementStack.size() == 1) //$NON-NLS-1$
-			{
-				Include include = new Include(sf);
-				sf.addInclude(include);
-				xslEl = include;
-			} else if ("import".equals(elName) && elementStack.size() == 1) //$NON-NLS-1$
-			{
-				Import include = new Import(sf);
-				sf.addImport(include);
-				xslEl = include;
-			} else if ("template".equals(elName) && elementStack.size() == 1) //$NON-NLS-1$
-			{
-				currentTemplate = new Template(sf);
-				sf.addTemplate(currentTemplate);
-				xslEl = currentTemplate;
-			} else if ("param".equals(elName) && parentEl.getModelType() != XSLModelObject.Type.STYLESHEET) { //$NON-NLS-1$
-				Parameter param = new Parameter(sf);
-				// determine whether param has a value
-				NodeList childNodes = element.getChildNodes();
-				for (int i = 0; i < childNodes.getLength(); i++) {
-					Node childNode = childNodes.item(i);
-					if (childNode.getNodeType() != Node.ATTRIBUTE_NODE) {
-						param.setValue(true);
-						break;
-					}
-				}
-				if (parentEl.getModelType() == XSLModelObject.Type.FUNCTION) {
-					Function function = (Function) parentEl;
-					function.addParameter(param);
-				} else if (parentEl.getModelType() == XSLModelObject.Type.TEMPLATE
-						&& elementStack.size() == 2 && currentTemplate != null) {
-					Template template = (Template) parentEl;
-					template.addParameter(param);
-				}
-				xslEl = param;
-			} else if ("call-template".equals(elName) && elementStack.size() >= 2) //$NON-NLS-1$
-			{
-				CallTemplate currentCallTemplate = new CallTemplate(sf);
-				callTemplates.push(currentCallTemplate);
-				sf.addCalledTemplate(currentCallTemplate);
-				xslEl = currentCallTemplate;
-			} else if ("with-param".equals(elName) && elementStack.size() >= 3 && callTemplates.size() > 0) //$NON-NLS-1$
-			{
-				Parameter param = new Parameter(sf);
-				// determine whether param has a value
-				NodeList childNodes = element.getChildNodes();
-				for (int i = 0; i < childNodes.getLength(); i++) {
-					Node childNode = childNodes.item(i);
-					if (childNode.getNodeType() != Node.ATTRIBUTE_NODE) {
-						param.setValue(true);
-						break;
-					}
-				}
-				// get the previous call-template
-				CallTemplate currentCallTemplate = callTemplates.peek();
-				currentCallTemplate.addParameter(param);
-				xslEl = param;
-			} else if ("variable".equals(elName) || "param".equals(elName)) //$NON-NLS-1$ //$NON-NLS-2$
-			{
-				if (elementStack.size() == 1) {// global variable
-					Variable var = new Variable(sf);
-					sf.addGlobalVariable(var);
-					xslEl = var;
-				} else if (elementStack.size() > 1 && currentTemplate != null) {// local
-																				// variable
-					Variable var = new Variable(sf);
-					currentTemplate.addVariable(var);
-					xslEl = var;
-				}
-			} else if ("function".equals(elName)) { //$NON-NLS-1$
-				currentTemplate = null;
-				Function function = new Function(sf);
-				functions.push(function);
-				sf.addFunction(function);
-				xslEl = function;
-			} else {
-				xslEl = new XSLElement(sf);
-			}
-			if (xslEl != null)
-				configure((IDOMNode) element, xslEl);
+			xslEl = createXSLModelObject(element, xslEl);
 		}
 		elementStack.push(element);
 		NodeList childNodes = element.getChildNodes();
@@ -149,6 +64,142 @@ public class StylesheetParser {
 		// currentCallTemplate = null;
 	}
 
+	private XSLElement createXSLModelObject(Element element, XSLElement xslEl) {
+		String elName = element.getLocalName();
+		int elementSize = elementStack.size();
+		if ("stylesheet".equals(elName) && elementSize == 0) //$NON-NLS-1$
+		{
+			xslEl = createStyleSheet(element);
+		} else if ("include".equals(elName) && elementSize == 1) //$NON-NLS-1$
+		{
+			xslEl = createInclude();
+		} else if ("import".equals(elName) && elementSize == 1) //$NON-NLS-1$
+		{
+			xslEl = createImport();
+		} else if ("template".equals(elName) && elementSize == 1) //$NON-NLS-1$
+		{
+			xslEl = createTemplate();
+		} else if ("param".equals(elName) && notParentStylesheet()) { //$NON-NLS-1$
+			xslEl = createParamater(element);
+		} else if ("call-template".equals(elName) && elementSize >= 2) //$NON-NLS-1$
+		{
+			xslEl = createCallTemplate();
+		} else if ("with-param".equals(elName) && elementSize >= 3 && callTemplates.size() > 0) //$NON-NLS-1$
+		{
+			xslEl = createWithParamParm(element);
+		} else if (isVariable(elName)) 
+		{
+			xslEl = createVariable(xslEl);
+		} else if ("function".equals(elName)) { //$NON-NLS-1$
+			xslEl = createFunction();
+		} else {
+			xslEl = new XSLElement(sf);
+		}
+		if (xslEl != null)
+			configure((IDOMNode) element, xslEl);
+		return xslEl;
+	}
+
+	private boolean isVariable(String elName) {
+		return "variable".equals(elName) || "param".equals(elName); //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	private boolean notParentStylesheet() {
+		return parentEl.getModelType() != XSLModelObject.Type.STYLESHEET;
+	}
+
+	private XSLElement createFunction() {
+		currentTemplate = null;
+		Function function = new Function(sf);
+		functions.push(function);
+		sf.addFunction(function);
+		return function;
+	}
+
+	private XSLElement createVariable(XSLElement xslEl) {
+		if (elementStack.size() == 1) {// global variable
+			Variable var = new Variable(sf);
+			sf.addGlobalVariable(var);
+			xslEl = var;
+		} else if (elementStack.size() > 1 && currentTemplate != null) {// local
+																		// variable
+			Variable var = new Variable(sf);
+			currentTemplate.addVariable(var);
+			xslEl = var;
+		}
+		return xslEl;
+	}
+
+	private XSLElement createWithParamParm(Element element) {
+		Parameter param = new Parameter(sf);
+		// determine whether param has a value
+		NodeList childNodes = element.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node childNode = childNodes.item(i);
+			if (childNode.getNodeType() != Node.ATTRIBUTE_NODE) {
+				param.setValue(true);
+				break;
+			}
+		}
+		// get the previous call-template
+		CallTemplate currentCallTemplate = callTemplates.peek();
+		currentCallTemplate.addParameter(param);
+		return param;
+	}
+
+	private XSLElement createCallTemplate() {
+		CallTemplate currentCallTemplate = new CallTemplate(sf);
+		callTemplates.push(currentCallTemplate);
+		sf.addCalledTemplate(currentCallTemplate);
+		return currentCallTemplate;
+	}
+
+	private XSLElement createParamater(Element element) {
+		Parameter param = new Parameter(sf);
+		// determine whether param has a value
+		NodeList childNodes = element.getChildNodes();
+		for (int i = 0; i < childNodes.getLength(); i++) {
+			Node childNode = childNodes.item(i);
+			if (childNode.getNodeType() != Node.ATTRIBUTE_NODE) {
+				param.setValue(true);
+				break;
+			}
+		}
+		if (parentEl.getModelType() == XSLModelObject.Type.FUNCTION) {
+			Function function = (Function) parentEl;
+			function.addParameter(param);
+		} else if (parentEl.getModelType() == XSLModelObject.Type.TEMPLATE
+				&& elementStack.size() == 2 && currentTemplate != null) {
+			Template template = (Template) parentEl;
+			template.addParameter(param);
+		}
+		return param;
+	}
+
+	private XSLElement createTemplate() {
+		currentTemplate = new Template(sf);
+		sf.addTemplate(currentTemplate);
+		return currentTemplate;
+	}
+
+	private XSLElement createImport() {
+		Import include = new Import(sf);
+		sf.addImport(include);
+		return include;
+	}
+
+	private XSLElement createInclude() {
+		Include include = new Include(sf);
+		sf.addInclude(include);
+		return include;
+	}
+
+	private XSLElement createStyleSheet(Element element) {
+		String version = element.getAttribute("version"); //$NON-NLS-1$
+		sf.setVersion(version);
+		return sf;
+	}
+
 	private void configure(IDOMNode node, XSLElement element) {
 		setPositionInfo(node, element);
 		IDOMElement domElem = (IDOMElement) node;
@@ -161,6 +212,10 @@ public class StylesheetParser {
 			setPositionInfo(attr, xslatt);
 			element.setAttribute(xslatt);
 		}
+		setParentElement(node, element);
+	}
+
+	private void setParentElement(IDOMNode node, XSLElement element) {
 		if (parentEl != null)
 			parentEl.addChild(element);
 		if (node.hasChildNodes()) {
@@ -173,7 +228,6 @@ public class StylesheetParser {
 				}
 			}
 		}
-		//parentEl = element;
 	}
 
 	private static void setPositionInfo(IDOMNode node, XSLNode inc) {
