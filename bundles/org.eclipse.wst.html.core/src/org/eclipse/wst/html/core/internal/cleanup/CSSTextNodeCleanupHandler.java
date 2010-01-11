27a2c08e72f8430d76e6106fb50c7f8a4f6e7dd9
nsandonato <nsandonato>
2010-01-11 16:44:05 +0000

    [272967] [formatting] "Cleanup document" reformats CSS even when reformating isn't requested

--- bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/cleanup/CSSTextNodeCleanupHandler.java
+++ bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/cleanup/CSSTextNodeCleanupHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2009 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -37,7 +37,9 @@ public class CSSTextNodeCleanupHandler extends AbstractNodeCleanupHandler {
 		IStructuredDocument structuredDocument = model.getStructuredDocument();
 		if (structuredDocument == null)
 			return node;
-
+		if ( !getCleanupPreferences().getFormatSource())
+			return node;
+		
 		String content = getCSSContent(node);
 		if (content == null)
 			return node;
