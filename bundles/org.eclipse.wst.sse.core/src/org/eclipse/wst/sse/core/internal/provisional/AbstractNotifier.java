492431713f62f3cdda4ffc3ee7c2eda4758ba40e
nsandonato <nsandonato>
2010-02-17 17:49:36 +0000

    [253222] org.eclipse.wst.sse.core.internal.provisional.AbstractNotifier not threadsafe enough

--- bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/provisional/AbstractNotifier.java
+++ bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/provisional/AbstractNotifier.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2009 IBM Corporation and others.
+ * Copyright (c) 2001, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -53,7 +53,7 @@ public abstract class AbstractNotifier implements INodeNotifier {
 		fAdapters[adapterCount++] = adapter;
 	}
 
-	private void ensureCapacity(int needed) {
+	private synchronized void ensureCapacity(int needed) {
 		if (fAdapters == null) {
 			// first time
 			fAdapters = new INodeAdapter[needed + growthConstant];
