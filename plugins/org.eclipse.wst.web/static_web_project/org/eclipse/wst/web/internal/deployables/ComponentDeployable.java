5e0a1294d5dd65430169ab8b28449a29e20dea7e
jsholl <jsholl>
2010-04-05 17:04:50 +0000

    [305306] Incorrect deployment of variable based dependencies in EAR

--- plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/web/internal/deployables/ComponentDeployable.java
+++ plugins/org.eclipse.wst.web/static_web_project/org/eclipse/wst/web/internal/deployables/ComponentDeployable.java
@@ -365,8 +365,7 @@ public abstract class ComponentDeployable extends ProjectModule {
 	
 	protected void addUtilMember(IVirtualComponent parent, IVirtualReference reference, IPath runtimePath) {
 		IModuleFile mf = null;
-		final String archiveName2 = reference.getArchiveName();
-		final String archiveName = new Path(archiveName2).lastSegment();
+		final String archiveName = reference.getArchiveName();
 		final IVirtualComponent virtualComp = reference.getReferencedComponent();
 		IFile ifile = (IFile)virtualComp.getAdapter(IFile.class);
 		if (ifile != null) { //In Workspace
