40085be381865ff23bd8cdf7fe97615a49ff2da9
nsandonato <nsandonato>
2010-04-05 19:07:30 +0000

    [245324] Default extension (.xml) is supplemented whenever the file name is set in XML Wizard.

--- bundles/org.eclipse.wst.xml.ui/src-wizards/org/eclipse/wst/xml/ui/internal/wizards/NewModelWizard.java
+++ bundles/org.eclipse.wst.xml.ui/src-wizards/org/eclipse/wst/xml/ui/internal/wizards/NewModelWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -249,7 +249,11 @@ public class NewModelWizard extends Wizard implements INewWizard {
 			}
 			// no fileExtension, let's check for this file with default file
 			// extension
-			fullFileName += defaultFileExtension;
+			if (fullFileName.lastIndexOf('.') == -1)
+			{
+				fullFileName += defaultFileExtension;
+			}
+
 			if ((getContainerFullPath() != null) && (getContainerFullPath().isEmpty() == false) && (getFileName().compareTo("") != 0)) //$NON-NLS-1$
 			{
 				Path fullPath = new Path(getContainerFullPath().toString() + '/' + fullFileName);
