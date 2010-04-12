d592663510e36fee0a49f9009bdb0f6ff1af2a22
nsandonato <nsandonato>
2010-04-12 20:05:39 +0000

    [308570] Readonly Controller allows modification before closing bracket

--- bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/ReadOnlyController.java
+++ bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/ReadOnlyController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -250,7 +250,7 @@ class ReadOnlyController {
 				lock(structuredDocument, offset, length, false, false);
 				offset = offset + span.offset + span.length;
 				length = flatNode.getEnd() - offset;
-				lock(structuredDocument, offset, length, canInsert, false);
+				lock(structuredDocument, offset, length, false, canInsert);
 			} else {
 				lock(flatNode, false, canInsert);
 			}
