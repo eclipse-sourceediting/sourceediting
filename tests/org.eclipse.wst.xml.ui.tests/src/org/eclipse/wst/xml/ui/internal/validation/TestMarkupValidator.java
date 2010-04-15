62c58e0384f45b450dc934b10e09f976cde36173
nsandonato <nsandonato>
2010-04-15 03:10:16 +0000

    [304620] [validation]Markup Validator should be run along with the XML Validator

--- tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/internal/validation/TestMarkupValidator.java
+++ tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/internal/validation/TestMarkupValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -17,6 +17,7 @@ import org.eclipse.wst.sse.core.StructuredModelManager;
 import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
 import org.eclipse.wst.sse.ui.internal.reconcile.validator.IncrementalReporter;
 import org.eclipse.wst.validation.internal.provisional.core.IReporter;
+import org.eclipse.wst.xml.core.internal.validation.MarkupValidator;
 
 /**
  * Tests MarkupValidator
