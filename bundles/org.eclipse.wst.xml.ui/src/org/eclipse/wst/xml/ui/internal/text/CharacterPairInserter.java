f2915d4f4a3d6cd71d9c43ac62c079c1a0100ebc
nsandonato <nsandonato>
2010-04-21 15:11:40 +0000

    [309980] NPE when typing at the end of a document

--- bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/text/CharacterPairInserter.java
+++ bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/text/CharacterPairInserter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -54,7 +54,7 @@ public class CharacterPairInserter extends AbstractCharacterPairInserter impleme
 			IStructuredDocumentRegion[] regions = ((IStructuredDocument) doc).getStructuredDocumentRegions(offset, 0);
 			if (regions != null && regions.length > 0) {
 				ITextRegion region = regions[0].getRegionAtCharacterOffset(offset);
-				return region.getType() != DOMRegionContext.XML_TAG_ATTRIBUTE_VALUE;
+				return region != null && region.getType() != DOMRegionContext.XML_TAG_ATTRIBUTE_VALUE;
 			}
 		}
 		return true;
