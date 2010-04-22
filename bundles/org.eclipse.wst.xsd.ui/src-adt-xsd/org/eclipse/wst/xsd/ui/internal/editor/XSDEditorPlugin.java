bbd15c4f814ddf9671939b95ff0e3dc642ed250a
vbaciu <vbaciu>
2010-04-22 03:11:25 +0000

    [301803] XSDEditorPlugin uses deprecated IPluginDescriptor -> NPE

--- bundles/org.eclipse.wst.xsd.ui/src-adt-xsd/org/eclipse/wst/xsd/ui/internal/editor/XSDEditorPlugin.java
+++ bundles/org.eclipse.wst.xsd.ui/src-adt-xsd/org/eclipse/wst/xsd/ui/internal/editor/XSDEditorPlugin.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2008 IBM Corporation and others.
+ * Copyright (c) 2001, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -48,6 +48,7 @@ public class XSDEditorPlugin extends AbstractUIPlugin
   private ExtensionsSchemasRegistry registry;  
   private NodeCustomizationRegistry nodeCustomizationRegistry;
   private XSDEditorConfiguration xsdEditorConfiguration = null;
+  private static Bundle bundle;
   
   public static final String CONST_USE_SIMPLE_EDIT_MODE = PLUGIN_ID + ".useSimpleEditMode"; //$NON-NLS-1$
   public static final String CONST_SHOW_INHERITED_CONTENT = PLUGIN_ID + ".showInheritedContent"; //$NON-NLS-1$
@@ -75,6 +76,7 @@ public class XSDEditorPlugin extends AbstractUIPlugin
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
+		bundle = context.getBundle();
 	}
 
 	/**
@@ -212,7 +214,7 @@ public class XSDEditorPlugin extends AbstractUIPlugin
 	}
 	
 	public URL getBaseURL() {
-		return getDescriptor().getInstallURL();
+		return bundle.getEntry("/"); //$NON-NLS-1$
 	}
 
 	public Image getIconImage(String object) {
