48f31527387b91cfe2122ad20c595ccc0dc8ab21
nitind <nitind>
2010-05-20 01:12:44 +0000

    [259465] [formatter] formatter in web page removes attribute value quotes and adds semicolon

--- bundles/org.eclipse.wst.jsdt.web.ui/src/org/eclipse/wst/jsdt/web/ui/StructuredTextViewerConfigurationJSDT.java
+++ bundles/org.eclipse.wst.jsdt.web.ui/src/org/eclipse/wst/jsdt/web/ui/StructuredTextViewerConfigurationJSDT.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2007 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -52,7 +52,7 @@ public class StructuredTextViewerConfigurationJSDT extends StructuredTextViewerC
 		 */
 		String[] contentTypes = getConfiguredContentTypes(sourceViewer);
 		for (int i = 0; i < contentTypes.length; i++) {
-				if (IHTMLPartitions.SCRIPT.equals(contentTypes[i]) || IHTMLPartitions.SCRIPT_EVENTHANDLER.equals(contentTypes[i])) {
+				if (IHTMLPartitions.SCRIPT.equals(contentTypes[i])) {
 					((MultiPassContentFormatter) formatter).setSlaveStrategy(new FormattingStrategyJSDT(), contentTypes[i]);
 				}
 			}
