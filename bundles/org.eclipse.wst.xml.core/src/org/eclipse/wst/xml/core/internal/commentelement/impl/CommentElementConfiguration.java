cab925693168669a9d3d8300f88ab940d16ed706
nitind <nitind>
2010-07-15 20:55:14 +0000

    [nobug] avoid possible NPE on bad elements

--- bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/commentelement/impl/CommentElementConfiguration.java
+++ bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/commentelement/impl/CommentElementConfiguration.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2009 IBM Corporation and others.
+ * Copyright (c) 2001, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -46,7 +46,7 @@ public class CommentElementConfiguration {
 	CommentElementConfiguration(IConfigurationElement element) {
 		super();
 		fElement = element;
-		fCustom = element.getName().equalsIgnoreCase("handler-custom"); //$NON-NLS-1$
+		fCustom = "handler-custom".equalsIgnoreCase(element.getName()); //$NON-NLS-1$
 
 		fillAttributes(element);
 
