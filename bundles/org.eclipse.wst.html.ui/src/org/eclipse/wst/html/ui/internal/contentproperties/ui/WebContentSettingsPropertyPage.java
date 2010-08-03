002bf68a88d8e6d672779f25ac55e19d77aa222f
nsandonato <nsandonato>
2010-08-03 15:34:55 +0000

    [174834] restore defaults in web content settings properties page does not restore document type

--- bundles/org.eclipse.wst.html.ui/src/org/eclipse/wst/html/ui/internal/contentproperties/ui/WebContentSettingsPropertyPage.java
+++ bundles/org.eclipse.wst.html.ui/src/org/eclipse/wst/html/ui/internal/contentproperties/ui/WebContentSettingsPropertyPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -353,6 +353,9 @@ public class WebContentSettingsPropertyPage extends PropertyPage {
 	private void performDoctypeDefaults() {
 		fPublicIdText.setText("");//$NON-NLS-1$
 		fSystemIdText.setText(""); //$NON-NLS-1$
+		int index = fDocumentTypeCombo.indexOf(SELECT_NONE);
+		if (index > -1)
+			fDocumentTypeCombo.select(index);
 	}
 
 	private boolean performDoctypeOk() {
