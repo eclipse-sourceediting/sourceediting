2004f1afd03de69ffcf38d1531c552906818c0e4
nsandonato <nsandonato>
2010-08-09 22:07:40 +0000

    [129881] [editor] QuickAssist surround with new element does not link edit

--- bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/correction/SurroundWithNewElementQuickAssistProposal.java
+++ bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/correction/SurroundWithNewElementQuickAssistProposal.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2006 IBM Corporation and others.
+ * Copyright (c) 2001, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -13,11 +13,19 @@
 package org.eclipse.wst.xml.ui.internal.correction;
 
 import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.ITextViewerExtension;
+import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.link.LinkedModeModel;
+import org.eclipse.jface.text.link.LinkedModeUI;
+import org.eclipse.jface.text.link.LinkedPosition;
+import org.eclipse.jface.text.link.LinkedPositionGroup;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.text.edits.InsertEdit;
 import org.eclipse.text.edits.MalformedTreeException;
 import org.eclipse.text.edits.MultiTextEdit;
+import org.eclipse.ui.texteditor.link.EditorLinkedModeUI;
 import org.eclipse.wst.sse.core.internal.format.IStructuredFormatProcessor;
 import org.eclipse.wst.sse.ui.internal.contentassist.ContentAssistUtils;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
@@ -29,7 +37,6 @@ import org.eclipse.wst.xml.ui.internal.editor.XMLEditorPluginImages;
 import org.w3c.dom.Node;
 
 public class SurroundWithNewElementQuickAssistProposal extends RenameInFileQuickAssistProposal {
-
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -57,23 +64,58 @@ public class SurroundWithNewElementQuickAssistProposal extends RenameInFileQuick
 			MultiTextEdit multiTextEdit = new MultiTextEdit();
 			// element tag name cannot be DBCS, do not translate "<element>"
 			// and "</element>"
-			multiTextEdit.addChild(new InsertEdit(startTagOffset, "<element>")); //$NON-NLS-1$
+			final String startElement = "<element>"; //$NON-NLS-1$
+			multiTextEdit.addChild(new InsertEdit(startTagOffset, startElement));
 			multiTextEdit.addChild(new InsertEdit(endTagOffset, "</element>")); //$NON-NLS-1$
 			multiTextEdit.apply(viewer.getDocument());
+			Position start = new Position(startTagOffset);
+			Position end = new Position(endTagOffset + startElement.length());
 
-			// get new element node
-			IDOMNode newElementNode = (IDOMNode) ContentAssistUtils.getNodeAt(viewer, startTagOffset);
+			try {
+				viewer.getDocument().addPosition(start);
+				viewer.getDocument().addPosition(end);
 
-			// format new element
-			IStructuredFormatProcessor formatProcessor = new FormatProcessorXML();
-			formatProcessor.formatNode(newElementNode);
+				// get new element node
+				IDOMNode newElementNode = (IDOMNode) ContentAssistUtils.getNodeAt(viewer, startTagOffset);
+				IStructuredFormatProcessor formatProcessor = new FormatProcessorXML();
+				formatProcessor.formatNode(newElementNode);
 
-			// rename new element
-			super.apply(viewer, trigger, stateMask, newElementNode.getStartOffset() + 1);
+				// rename new element
+				apply(viewer, trigger, stateMask, start, end, 7);
+			} finally {
+				viewer.getDocument().removePosition(start);
+				viewer.getDocument().removePosition(end);
+			}
 		}
 		catch (MalformedTreeException e) {
 			// log for now, unless find reason not to
 			Logger.log(Logger.INFO, e.getMessage());
+		}
+		catch (BadLocationException e) {
+			// log for now, unless find reason not to
+			Logger.log(Logger.INFO, e.getMessage());
+		} 
+	}
+
+	private void apply(ITextViewer viewer, char trigger, int stateMask, Position start, Position end, int length) {
+		IDocument document = viewer.getDocument();
+		LinkedPositionGroup group = new LinkedPositionGroup();
+		try {
+				group.addPosition(new LinkedPosition(document, start.offset + 1, length, 0));
+				group.addPosition(new LinkedPosition(document, end.offset + 2, length, 1));
+			
+			if(viewer instanceof ITextViewerExtension)
+				((ITextViewerExtension) viewer).setRedraw(true);
+
+			LinkedModeModel linkedModeModel = new LinkedModeModel();
+			linkedModeModel.addGroup(group);
+			linkedModeModel.forceInstall();
+	
+			LinkedModeUI ui = new EditorLinkedModeUI(linkedModeModel, viewer);
+			ui.setExitPosition(viewer, start.offset, 0, LinkedPositionGroup.NO_STOP);
+			ui.enter();
+	
+			fSelectedRegion = ui.getSelectedRegion();
 		}
 		catch (BadLocationException e) {
 			// log for now, unless find reason not to
