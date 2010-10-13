197bca0539fe9cde2f316b6529f06296472c01c4
nsandonato <nsandonato>
2010-10-13 18:06:34 +0000

    [295950] [validation] jsp:directive file not validated

--- tests/org.eclipse.jst.jsp.core.tests/src/org/eclipse/jst/jsp/core/tests/validation/JSPDirectiveValidatorTest.java
+++ tests/org.eclipse.jst.jsp.core.tests/src/org/eclipse/jst/jsp/core/tests/validation/JSPDirectiveValidatorTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 IBM Corporation and others.
+ * Copyright (c) 2009, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,21 +10,33 @@
  *******************************************************************************/
 package org.eclipse.jst.jsp.core.tests.validation;
 
+import java.io.ByteArrayInputStream;
 import java.util.Iterator;
+import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jst.jsp.core.internal.JSPCoreMessages;
+import org.eclipse.jst.jsp.core.internal.validation.JSPBatchValidator;
 import org.eclipse.jst.jsp.core.internal.validation.JSPDirectiveValidator;
 import org.eclipse.jst.jsp.core.tests.taglibindex.BundleResourceUtil;
 import org.eclipse.osgi.util.NLS;
+import org.eclipse.wst.sse.core.internal.util.Sorter;
+import org.eclipse.wst.validation.AbstractValidator;
+import org.eclipse.wst.validation.ValidationResult;
+import org.eclipse.wst.validation.ValidationState;
 import org.eclipse.wst.validation.internal.provisional.core.IMessage;
 import org.eclipse.wst.validation.internal.provisional.core.IReporter;
+import org.eclipse.wst.xml.core.internal.validation.core.ValidationMessage;
 
 public class JSPDirectiveValidatorTest extends TestCase {
 	
@@ -124,6 +136,25 @@ public class JSPDirectiveValidatorTest extends TestCase {
 		assertTrue("JSP Directive Validator did not report the missing fragment.", foundMissingInclude);
 	}
 	
+	public void testIncludeDirectiveXML() throws Exception {
+		String contents = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"+
+		"<jsp:root xmlns:jsp=\"http://java.sun.com/JSP/Page\" version=\"2.0\">\n"+
+		"<jsp:directive.include  file=\"missing.jspf\"/>\n";
+		runNegativeTest("/"+ getName() + "/test.jsp", contents, new JSPBatchValidator(), "Fragment \"missing.jspf\" was not found at expected path /testIncludeDirectiveXML/missing.jspf");
+	}
+	
+	public void testIncludeDirective2() throws Exception {
+		String contents = "<%@include file=\"missing.jspf\"%>\n";
+		runNegativeTest("/"+ getName() + "/test.jsp", contents, new JSPBatchValidator(), "Fragment \"missing.jspf\" was not found at expected path /testIncludeDirective2/missing.jspf");
+	}
+	
+	public void testIncludeAction() throws Exception {
+		String contents = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"+
+		"<jsp:root xmlns:jsp=\"http://java.sun.com/JSP/Page\" version=\"2.0\">\n"+
+		"<jsp:include page=\"missing.jspf\"/>\n";
+		runNegativeTest("/"+ getName() + "/test.jsp", contents, new JSPBatchValidator(), "Fragment \"missing.jspf\" was not found at expected path /testIncludeAction/missing.jspf");
+	}
+	
 	public void testIncludeMappedURL() throws Exception {
 		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject("delos");
 		if (!project.exists()) {
@@ -149,4 +180,50 @@ public class JSPDirectiveValidatorTest extends TestCase {
 		}
 	}
 
+	/**
+	 * The purpose of a validator is to generate messages.  Let us make sure the expected messages are generated.
+	 * @param filePath
+	 * @param content
+	 * @param validator
+	 * @param expectedProblemMessages
+	 * @throws Exception
+	 */
+	protected void runNegativeTest(String filePath, String content, AbstractValidator validator, String expectedProblemMessages) throws Exception {
+		IPath path = new Path (filePath);
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(path.segment(0));
+		if (!project.exists()) {
+			BundleResourceUtil.createSimpleProject(path.segment(0), null, new String[]{JavaCore.NATURE_ID});
+		}
+		IFile file = ResourcesPlugin.getWorkspace().getRoot().getFile(path);
+		file.create(new ByteArrayInputStream(content.getBytes("utf8")), true, null);
+		ValidationResult result = validator.validate(file, IResourceDelta.ADDED, new ValidationState(), new NullProgressMonitor());
+		assertEquals(expectedProblemMessages, sortMessages(result.getReporter(new NullProgressMonitor()).getMessages()));
+	}
+	
+	static class ValidatorMessageSorter extends Sorter {
+		/* (non-Javadoc)
+		 * @see org.eclipse.wst.sse.core.internal.util.Sorter#compare(java.lang.Object, java.lang.Object)
+		 */
+		public boolean compare(Object elementOne, Object elementTwo) {
+			if (elementOne instanceof ValidationMessage)
+				return ((ValidationMessage) elementTwo).getMessage().compareTo(((ValidationMessage) elementOne).getMessage()) > 0;
+			return ((IMessage) elementTwo).getOffset() > ((IMessage) elementOne).getOffset();
+		}
+	}
+
+	/**
+	 * @param messages
+	 * @return
+	 */
+	private String sortMessages(List messages) {
+		Object[] sorted = new ValidatorMessageSorter().sort(messages.toArray());
+		StringBuffer s = new StringBuffer();
+		for (int i = 0; i < sorted.length; i++) {
+			if (sorted[i] instanceof ValidationMessage)
+				s.append(((ValidationMessage) sorted[i]).getMessage());
+			else
+				s.append(((IMessage) sorted[i]).getText());
+		}
+		return s.toString();
+	}
 }
