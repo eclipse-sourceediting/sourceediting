8a13210b4574b0e793bb43da0013711d2ba31465
nsandonato <nsandonato>
2010-10-26 13:58:15 +0000

    [148126] Problem with compareBoundaryPoints when comparing text nodes

--- bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/RangeImpl.java
+++ bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/RangeImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2005 IBM Corporation and others.
+ * Copyright (c) 2001, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -202,7 +202,7 @@ public class RangeImpl implements Range {
 			node2 = (IDOMNode) child;
 			offset2 = 0;
 		} else {
-			node2 = (IDOMNode) container1;
+			node2 = (IDOMNode) container2;
 		}
 		int nodeOffset2 = node2.getStartOffset();
 
