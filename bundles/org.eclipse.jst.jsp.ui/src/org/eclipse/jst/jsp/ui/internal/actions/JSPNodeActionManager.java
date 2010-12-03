ad7500beb8a26d80102718f20dea95e2066edc6d
nsandonato <nsandonato>
2010-12-03 19:38:31 +0000

    [129715] [outline] Outline context menu doesn't show preferred HTML cases

--- bundles/org.eclipse.jst.jsp.ui/src/org/eclipse/jst/jsp/ui/internal/actions/JSPNodeActionManager.java
+++ bundles/org.eclipse.jst.jsp.ui/src/org/eclipse/jst/jsp/ui/internal/actions/JSPNodeActionManager.java
@@ -10,8 +10,12 @@
  *******************************************************************************/
 package org.eclipse.jst.jsp.ui.internal.actions;
 
+import org.eclipse.core.runtime.Preferences;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jst.jsp.core.internal.provisional.JSP11Namespace;
+import org.eclipse.jst.jsp.core.internal.provisional.contenttype.ContentTypeIdForJSP;
+import org.eclipse.wst.html.core.internal.HTMLCorePlugin;
+import org.eclipse.wst.html.core.internal.preferences.HTMLCorePreferenceNames;
 import org.eclipse.wst.html.ui.internal.contentoutline.HTMLNodeActionManager;
 import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
 import org.w3c.dom.Node;
@@ -25,6 +29,19 @@ public class JSPNodeActionManager extends HTMLNodeActionManager {
 	protected boolean canContributeChildActions(Node node){
 		String nodeName = node.getNodeName().toLowerCase();
 		return !(nodeName.equals(JSP11Namespace.ElementName.SCRIPTLET) || nodeName.equals(JSP11Namespace.ElementName.DECLARATION) || nodeName.equals(JSP11Namespace.ElementName.EXPRESSION));
+	}
+	
+	protected void updateCase() {
+		if (fModel != null) {
+			String modelContentTypeId = fModel.getContentTypeIdentifier();
+			if (modelContentTypeId != null) {
+				if (modelContentTypeId.equals(ContentTypeIdForJSP.ContentTypeID_JSP)) {
+					Preferences prefs = HTMLCorePlugin.getDefault().getPluginPreferences(); //$NON-NLS-1$
+					fTagCase = prefs.getInt(HTMLCorePreferenceNames.TAG_NAME_CASE);
+					fAttrCase = prefs.getInt(HTMLCorePreferenceNames.ATTR_NAME_CASE);
+				}
+			}
+		}
 	}
 
 }
