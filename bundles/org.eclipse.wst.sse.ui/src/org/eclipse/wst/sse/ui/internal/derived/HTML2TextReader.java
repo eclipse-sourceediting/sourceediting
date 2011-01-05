630f3f2eb423fd471d601afec32f2d0f2c96ae4d
nsandonato <nsandonato>
2011-01-05 21:02:58 +0000

    [229178] [content assist] Portions of tag library description can be missing or altered when displayed by JSP content assist

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/derived/HTML2TextReader.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/derived/HTML2TextReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -139,7 +139,7 @@ public class HTML2TextReader extends SubstitutionTextReader {
 			tag= tag.substring(1);
 
 		if (!fgTags.contains(tag))
-			return EMPTY_STRING;
+			return '<' + html + '>';
 
 
 		if ("pre".equals(html)) { //$NON-NLS-1$
