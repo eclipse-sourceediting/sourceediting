53e49c260988a80885614896f9366c33034bb885
nsandonato <nsandonato>
2011-01-12 19:37:20 +0000

    [332418] "An internal error occurred during: "Processing Dirty Regions"."

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/style/SemanticHighlightingReconciler.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/style/SemanticHighlightingReconciler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -104,7 +104,7 @@ public class SemanticHighlightingReconciler implements IReconcilingStrategy, IRe
 		fJobHighlightings = fHighlightings;
 		IStructuredModel model = null;
 		try {
-			if (fJobPresenter == null || fJobSemanticHighlightings == null || fJobHighlightings == null)
+			if (fJobPresenter == null || fJobSemanticHighlightings == null || fJobHighlightings == null || fDocument == null)
 				return;
 
 			fJobPresenter.setCanceled(false);
