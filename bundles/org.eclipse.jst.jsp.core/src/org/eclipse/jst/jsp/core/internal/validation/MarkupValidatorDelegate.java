633dfcefff89b045136a2b8865f28d31d8490efb
nitind <nitind>
2011-01-28 19:10:14 +0000

    This commit was manufactured by cvs2svn to create tag 'v201101281910'.

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/validation/MarkupValidatorDelegate.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jst.jsp.core.internal.validation;
-
-import org.eclipse.core.resources.IResource;
-import org.eclipse.wst.validation.internal.provisional.core.IReporter;
-
-public interface MarkupValidatorDelegate {
-	void validate(IResource resource, IReporter reporter);
-}
