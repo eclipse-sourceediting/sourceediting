52d63a731949029810f2fb3ded5d891b60e767a0
nsandonato <nsandonato>
2011-02-22 14:19:13 +0000

    [313742] xml editor automatically refreshes files that change in the file system

--- bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/validation/DelegatingSourceValidatorForXML.java
+++ bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/validation/DelegatingSourceValidatorForXML.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2008 IBM Corporation and others.
+ * Copyright (c) 2001, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,6 +12,7 @@
 package org.eclipse.wst.xml.ui.internal.validation;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.wst.validation.ValidationFramework;
 import org.eclipse.wst.validation.Validator;
 import org.eclipse.wst.validation.internal.provisional.core.IValidator;
@@ -49,6 +50,8 @@ public class DelegatingSourceValidatorForXML extends DelegatingSourceValidator
     Validator v = getValidator();
     if (v == null)
       return false;
+    if (!file.isSynchronized(IResource.DEPTH_ZERO))
+    	return false;
     if (!v.shouldValidate(file, false, false))
       return false;
     return v.isBuildValidation() || v.isManualValidation();
