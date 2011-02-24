124b7aadc2d862b8f7d2ebb06a8b7233d4a36730
nitind <nitind>
2011-02-24 03:01:20 +0000

    [nobug] adjust for compiler messages

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/StorageModelProvider.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/StorageModelProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2009 IBM Corporation and others.
+ * Copyright (c) 2001, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -441,9 +441,7 @@ public class StorageModelProvider extends StorageDocumentProvider implements IMo
 					}
 
 				}
-				if (document == null) {
-					document = model.getStructuredDocument();
-				}
+				document = model.getStructuredDocument();
 			}
 		}
 		return document;
