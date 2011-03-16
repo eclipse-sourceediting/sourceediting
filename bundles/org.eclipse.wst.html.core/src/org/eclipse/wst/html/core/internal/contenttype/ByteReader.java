8992a26b15a42b3fbc623b8ce8f01c1cd9b459ee
nsandonato <nsandonato>
2011-03-16 20:05:35 +0000

    [338951] org.eclipse.wst.html.core.internal.contenttype.ByteReader doesn't delegate markSupported()

--- bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/contenttype/ByteReader.java
+++ bundles/org.eclipse.wst.html.core/src/org/eclipse/wst/html/core/internal/contenttype/ByteReader.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2005 IBM Corporation and others.
+ * Copyright (c) 2001, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -68,7 +68,7 @@ public class ByteReader extends Reader {
 	}
 
 	public boolean markSupported() {
-		return true;
+		return fInputStream.markSupported();
 	}
 
 	public int read() throws IOException {
