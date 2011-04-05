c5e548e4b0a44a87d0f19fa3df8df618d96ba4f7
nitind <nitind>
2011-04-05 04:52:56 +0000

    [nobug] pull up #getElIgnored()

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/document/PageDirectiveAdapter.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/document/PageDirectiveAdapter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -36,6 +36,8 @@ public interface PageDirectiveAdapter extends INodeAdapter {
 	void setEmbeddedType(EmbeddedTypeHandler handler);
 
 	EmbeddedTypeHandler getEmbeddedType();
+
+	String getElIgnored();
 
 	/**
 	 * This method is to give this adapter a chance to use
