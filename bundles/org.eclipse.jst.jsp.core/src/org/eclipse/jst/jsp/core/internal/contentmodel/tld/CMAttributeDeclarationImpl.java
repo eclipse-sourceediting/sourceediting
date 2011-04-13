55135930ffd1b71544cee9f9e068c33128376d44
nsandonato <nsandonato>
2011-04-13 20:13:40 +0000

    [302060] Wrong default used for rtexprvalue in tag file attribute (JSP 2.1)

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/contentmodel/tld/CMAttributeDeclarationImpl.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/contentmodel/tld/CMAttributeDeclarationImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2008 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -53,6 +53,11 @@ public class CMAttributeDeclarationImpl implements TLDAttributeDeclaration {
 	public CMAttributeDeclarationImpl(CMDocument owner) {
 		super();
 		fOwnerDocument = owner;
+	}
+
+	public CMAttributeDeclarationImpl(CMDocument owner, String defaultRtexprvalue) {
+		this(owner);
+		rtexprvalue = defaultRtexprvalue;
 	}
 
 	/**
