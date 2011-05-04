7b2a8aa35229ec0a4d4c6b892e9f266aecd6ca6f
nsandonato <nsandonato>
2011-05-04 14:30:22 +0000

    [344194] NPE in ValidatorStrategy

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/reconcile/validator/ValidatorStrategy.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/reconcile/validator/ValidatorStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2010 IBM Corporation and others.
+ * Copyright (c) 2001, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -176,7 +176,7 @@ public class ValidatorStrategy extends StructuredTextReconcilingStrategy {
 		 * or validation has been disabled
 		 */
 		IFile file = getFile();
-		if (isCanceled() || fMetaData.isEmpty() || areAllValidatorsSuspended(file)) {
+		if (isCanceled() || fMetaData.isEmpty() || (file != null && areAllValidatorsSuspended(file))) {
 			return;
 		}
 
