660b255a0d04f90cf558abc710b4c19e80e90fef
nsandonato <nsandonato>
2011-07-25 19:53:18 +0000

    [352633] [Compatibility] WST XML UI test suite hangs on a confirmation dialog

--- tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/tests/XMLCodeFoldingTest.java
+++ tests/org.eclipse.wst.xml.ui.tests/src/org/eclipse/wst/xml/ui/tests/XMLCodeFoldingTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -446,7 +446,8 @@ public class XMLCodeFoldingTest extends TestCase implements ISourceReconcilingLi
 			while(iter.hasNext()) {
 				StructuredTextEditor editor = (StructuredTextEditor)iter.next();
 				editor.doSave(null);
-				editor.close(false);
+				final IEditorPart editorPart = editor.getEditorPart();
+				editorPart.getSite().getPage().closeEditor(editorPart, false);
 			}
 			
 			if (previousWTPAutoTestNonInteractivePropValue != null) {
