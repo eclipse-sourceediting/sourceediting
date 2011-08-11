3a65a96c3cae8f5aab90712b8b9183838a550173
nitind <nitind>
2011-08-11 00:21:01 +0000

    [nobug] doc

--- bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/provisional/StructuredModelManager.java
+++ bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/provisional/StructuredModelManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2009 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -19,9 +19,7 @@ import org.osgi.framework.Bundle;
 /**
  * Class to allow access to ModelManager. Not intended to be subclassed.
  * 
- * @plannedfor 1.0
- * @deprecated - use the one that is in
- *             org.eclipse.wst.sse.core.SttructuredModelManager
+ * @deprecated - use {@link org.eclipse.wst.sse.core.StructuredModelManager} instead
  */
 final public class StructuredModelManager {
 	/**
