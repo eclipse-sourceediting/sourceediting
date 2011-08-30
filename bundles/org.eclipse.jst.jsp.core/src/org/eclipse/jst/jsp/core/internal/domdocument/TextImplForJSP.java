3050d7b5fbb01dbc9bb0ec843f5ee115cce969b9
nsandonato <nsandonato>
2011-08-30 21:44:29 +0000

    [156293] [api] Several Node implementations need cloneNode method

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/domdocument/TextImplForJSP.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/domdocument/TextImplForJSP.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,14 +15,30 @@ package org.eclipse.jst.jsp.core.internal.domdocument;
 import org.eclipse.jst.jsp.core.internal.regions.DOMJSPRegionContexts;
 import org.eclipse.wst.xml.core.internal.document.TextImpl;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.UserDataHandler;
 
 public class TextImplForJSP extends TextImpl {
+
+	protected TextImplForJSP() {
+	}
+
+	protected TextImplForJSP(TextImplForJSP that) {
+		super(that);
+	}
+
 	protected boolean isNotNestedContent(String regionType) {
 		boolean result = regionType != DOMJSPRegionContexts.JSP_CONTENT;
 		return result;
 	}
 	protected void setOwnerDocument(Document ownerDocument) {
 		super.setOwnerDocument(ownerDocument);
+	}
+
+	public Node cloneNode(boolean deep) {
+		Node cloned = new TextImplForJSP(this);
+		notifyUserDataHandlers(UserDataHandler.NODE_CLONED, cloned);
+		return cloned;
 	}
 
 }
