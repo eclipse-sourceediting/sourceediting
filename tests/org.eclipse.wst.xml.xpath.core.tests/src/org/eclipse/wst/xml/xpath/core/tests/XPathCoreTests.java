addf101ea806a47d851d91a4ebccf0ee20bea07d
jmoller <jmoller>
2011-08-31 20:22:28 +0000

    Bug 348737 - Hook up default namespace from context

--- tests/org.eclipse.wst.xml.xpath.core.tests/src/org/eclipse/wst/xml/xpath/core/tests/XPathCoreTests.java
+++ tests/org.eclipse.wst.xml.xpath.core.tests/src/org/eclipse/wst/xml/xpath/core/tests/XPathCoreTests.java
@@ -1,5 +1,6 @@
 package org.eclipse.wst.xml.xpath.core.tests;
 
+import org.eclipse.wst.xsl.internal.core.xpath.tests.TestXPath20Helper;
 import org.eclipse.wst.xsl.internal.core.xpath.tests.TestXSLXPathHelper;
 
 import junit.framework.Test;
@@ -12,6 +13,7 @@ public class XPathCoreTests {
 				"Test for org.eclipse.wst.xml.xpath.core.tests");
 		//$JUnit-BEGIN$
 		suite.addTestSuite(TestXSLXPathHelper.class);
+		suite.addTestSuite(TestXPath20Helper.class);
 		//$JUnit-END$
 		return suite;
 	}
