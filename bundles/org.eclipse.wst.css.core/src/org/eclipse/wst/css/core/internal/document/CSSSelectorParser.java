6008fd0edc1480a4160bf718a17dbe543b02f6ab
nsandonato <nsandonato>
2011-09-27 15:07:59 +0000

    [359049] NullPointerException in CSSDocumentRegionEdgeMatcher while typing in CSS file

--- bundles/org.eclipse.wst.css.core/src/org/eclipse/wst/css/core/internal/document/CSSSelectorParser.java
+++ bundles/org.eclipse.wst.css.core/src/org/eclipse/wst/css/core/internal/document/CSSSelectorParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -121,7 +121,7 @@ public class CSSSelectorParser {
 					}
 					break;
 				case ATTRIBUTE :
-					if (isAttributeEnd(token)) {
+					if (isAttributeEnd(token) && attrBuf != null) {
 						appendAttribute(item, attrBuf);
 						attrBuf = null;
 						status = SIMPLE;
