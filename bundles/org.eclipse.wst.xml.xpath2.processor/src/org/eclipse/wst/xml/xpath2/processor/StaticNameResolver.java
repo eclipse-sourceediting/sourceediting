22ebf2c42f17de40b07da3dbe449eb83f2ba2c96
jmoller <jmoller>
2011-11-01 21:36:49 +0000

    Bug 361802: Variables should not get default namespace by default (updated contributor list)

--- bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/StaticNameResolver.java
+++ bundles/org.eclipse.wst.xml.xpath2.processor/src/org/eclipse/wst/xml/xpath2/processor/StaticNameResolver.java
@@ -9,7 +9,8 @@
  *     Andrea Bittau - initial API and implementation from the PsychoPath XPath 2.0 
  *     Jesper Steen Moller  - bug 340933 - Migrate to new XPath2 API
  *     Jesper Steen Moller - bug 343804 - Updated API information
- *     Jesper Steen Moller - bug 343804 - Updated API information
+ *     Lukasz Wycisk - bug 361802 - Default variable namespace Ð no namespace
+ *     
  *******************************************************************************/
 
 package org.eclipse.wst.xml.xpath2.processor;
