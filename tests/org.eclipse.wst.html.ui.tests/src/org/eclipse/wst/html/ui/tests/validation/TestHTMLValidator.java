3826acdf80a893b5783ffea290a255bac0f5f576
nsandonato <nsandonato>
2011-11-09 21:39:52 +0000

    [338111] Separate the HTML validator into the org.eclipse.wst.html.core bundle so it can be used in headless tools

--- tests/org.eclipse.wst.html.ui.tests/src/org/eclipse/wst/html/ui/tests/validation/TestHTMLValidator.java
+++ tests/org.eclipse.wst.html.ui.tests/src/org/eclipse/wst/html/ui/tests/validation/TestHTMLValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 IBM Corporation and others.
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -22,7 +22,7 @@ import junit.framework.TestSuite;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.wst.html.internal.validation.HTMLValidator;
+import org.eclipse.wst.html.core.internal.validation.HTMLValidator;
 import org.eclipse.wst.html.ui.tests.ProjectUtil;
 import org.eclipse.wst.sse.core.StructuredModelManager;
 import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
@@ -35,7 +35,7 @@ import org.eclipse.wst.validation.internal.provisional.core.IValidator;
 /**
  * Test for the HTMLValidator
  * 
- * @see org.eclipse.wst.html.internal.validation.HTMLValidator
+ * @see org.eclipse.wst.html.core.internal.validation.HTMLValidator
  */
 public class TestHTMLValidator extends TestCase {
 
