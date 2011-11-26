c41a92e5c01537484a69398dfe27f673ac6cf22f
jmoller <jmoller>
2011-11-26 10:53:52 +0000

    Bug 364885: Reduce XPath2 performance and memory footprint https://bugs.eclipse.org/bugs/show_bug.cgi?id=364885

--- tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/test/newapi/ContextBuilderTest.java
+++ tests/org.eclipse.wst.xml.xpath2.processor.tests/src/org/eclipse/wst/xml/xpath2/processor/test/newapi/ContextBuilderTest.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Jesper Steen Moller and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Jesper Steen Moller  - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.wst.xml.xpath2.processor.test.newapi;
 
 import junit.framework.TestCase;
