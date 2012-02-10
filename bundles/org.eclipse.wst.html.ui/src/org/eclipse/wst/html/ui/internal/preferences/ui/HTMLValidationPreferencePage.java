caf510940d5c9d0aa218347a17b3ee1b62b5ee99
nitind <nitind>
2012-02-10 03:58:06 +0000

    [371019] Incorrect validation of resource across web projects

--- bundles/org.eclipse.wst.html.ui/src/org/eclipse/wst/html/ui/internal/preferences/ui/HTMLValidationPreferencePage.java
+++ bundles/org.eclipse.wst.html.ui/src/org/eclipse/wst/html/ui/internal/preferences/ui/HTMLValidationPreferencePage.java
@@ -165,8 +165,8 @@ public class HTMLValidationPreferencePage extends AbstractValidationSettingsPage
 		label = HTMLUIMessages.HTMLValidationPreferencePage_7;
 		addComboBox(inner, label, HTMLCorePreferenceNames.ATTRIBUTE_VALUE_UNCLOSED, SEVERITIES, errorWarningIgnoreLabel, 0);
 		
-		label = HTMLUIMessages.HTMLValidationPreferencePage_35;
-		addComboBox(inner, label, HTMLCorePreferenceNames.ATTRIBUTE_VALUE_RESOURCE_NOT_FOUND, SEVERITIES, errorWarningIgnoreLabel, 0);
+		//label = HTMLUIMessages.HTMLValidationPreferencePage_35;
+		//addComboBox(inner, label, HTMLCorePreferenceNames.ATTRIBUTE_VALUE_RESOURCE_NOT_FOUND, SEVERITIES, errorWarningIgnoreLabel, 0);
 
 		// End Attribute section
 		
