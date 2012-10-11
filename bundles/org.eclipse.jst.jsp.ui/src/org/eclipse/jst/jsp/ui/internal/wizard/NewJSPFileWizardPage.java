c8c092dd8c2b6f6b54c6d409fed02bcfcd2c057a
Nick Sandonato <nsandona@us.ibm.com>
2012-10-11 16:06:01 -0400

    [375605] 'New File (HTML/JSP)' wizard uses wrong default location

--- bundles/org.eclipse.jst.jsp.ui/src/org/eclipse/jst/jsp/ui/internal/wizard/NewJSPFileWizardPage.java
+++ bundles/org.eclipse.jst.jsp.ui/src/org/eclipse/jst/jsp/ui/internal/wizard/NewJSPFileWizardPage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 IBM Corporation and others.
+ * Copyright (c) 2006, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -82,12 +82,14 @@ class NewJSPFileWizardPage extends WizardNewFileCreationPage {
 
 		IPath fullPath = getContainerFullPath();
 		IProject project = getProjectFromPath(fullPath);
-		IPath[] webContentPaths = FacetModuleCoreSupport.getAcceptableRootPaths(project);
-		for (int i = 0; webContentPaths != null && i < webContentPaths.length; i++) {
-			if (!webContentPaths[i].isPrefixOf(fullPath)) {
-				setContainerFullPath(webContentPaths[i]);
-				break;
-			}
+		IPath root = FacetModuleCoreSupport.getRootContainerForPath(project, fullPath);
+		if (root != null) {
+			return;
+		}
+		root = FacetModuleCoreSupport.getDefaultRootContainer(project);
+		if (root != null) {
+			setContainerFullPath(root);
+			return;
 		}
 	}
 
