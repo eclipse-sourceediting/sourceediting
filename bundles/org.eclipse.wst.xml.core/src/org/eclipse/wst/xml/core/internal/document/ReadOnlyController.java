60816b23e16d6f2251bce99a67c101f725de63ab
Nick Sandonato <nsandona@us.ibm.com>
2012-11-08 15:15:14 -0500

    [163043] [api] Error on replacing a DOM element with another read-only element

--- bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/ReadOnlyController.java
+++ bundles/org.eclipse.wst.xml.core/src/org/eclipse/wst/xml/core/internal/document/ReadOnlyController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2010 IBM Corporation and others.
+ * Copyright (c) 2001, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -101,7 +101,7 @@ class ReadOnlyController {
 
 	private Span getDataSpanForElement(IDOMElement node) {
 		IStructuredDocumentRegion docRegion = node.getStartStructuredDocumentRegion();
-		if (docRegion == null) {
+		if (docRegion == null || docRegion.getParentDocument() == null) {
 			return new Span(0, -1);
 		}
 
