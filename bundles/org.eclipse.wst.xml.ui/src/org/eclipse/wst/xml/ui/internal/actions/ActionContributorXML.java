922e67f7fce0040629c50dbf06a3be110bfb3dc5
Nick Sandonato <nsandona@us.ibm.com>
2013-01-16 18:25:25 -0500

    [398345] Numerous action handler conflicts when changing editors

--- bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/actions/ActionContributorXML.java
+++ bundles/org.eclipse.wst.xml.ui/src/org/eclipse/wst/xml/ui/internal/actions/ActionContributorXML.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2008 IBM Corporation and others.
+ * Copyright (c) 2001, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -196,7 +196,9 @@ public class ActionContributorXML extends ActionContributor {
 
 		fGoToMatchingTagAction.setEditor(textEditor);
 		if (textEditor != null) {
-			textEditor.setAction(GO_TO_MATCHING_TAG_ID, fGoToMatchingTagAction);
+			if (textEditor.getAction(GO_TO_MATCHING_TAG_ID) != fGoToMatchingTagAction) {
+				textEditor.setAction(GO_TO_MATCHING_TAG_ID, fGoToMatchingTagAction);
+			}
 		}
 	}
 
