0f6b4b67be773432c05cbe27d63e89763d85dda7
Salvador Zalapa <zalapa@mx1.ibm.com>
2013-01-24 12:08:33 -0600

    [346128] [content model] New XML Wizard creates invalid value for gMonth data type

--- bundles/org.eclipse.wst.xsd.core/src-contentmodel/org/eclipse/wst/xsd/contentmodel/internal/XSDTypeUtil.java
+++ bundles/org.eclipse.wst.xsd.core/src-contentmodel/org/eclipse/wst/xsd/contentmodel/internal/XSDTypeUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2010 IBM Corporation and others.
+ * Copyright (c) 2001, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -41,7 +41,7 @@ public class XSDTypeUtil
     defaultValue.put("ENTITIES", "entities");
     defaultValue.put("float", "0.0");
     defaultValue.put("gDay", "---01");
-    defaultValue.put("gMonth", "--01--");
+    defaultValue.put("gMonth", "--01");
     defaultValue.put("gMonthDay", "--01-01");
     defaultValue.put("gYear", "2001");
     defaultValue.put("gYearMonth", "2001-01");
