2bc9389868e8e197dd3e4ea74c509b9dd5c5a246
Jesper Steen Møller <jesper@selskabet.org>
2013-04-23 01:04:25 +0200

    [406262] [xsl][validation] StackOverflow when checking for circular includes on next level import (copyright fix)

--- tests/org.eclipse.wst.xsl.core.tests/src/org/eclipse/wst/xsl/internal/model/tests/TestStylesheetModel.java
+++ tests/org.eclipse.wst.xsl.core.tests/src/org/eclipse/wst/xsl/internal/model/tests/TestStylesheetModel.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2013 Chase Technology Ltd - http://www.chasetechnology.co.uk
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Doug Satchwell (Chase Technology Ltd) - initial API and implementation
+ *     Jesper S Moller - bug 406262 - StackOverflow when checking for circular includes on next level import
+ *******************************************************************************/
+
 package org.eclipse.wst.xsl.internal.model.tests;
 
 import java.util.ArrayList;
