b91a904e9de156d21bf627fe19dbc9a06345742c
Nick Sandonato <nsandona@us.ibm.com>
2014-03-12 18:01:06 -0400

    [430224] Null Pointer Exception in DesignPageNavigationLocation.java

--- bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/DesignPageNavigationLocation.java
+++ bundles/org.eclipse.wst.xml.ui/src-multipage/org/eclipse/wst/xml/ui/internal/tabletree/DesignPageNavigationLocation.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2011 IBM Corporation and others. All rights reserved. This
+ * Copyright (c) 2011, 2014 IBM Corporation and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms
  * of the Eclipse Public License v1.0 which accompanies this distribution, and
  * is available at http://www.eclipse.org/legal/epl-v10.html
@@ -372,7 +372,7 @@ class DesignPageNavigationLocation extends NavigationLocation {
 	public void update() {
 		final IEditorPart part = getEditorPart();
 
-		if (equalsLocationOf())
+		if (part == null || equalsLocationOf())
 			return;
 
 		final ISelection selection = fViewer.getSelectionProvider().getSelection();
