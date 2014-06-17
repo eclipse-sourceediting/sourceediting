2a2507956ec0d60328429747f5ba0c2147a880ce
Nick Sandonato <nsandona@us.ibm.com>
2014-06-17 13:09:12 -0400

    [367267] Code Assist Item contains string with new line character and therefore is not displayed properly.

--- bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/contentassist/CustomCompletionProposal.java
+++ bundles/org.eclipse.wst.sse.ui/src/org/eclipse/wst/sse/ui/internal/contentassist/CustomCompletionProposal.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2010 IBM Corporation and others.
+ * Copyright (c) 2001, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -87,6 +87,9 @@ public class CustomCompletionProposal implements ICompletionProposal, ICompletio
 		fCursorPosition = cursorPosition;
 		fImage = image;
 		fDisplayString = displayString;
+		if (fDisplayString != null) {
+			fDisplayString = fDisplayString.replaceAll("\\s+", " "); //$NON-NLS-1$ //$NON-NLS-2$
+		}
 		fAlternateMatch = alternateMatch;
 		fContextInformation = contextInformation;
 		fAdditionalProposalInfo = additionalProposalInfo;
