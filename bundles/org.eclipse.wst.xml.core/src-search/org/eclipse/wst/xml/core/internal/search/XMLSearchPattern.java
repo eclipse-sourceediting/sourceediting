d32e15046cc9340dcd58f00bd29ffca4616c8f27
kchong <kchong@ca.ibm.com>
2014-08-11 16:13:39 -0400

    [440912] Can not change names in XML Schema Editor when schema does not have a target namespace

--- bundles/org.eclipse.wst.xml.core/src-search/org/eclipse/wst/xml/core/internal/search/XMLSearchPattern.java
+++ bundles/org.eclipse.wst.xml.core/src-search/org/eclipse/wst/xml/core/internal/search/XMLSearchPattern.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2011 IBM Corporation and others.
+ * Copyright (c) 2004, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -127,7 +127,8 @@ public class XMLSearchPattern extends SearchPattern{
 					 return true;
 			 }
 			 else {
-				 if ((namespace.equals(searchNamespace) || "*".equals(searchNamespace)) && name.equals(searchName))
+				 if ((namespace.equals(searchNamespace) || "*".equals(searchNamespace) || ("".equals(namespace) && searchNamespace==null)
+						 )&& name.equals(searchName))
 					 return true;
 			 }
 		 }
