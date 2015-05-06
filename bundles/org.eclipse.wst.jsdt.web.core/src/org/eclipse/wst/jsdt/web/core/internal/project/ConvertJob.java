426e4ff619000acb94f369c1347eded89fc2f643
vrubezhny <vrubezhny@exadel.com>
2015-05-06 19:28:14 -0400

    Bug 442508 - Bug in org.eclipse.wst.jsdt.web.core.internal.project.ConvertJob.runInWorkspace
    Working copy is disposed, so no any listeners should stay on IFacetedProjectWorkingCopy
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.jsdt.web.core/src/org/eclipse/wst/jsdt/web/core/internal/project/ConvertJob.java
+++ bundles/org.eclipse.wst.jsdt.web.core/src/org/eclipse/wst/jsdt/web/core/internal/project/ConvertJob.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 IBM Corporation and others.
+ * Copyright (c) 2010, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -64,6 +64,7 @@ class ConvertJob extends WorkspaceJob {
 					fixed.add(projectFacet);
 					copy.setFixedProjectFacets(fixed);
 					copy.commitChanges(new NullProgressMonitor());
+					copy.dispose();
 				}
 				else {
 					Set fixed = new HashSet(facetedProject.getFixedProjectFacets());
