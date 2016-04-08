917c0c879973c7075f0867ff36f92782fd41cb23
Victor Rubezhny <vrubezhny@exadel.com>
2016-04-08 21:05:38 +0300

    Bug 487036 - JSON Editor missing some icon(s?)
    Outline icons are added
    
    Signed-off-by: Victor Rubezhny <vrubezhny@exadel.com>

--- bundles/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/editor/JSONEditorPluginImages.java
+++ bundles/org.eclipse.wst.json.ui/src/org/eclipse/wst/json/ui/internal/editor/JSONEditorPluginImages.java
@@ -1,23 +1,23 @@
 /**
- *  Copyright (c) 2013-2014 Angelo ZERR.
+ *  Copyright (c) 2015, 2016 Angelo ZERR.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
  *  http://www.eclipse.org/legal/epl-v10.html
  *
  *  Contributors:
- *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *  	Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
  */
 package org.eclipse.wst.json.ui.internal.editor;
 
 public class JSONEditorPluginImages {
 
-	public static final String IMG_OBJ_OBJECT = "icons/full/obj16/json-16.png";
-	public static final String IMG_OBJ_ARRAY = "icons/full/obj16/json-16.png";
-	public static final String IMG_OBJ_VALUE_BOOLEAN = "icons/full/obj16/json-16.png";
-	public static final String IMG_OBJ_VALUE_NUMBER = "icons/full/obj16/json-16.png";
-	public static final String IMG_OBJ_VALUE_NULL = "icons/full/obj16/json-16.png";
-	public static final String IMG_OBJ_VALUE_STRING = "icons/full/obj16/json-16.png";
+	public static final String IMG_OBJ_OBJECT = "icons/full/obj16/json-object.png";
+	public static final String IMG_OBJ_ARRAY = "icons/full/obj16/json-array.png";
+	public static final String IMG_OBJ_VALUE_BOOLEAN = "icons/full/obj16/json-boolean.png";
+	public static final String IMG_OBJ_VALUE_NUMBER = "icons/full/obj16/json-number.png";
+	public static final String IMG_OBJ_VALUE_NULL = "icons/full/obj16/json-null.png";
+	public static final String IMG_OBJ_VALUE_STRING = "icons/full/obj16/json-string.png";
 	
 	public static final String IMG_OBJ_SORT = "icons/full/obj16/sort.gif";
 
