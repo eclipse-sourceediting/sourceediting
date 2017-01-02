bc66e4f0782ef0e23d21657890cfe09c04e9bcaa
Nitin Dahyabhai <nitind@us.ibm.com>
2017-01-02 01:15:13 -0500

    [nobug] drop the number of background change requesters in TestStructuredTextEditor.testManyNonUIThreadsUpdatingEditorDocument()

--- tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/TestStructuredTextEditor.java
+++ tests/org.eclipse.wst.sse.ui.tests/src/org/eclipse/wst/sse/ui/tests/TestStructuredTextEditor.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2016 IBM Corporation and others.
+ * Copyright (c) 2005, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -253,7 +253,7 @@ public class TestStructuredTextEditor extends TestCase {
 	 * @throws Exception
 	 */
 	public void testManyNonUIThreadsUpdatingEditorDocument() throws Exception {
-		final int numberOfJobs = 30;
+		final int numberOfJobs = 3;
 		/**
 		 * 15 minute timeout before we stop waiting for the change jobs to
 		 * complete
