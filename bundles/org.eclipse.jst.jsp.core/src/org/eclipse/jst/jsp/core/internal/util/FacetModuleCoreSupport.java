1801db94ff23c1a7a999aa0631de25f5d4fda473
Nitin Dahyabhai <nitind@pobox.com>
2017-07-04 20:24:47 -0400

    [519188] Update JSP FacetModuleCoreSupport for Java EE 8

--- bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/util/FacetModuleCoreSupport.java
+++ bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/util/FacetModuleCoreSupport.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2013 IBM Corporation and others.
+ * Copyright (c) 2007, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -38,7 +38,7 @@ public final class FacetModuleCoreSupport {
 	static final String META_INF_RESOURCES = "META-INF/resources/"; //$NON-NLS-1$
 	static final IPath META_INF_RESOURCES_PATH = new Path(META_INF_RESOURCES);
 
-	static final float DEFAULT_SERVLET_VERSION = 3f;
+	static final float DEFAULT_SERVLET_VERSION = 3.1f;
 
 	/**
 	 * @param project
