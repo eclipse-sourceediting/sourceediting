ac15f0565f5b71b5bd50e93ca10010031888050d
Rob Stryker <rob@oxbeef.net>
2017-10-26 15:02:33 -0400

    Fix org.eclipse.wst.sse.unittests which fails to run
    Signed-off-by: Rob Stryker <rob@oxbeef.net>
    
    Fix more unit tests
    
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

--- web/tests/org.eclipse.wst.sse.unittests/src/org/eclipse/wst/sse/unittests/MasterListTestSuite.java
+++ web/tests/org.eclipse.wst.sse.unittests/src/org/eclipse/wst/sse/unittests/MasterListTestSuite.java
@@ -12,13 +12,8 @@
 
 package org.eclipse.wst.sse.unittests;
 
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.jst.jsp.core.tests.JSPCoreTestSuite;
 import org.eclipse.jst.jsp.ui.tests.JSPUITestSuite;
-import org.eclipse.ui.IWorkbenchPreferenceConstants;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.wst.css.core.tests.CSSCoreTestSuite;
 import org.eclipse.wst.css.ui.tests.CSSUITestSuite;
 import org.eclipse.wst.dtd.core.tests.DTDCoreTestSuite;
@@ -35,87 +30,42 @@ import org.eclipse.wst.xml.validation.tests.internal.AllXMLTests;
 import org.eclipse.wst.xsd.core.tests.internal.AllXSDCoreTests;
 import org.eclipse.wst.xsd.validation.tests.internal.AllXSDTests;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
-public class MasterListTestSuite extends TestSuite {
-	private static final String CLASS = "class";
-	private static final String EXTENSION_POINT_ID = "org.eclipse.wst.sse.unittests.additionalTests";
-
-	public MasterListTestSuite() {
-		super("WTP Source Editing Master List Test Suite");
-
-		System.setProperty("wtp.autotest.noninteractive", "true");
-		PlatformUI.getPreferenceStore().setValue(IWorkbenchPreferenceConstants.SHOW_SYSTEM_JOBS, true);
-		
-		addTest(SSEModelTestSuite.suite());
-
-		addTest(SSEModelXMLTestSuite.suite());
-		addTest(DTDCoreTestSuite.suite());
-		addTest(AllXSDCoreTests.suite());
-		addTest(CSSCoreTestSuite.suite());
-		addTest(HTMLCoreTestSuite.suite());
-		addTest(JSPCoreTestSuite.suite());
-
-//		addTest(EncodingTestSuite.suite());
-//		addTest(CSSEncodingTestSuite.suite());
-//		addTest(HTMLEncodingTestSuite.suite());
-//		addTest(JSPEncodingTestSuite.suite());
-//
-		addTest(AllXMLTests.suite());
-		addTest(AllXSDTests.suite());
-
-		addTest(SSEUITestSuite.suite());
-		addTest(XMLUITestSuite.suite());
-		addTest(DTDUITestSuite.suite());
-		addTest(CSSUITestSuite.suite());
-		addTest(HTMLUITestSuite.suite());
-		addTest(JSPUITestSuite.suite());
-		
-//		addTest(RunJSDTCoreTests.suite());
-//		addTest(JSDTCompilerTests.suite());
-//		addTest(JSDTUITests.suite());
-		addTest(AllWebCoreTests.suite());
-		addTest(AllWebUITests.suite());
-		
-
+@RunWith(Suite.class)
+@SuiteClasses({
+		SSEModelTestSuite.class,
+		SSEModelXMLTestSuite.class,
+		DTDCoreTestSuite.class,
+		AllXSDCoreTests.class,
+		CSSCoreTestSuite.class,
+		HTMLCoreTestSuite.class,
+		JSPCoreTestSuite.class,
+		AllXMLTests.class,
+		AllXSDTests.class,
+		SSEUITestSuite.class,
+		XMLUITestSuite.class,
+		DTDUITestSuite.class,
+		CSSUITestSuite.class,
+		HTMLUITestSuite.class,
+		JSPUITestSuite.class,
+		AllWebCoreTests.class,
+		AllWebUITests.class,
+//		addTest(RunJSDTCoreTests.class);
+//		addTest(JSDTCompilerTests.class);
+//		addTest(JSDTUITests.class);
+//		addTest(EncodingTestSuite.class);
+//		addTest(CSSEncodingTestSuite.class);
+//		addTest(HTMLEncodingTestSuite.class);
+//		addTest(JSPEncodingTestSuite.class);
 //		addTest(new AllTestsSuite());
 		
-		// addTest(RegressionBucket.suite());
-		// addTest(AllTestCases.suite());
+		// addTest(RegressionBucket.class);
+		// addTest(AllTestCases.class);
 
-		IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(EXTENSION_POINT_ID);
-		for (int i = 0; i < elements.length; i++) {
-			if (elements[i].getName().equals("suite")) {
-				TestSuite suite;
-				try {
-					suite = (TestSuite) elements[i].createExecutableExtension(CLASS);
-					addTestSuite(suite.getClass());
-					System.err.println("Adding TestSuite " + suite.getClass().getName());
-				}
-				catch (CoreException e) {
-					e.printStackTrace(System.err);
-					Platform.getLog(Platform.getBundle("org.eclipse.wst.sse.unittests")).log(e.getStatus());
-				}
-			}
-			else if (elements[i].getName().equals("test")) {
-				Test test;
-				try {
-					test = (Test) elements[i].createExecutableExtension(CLASS);
-					addTestSuite(test.getClass());
-					System.err.println("Adding TestCase " + test.getClass().getName());
-				}
-				catch (CoreException e) {
-					e.printStackTrace(System.err);
-					Platform.getLog(Platform.getBundle("org.eclipse.wst.sse.unittests")).log(e.getStatus());
-				}
-			}
-		}
-	}
+})
 
-	public void testAll() {
-		// this method needs to exist, but doesn't really do anything
-		// other than to signal to create an instance of this class.
-		// The rest it automatic from the tests added in constructor.
-	}
+public class MasterListTestSuite {
 }
