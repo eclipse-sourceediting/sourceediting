a8e7af18d2ef1199e9e63f334d0e820bf5539364
Nitin Dahyabhai <nitind@pobox.com>
2018-04-18 19:00:01 -0400

    [519189] Source Editing should move up to Java 8

--- core/bundles/org.eclipse.wst.sse.core/src-tasktags/org/eclipse/wst/sse/core/internal/provisional/tasks/IFileTaskScanner.java
+++ core/bundles/org.eclipse.wst.sse.core/src-tasktags/org/eclipse/wst/sse/core/internal/provisional/tasks/IFileTaskScanner.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2008 IBM Corporation and others.
+ * Copyright (c) 2001, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,7 +60,7 @@ public interface IFileTaskScanner {
 	 *         specify a type to use when creating the task marker.
 	 *         </p>
 	 */
-	Map[] scan(IFile file, TaskTag[] taskTags, IProgressMonitor monitor);
+	Map<String, Object>[] scan(IFile file, TaskTag[] taskTags, IProgressMonitor monitor);
 
 	/**
 	 * Notifies the scanner that scanning is done for now. Resources held from
