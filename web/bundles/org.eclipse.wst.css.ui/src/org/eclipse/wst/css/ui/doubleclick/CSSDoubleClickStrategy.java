583a7960c7c9d75cb4649eec12cf0b6424c0eb84
Nitin Dahyabhai <nitind@us.ibm.com>
2019-11-26 18:04:11 -0500

    [473908] Smarter double-click selection in CSS editor

--- web/bundles/org.eclipse.wst.css.ui/src/org/eclipse/wst/css/ui/doubleclick/CSSDoubleClickStrategy.java
+++ web/bundles/org.eclipse.wst.css.ui/src/org/eclipse/wst/css/ui/doubleclick/CSSDoubleClickStrategy.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 IBM Corporation and others.
+ * Copyright (c) 2013, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -14,6 +14,8 @@
 
 package org.eclipse.wst.css.ui.doubleclick;
 
+import java.util.Arrays;
+
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.DefaultTextDoubleClickStrategy;
 import org.eclipse.jface.text.IDocument;
@@ -21,9 +23,14 @@ import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.Region;
 
 public class CSSDoubleClickStrategy extends DefaultTextDoubleClickStrategy {
-	
+	private static final char[] NON_BREAKERS = new char[]{'-'};
+	static {
+		Arrays.sort(NON_BREAKERS);
+	}
+
+	@Override
 	protected IRegion findExtendedDoubleClickSelection(IDocument document, int offset) {
-		IRegion word= super.findExtendedDoubleClickSelection(document, offset);
+		IRegion word = super.findExtendedDoubleClickSelection(document, offset);
 		if (word != null)
 			return word;
 		word = findWord(document, offset);
@@ -32,20 +39,26 @@ public class CSSDoubleClickStrategy extends DefaultTextDoubleClickStrategy {
 			line = document.getLineInformationOfOffset(offset);
 			if (offset == line.getOffset() + line.getLength())
 				return null;
-			int start= word.getOffset();
-			int end= start + word.getLength();
-			if (start > 0 && document.getChar(start - 1) == '#'){
-				start --;
+			int start = word.getOffset();
+			int end = start + word.getLength();
+			while (start > 0 && Arrays.binarySearch(NON_BREAKERS, document.getChar(start - 1)) > -1) {
+				IRegion previous = findWord(document, --start);
+				if (previous != null) {
+					start = previous.getOffset();
+				}
 			}
-			else if (end == offset && end == start + 1 && end < line.getOffset() + line.getLength() && document.getChar(end) == '#') {
-				return findExtendedDoubleClickSelection(document, offset + 1);
+			while (end < line.getOffset() + line.getLength() && Arrays.binarySearch(NON_BREAKERS, document.getChar(end)) > -1) {
+				IRegion next = findExtendedDoubleClickSelection(document, end + 1);
+				if (next != null) {
+					end = next.getOffset() + next.getLength();
+				}
 			}
 			if (start == end)
 				return null;
 			return new Region(start, end - start);
-		} catch (BadLocationException e) {
+		}
+		catch (BadLocationException e) {
 			return null;
 		}
 	}
-
 }
