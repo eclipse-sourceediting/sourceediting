78a344ca1bd0cc54273a31ffd8a0f75ace880797
Nitin Dahyabhai <thatnitind@gmail.com>
2022-07-06 10:55:39 -0400

    [nobug] update default task tag values
    Change-Id: Ib9389105b67e8dae4be0a6aeab5d2e6a50059fa3

--- core/bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/preferences/PreferenceInitializer.java
+++ core/bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/preferences/PreferenceInitializer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2005 IBM Corporation and others.
+ * Copyright (c) 2001, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -19,16 +19,13 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.wst.sse.core.internal.tasks.TaskTagPreferenceKeys;
 
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
-	 */
+	@Override
 	public void initializeDefaultPreferences() {
-		IEclipsePreferences taskTagDefaults = new DefaultScope().getNode(TaskTagPreferenceKeys.TASK_TAG_NODE);
+		IEclipsePreferences taskTagDefaults = DefaultScope.INSTANCE.getNode(TaskTagPreferenceKeys.TASK_TAG_NODE);
 		taskTagDefaults.putBoolean(TaskTagPreferenceKeys.TASK_TAG_ENABLE, false);
-		taskTagDefaults.put(TaskTagPreferenceKeys.TASK_TAG_TAGS, "TODO,FIXME,XXX"); //$NON-NLS-1$
-		taskTagDefaults.put(TaskTagPreferenceKeys.TASK_TAG_PRIORITIES, "1,2,1"); //$NON-NLS-1$
+		taskTagDefaults.put(TaskTagPreferenceKeys.TASK_TAG_TAGS, "BUG,NOTE,FIXME,HACK,TODO,XXX"); //$NON-NLS-1$
+		// 2,1,0 = high,medium,low
+		taskTagDefaults.put(TaskTagPreferenceKeys.TASK_TAG_PRIORITIES, "1,0,2,1,1,1"); //$NON-NLS-1$
 		taskTagDefaults.put(TaskTagPreferenceKeys.TASK_TAG_CONTENTTYPES_IGNORED, ""); //$NON-NLS-1$
 	}
 }
