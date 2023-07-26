756a9071a523058b065865f9a9d6071cf75ee2a7
Nitin Dahyabhai <thatnitind@gmail.com>
2023-07-26 02:51:02 -0400

    [582204] Correct for accidental included change

--- web/bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/tips/LocalProvider.java
+++ web/bundles/org.eclipse.jst.jsp.core/src/org/eclipse/jst/jsp/core/internal/tips/LocalProvider.java
@@ -1,14 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Nitin Dahyabhai.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Nitin Dahyabhai - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.jst.jsp.core.internal.tips;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.tips.core.TipImage;
 
 public class LocalProvider extends org.eclipse.tips.core.TipProvider {
 
 	@Override
 	public void dispose() {
-		
+		// TODO Auto-generated method stub		
 	}
 
 	@Override
@@ -28,7 +41,7 @@ public class LocalProvider extends org.eclipse.tips.core.TipProvider {
 
 	@Override
 	public IStatus loadNewTips(IProgressMonitor monitor) {
-		return null;
+		return Status.OK_STATUS;
 	}
 
 }
