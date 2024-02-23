9a085ab6ef938d533c888a52e71f611d7e608d58
Nitin Dahyabhai <thatnitind@gmail.com>
2024-02-22 21:37:39 -0500

    #4 FileBufferModelManager.detectContentType should ignore FNFEs on file stores

--- core/bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/FileBufferModelManager.java
+++ core/bundles/org.eclipse.wst.sse.core/src/org/eclipse/wst/sse/core/internal/FileBufferModelManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2023 IBM Corporation and others.
+ * Copyright (c) 2001, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -603,7 +603,10 @@ public class FileBufferModelManager {
 						 */
 					}
 					catch (CoreException e) {
-						Logger.logException(e);
+						/*
+						 * Also failure. Resort to finding the content type based
+						 * solely on the file name.
+						 */
 					}
 					if (type == null) {
 						type = Platform.getContentTypeManager().findContentTypeFor(fileStore.getName());
