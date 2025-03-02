68247112b72c95ec60760cb9c51979a3ae0e0904
Nitin Dahyabhai <thatnitind@gmail.com>
2025-03-02 03:53:20 -0500

    Increment versions for 3.37

--- xml/bundles/org.eclipse.wst.xsd.core/src-contentmodel/org/eclipse/wst/xsd/contentmodel/internal/XSDImpl.java
+++ xml/bundles/org.eclipse.wst.xsd.core/src-contentmodel/org/eclipse/wst/xsd/contentmodel/internal/XSDImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2014 IBM Corporation and others.
+ * Copyright (c) 2001, 2025 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -370,7 +370,7 @@ public class XSDImpl
     // type with simple content
     if (type instanceof XSDComplexTypeDefinition)
     {
-      type = ((XSDComplexTypeDefinition)type).getSimpleType();
+      type = type.getSimpleType();
     }
     if (type instanceof XSDSimpleTypeDefinition)
     {         
